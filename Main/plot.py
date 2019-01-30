1afb5a94c4b9b1e21428328d2daa523de6611fd0
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2019-01-30 13:38:19 +0100

    Fixed the error with different sizes

--- Main/plot.py
+++ Main/plot.py
@@ -10,32 +10,54 @@ import csv
 T = []
 d = []
 t = []
-d2 = []
-d3 = []
 
 with open('desorption1e22.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
+    d1 = []
+    t1 = []
+    T1 = []
     for row in plots:
         if 'd' not in row and 'T' not in row and 't' not in row:
-            d.append(float(row[0]))
-            T.append(float(row[1]))
-            t.append(float(row[2]))
-
-with open('desorption.csv', 'r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        if 'd' not in row and 'T' not in row and 't' not in row:
-            d2.append(float(row[0]))
+            d1.append(float(row[0]))
+            T1.append(float(row[1]))
+            t1.append(float(row[2]))
+    T.append(T1)
+    d.append(d1)
+    t.append(t1)
 
 with open('desorption1e23.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
+    d1 = []
+    t1 = []
+    T1 = []
     for row in plots:
         if 'd' not in row and 'T' not in row and 't' not in row:
-            d3.append(float(row[0]))
+            d1.append(float(row[0]))
+            T1.append(float(row[1]))
+            t1.append(float(row[2]))
+    T.append(T1)
+    d.append(d1)
+    t.append(t1)
 
-plt.plot(T, d, label=r'fluence = $1\times 10^{22}$ D/m$ ^{2}$')
-plt.plot(T, d3, label=r'fluence = $1\times 10^{23}$ D/m$ ^{2}$')
-plt.plot(T, d2, label="desorption.csv")
+with open('desorption.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    d1 = []
+    t1 = []
+    T1 = []
+    for row in plots:
+        if 'd' not in row and 'T' not in row and 't' not in row:
+            d1.append(float(row[0]))
+            T1.append(float(row[1]))
+            t1.append(float(row[2]))
+    T.append(T1)
+    d.append(d1)
+    t.append(t1)
+
+
+
+plt.plot(T[0], d[0], label=r'fluence = $1\times 10^{22}$ D/m$ ^{2}$')
+plt.plot(T[1], d[1], label=r'fluence = $1\times 10^{23}$ D/m$ ^{2}$')
+plt.plot(T[2], d[2], label="desorption.csv")
 plt.xlabel('T(K)')
 plt.ylabel(r'desorption(D/m$ ^{2}$/s)')
 plt.title('TDS')
