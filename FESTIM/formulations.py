e0b5e010092170a4f50454ce816334c07cf7245f
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2019-05-06 13:58:05 +0200

    cond, rho and cp can now be functions

--- FESTIM/formulations.py
+++ FESTIM/formulations.py
@@ -142,6 +142,8 @@ def define_variational_problem_heat_transfers(
         if "thermal_cond" not in mat.keys():
             raise NameError("Missing thermal_cond key in material")
         thermal_cond = mat["thermal_cond"]
+        if callable(thermal_cond):  # if thermal_cond is a function
+            thermal_cond = thermal_cond(T)
         vol = mat["id"]
         if parameters["temperature"]["type"] == "solve_transient":
             T_n = functions[2]
@@ -151,6 +153,10 @@ def define_variational_problem_heat_transfers(
                 raise NameError("Missing rho key in material")
             cp = mat["heat_capacity"]
             rho = mat["rho"]
+            if callable(cp):  # if cp or rho are functions, apply T
+                cp = cp(T)
+            if callable(rho):
+                rho = rho(T)
             # Transien term
             F += rho*cp*(T-T_n)/dt*vT*dx(vol)
         # Diffusion term
