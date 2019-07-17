a1e94322be0276dc6d981f7e8324f8dc955583ae
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2019-07-17 14:56:15 +0200

    Initial condition from xdmf if xdmf file given

--- FESTIM/initialise_solutions.py
+++ FESTIM/initialise_solutions.py
@@ -16,9 +16,20 @@ def initialising_solutions(V, initial_conditions):
     # initial conditions are 0 by default
     expression = ['0'] * len(components)
     for ini in initial_conditions:
-        value = ini["value"]
-        value = sp.printing.ccode(value)
-        comp = Expression(value, degree=3, t=0)
+        if type(ini['value']) == str and ini['value'].endswith(".xdmf"):
+            comp = Function(V.sub(ini["component"]).collapse())
+            if "label" not in ini.keys():
+                raise KeyError("label key not found")
+            if "time_step" not in ini.keys():
+                raise KeyError("time_step key not found")
+            with XDMFFile(ini["value"]) as file:
+                file.read_checkpoint(comp, ini["label"], ini["time_step"])
+            #  only works if meshes are the same
+        else:
+            print('coucou')
+            value = ini["value"]
+            value = sp.printing.ccode(value)
+            comp = Expression(value, degree=3, t=0)
         comp = interpolate(comp, V.sub(ini["component"]).collapse())
         assign(u_n.sub(ini["component"]), comp)
     components = split(u_n)
