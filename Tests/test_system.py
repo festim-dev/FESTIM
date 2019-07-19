c6cfe19477a6d8e51c031cd89ffd1861ff3bd7d6
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2019-07-19 11:42:43 +0200

    New system test for steady state

--- Tests/test_system.py
+++ Tests/test_system.py
@@ -350,3 +350,123 @@ def test_run_MMS():
         assert output["temperature"][1][1] == 700
         assert output["temperature"][5][1] == 700
         assert error_max_u < tol_u and error_max_v < tol_v
+
+
+def test_run_MMS_steady_state():
+    '''
+    Test function run() for several refinements in steady state
+    '''
+
+    u = 1 + sp.cos(2*fenics.pi*FESTIM.x)
+    v = 1 + sp.cos(2*fenics.pi*FESTIM.x)
+
+    def parameters(h, u, v):
+        size = 1
+        folder = 'Solution_Test'
+        v_0 = 1e13
+        E_t = 1.5
+        T = 700
+        density = 1
+        beta = 6*density
+        alpha = 1
+        n_trap = 1e-1*density
+        E_diff = 0
+        D_0 = 1
+        k_B = 8.6e-5
+        D = D_0 * fenics.exp(-E_diff/k_B/T)
+        v_i = v_0 * fenics.exp(-E_t/k_B/T)
+        v_m = D/alpha/alpha/beta
+        f = sp.diff(u, FESTIM.t) + sp.diff(v, FESTIM.t) - \
+            D * sp.diff(u, FESTIM.x, 2)
+        g = sp.diff(v, FESTIM.t) + v_i*v - v_m * u * (n_trap-v)
+        parameters = {
+            "materials": [
+                {
+                    "alpha": alpha,  # lattice constant ()
+                    "beta": beta,  # number of solute sites pe
+                    "density": density,
+                    "borders": [0, size],
+                    "E_diff": E_diff,
+                    "D_0": D_0,
+                    "id": 1
+                    }
+                    ],
+            "traps": [
+                {
+                    "energy": E_t,
+                    "density": n_trap,
+                    "materials": 1,
+                    "source_term": g
+                }
+                ],
+            "mesh_parameters": {
+                    "initial_number_of_cells": round(size/h),
+                    "size": size,
+                    "refinements": [
+                    ],
+                },
+            "boundary_conditions": [
+                    {
+                        "surface": [1, 2],
+                        "value": u,
+                        "component": 0,
+                        "type": "dc"
+                    },
+                    {
+                        "surface": [1, 2],
+                        "value": v,
+                        "component": 1,
+                        "type": "dc"
+                    }
+                ],
+            "temperature": {
+                    'type': "expression",
+                    'value': T
+                },
+            "source_term": {
+                'value': f
+                },
+            "solving_parameters": {
+                "newton_solver": {
+                    "absolute_tolerance": 1e-10,
+                    "relative_tolerance": 1e-9,
+                    "maximum_iterations": 50,
+                },
+                "type": "solve_stationary"
+                },
+            "exports": {
+                "txt": {
+                    "functions": [],
+                    "times": [],
+                    "labels": [],
+                    "folder": folder
+                },
+                "xdmf": {
+                    "functions": ['solute', '1'],
+                    "labels":  ['solute', '1'],
+                    "folder": folder
+                },
+                "error": [
+                    {
+                        "exact_solution": [u, v],
+                        "norm": 'error_max',
+                        "degree": 4
+                    }
+                ]
+                },
+        }
+        return parameters
+
+    tol_u = 1e-7
+    tol_v = 1e-1
+    sizes = [1/1600, 1/1700]
+    for h in sizes:
+        output = FESTIM.generic_simulation.run(
+            parameters(h, u, v))
+        error_max_u = output["error"][0][1]
+        error_max_v = output["error"][0][2]
+        msg = 'Maximum error on u is:' + str(error_max_u) + '\n \
+            Maximum error on v is:' + str(error_max_v) + '\n \
+            with h = ' + str(h)
+        print(msg)
+        assert error_max_u < tol_u and error_max_v < tol_v
