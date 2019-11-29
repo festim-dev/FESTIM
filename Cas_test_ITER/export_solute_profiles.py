0dbc9d032d216eb0cde748ff0aee492190340ff5
remithewarrior <delaporte.mathurin@laposte.net>
2019-11-29 11:19:59 +0100

    modified csv filename + corrected coordinates

--- Cas_test_ITER/export_solute_profiles.py
+++ Cas_test_ITER/export_solute_profiles.py
@@ -82,8 +82,10 @@ plotOverLine1 = PlotOverLine(Input=solute_m3xdmf,
     Source='High Resolution Line Source')
 
 # init the 'High Resolution Line Source' selected for 'Source'
-plotOverLine1.Source.Point1 = [-0.014000000432133675, -0.013500000350177288, 0.0]
-plotOverLine1.Source.Point2 = [1.6488543198088203e-16, 0.014499999582767487, 0.0]
+point1 = [-1e-9, -0.013500000350177288, 0.0]
+point2 = [-1e-9, 0.014499999582767487, 0.0]
+plotOverLine1.Source.Point1 = point1
+plotOverLine1.Source.Point2 = point2
 
 # show data in view
 plotOverLine1Display = Show(plotOverLine1, renderView1)
@@ -144,17 +146,17 @@ AssignViewToLayout(view=lineChartView1, layout=layout1, hint=0)
 lineChartView1.Update()
 
 # save data
-SaveData('/home/rdelaporte/FESTIM_4_JONATHAN/Cas_test_ITER/results/05_ITER_case_theta_sol2/profile_implantation.csv', proxy=plotOverLine1)
+SaveData('/home/rdelaporte/FESTIM_4_JONATHAN/Cas_test_ITER/results/05_ITER_case_theta_sol2/profile_solute_implantation.csv', proxy=plotOverLine1)
 
 animationScene1.GoToNext()
 
 # save data
-SaveData('/home/rdelaporte/FESTIM_4_JONATHAN/Cas_test_ITER/results/05_ITER_case_theta_sol2/profile_rest.csv', proxy=plotOverLine1)
+SaveData('/home/rdelaporte/FESTIM_4_JONATHAN/Cas_test_ITER/results/05_ITER_case_theta_sol2/profile_solute_rest.csv', proxy=plotOverLine1)
 
 animationScene1.GoToNext()
 
 # save data
-SaveData('/home/rdelaporte/FESTIM_4_JONATHAN/Cas_test_ITER/results/05_ITER_case_theta_sol2/profile_baking.csv', proxy=plotOverLine1)
+SaveData('/home/rdelaporte/FESTIM_4_JONATHAN/Cas_test_ITER/results/05_ITER_case_theta_sol2/profile_solute_baking.csv', proxy=plotOverLine1)
 
 #### saving camera placements for all active views
 
