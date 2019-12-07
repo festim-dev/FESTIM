7fb45783f10a399b5636e3a5d0cf6f7963753d3a
Rémi Delaporte-Mathurin <40028739+RemiTheWarrior@users.noreply.github.com>
2019-12-07 12:21:29 +0100

    new parameters for test case, fixed

--- Cas_test_solubility/cas_test_solubility.py
+++ Cas_test_solubility/cas_test_solubility.py
@@ -27,8 +27,9 @@ def formulation(parameters, extrinsic_traps, solutions, testfunctions,
         E_diff = material['E_diff']
         E_S = material['E_S']
         subdomain = material['id']
-        F += ((S_0*exp(-E_S/k_B/T)*solutions[0]-previous_solutions[0])/dt) *\
-            testfunctions[0]*dx(subdomain)
+        if transient:
+            F += ((S_0*exp(-E_S/k_B/T)*solutions[0]-previous_solutions[0])/dt) *\
+                testfunctions[0]*dx(subdomain)
         F += dot(D_0 * exp(-E_diff/k_B/T) *
                  grad(S_0 * exp(-E_S/k_B/T)*solutions[0]),
                  grad(testfunctions[0]))*dx(subdomain)
@@ -42,8 +43,9 @@ def formulation(parameters, extrinsic_traps, solutions, testfunctions,
 
         energy = trap['energy']
         material = trap['materials']
-        F += ((solutions[i] - previous_solutions[i]) / dt) * \
-            testfunctions[i]*dx
+        if transient:
+            F += ((solutions[i] - previous_solutions[i]) / dt) * \
+                testfunctions[i]*dx
         if type(material) is not list:
             material = [material]
         for subdomain in material:
@@ -73,20 +75,32 @@ def formulation(parameters, extrinsic_traps, solutions, testfunctions,
 
 def run(parameters, log_level=40):
     # Export parameters
-    # FESTIM.export.export_parameters(parameters)
+    try:  # if parameters are in the export key
+        FESTIM.export.export_parameters(parameters)
+    except:
+        pass
 
+    # Check if transient
     transient = True
+    if "type" in parameters["solving_parameters"].keys():
+        if parameters["solving_parameters"]["type"] == "solve_transient":
+            transient = True
+        elif parameters["solving_parameters"]["type"] == "solve_stationary":
+            transient = False
+        elif "type" in parameters["solving_parameters"].keys():
+            raise ValueError(
+                str(parameters["solving_parameters"]["type"]) + ' unkown')
 
     # Declaration of variables
     dt = 0
-    Time = parameters["solving_parameters"]["final_time"]
-    initial_stepsize = parameters["solving_parameters"]["initial_stepsize"]
-    dt = Constant(initial_stepsize)  # time step size
+    if transient:
+        Time = parameters["solving_parameters"]["final_time"]
+        initial_stepsize = parameters["solving_parameters"]["initial_stepsize"]
+        dt = Constant(initial_stepsize)  # time step size
     set_log_level(log_level)
 
     # Mesh and refinement
     mesh = FESTIM.meshing.create_mesh(parameters["mesh_parameters"])
-
     # Define function space for system of concentrations and properties
     V, W = FESTIM.functionspaces_and_functions.create_function_spaces(
         mesh, len(parameters["traps"]))
@@ -98,32 +112,47 @@ def run(parameters, log_level=40):
     dx = Measure('dx', domain=mesh, subdomain_data=volume_markers)
 
     # Create functions for flux computation
-    D_0, E_diff, thermal_cond, G, S =\
-        FESTIM.post_processing.create_flux_functions(
-            mesh, parameters["materials"], volume_markers)
+    D_0, E_diff, thermal_cond, G, S = [None]*5
+    if "derived_quantities" in parameters["exports"]:
+        if "surface_flux" in parameters["exports"]["derived_quantities"]:
+            D_0, E_diff, thermal_cond, G, S =\
+                FESTIM.post_processing.create_flux_functions(
+                    mesh, parameters["materials"], volume_markers)
 
-    # Define variational problem for heat transfers
-    T = Function(W, name="T")
-    vT = TestFunction(W)
-    T_n = sp.printing.ccode(
-        parameters["temperature"]["initial_condition"])
-    T_n = Expression(T_n, degree=2, t=0)
-    T_n = interpolate(T_n, W)
-
-    bcs_T, expressions_bcs_T = \
-        FESTIM.boundary_conditions.define_dirichlet_bcs_T(
-            parameters, W, surface_markers)
-    FT, expressions_FT = \
-        FESTIM.formulations.define_variational_problem_heat_transfers(
-            parameters, [T, vT, T_n], [dx, ds], dt)
+    # Define temperature
+    if parameters["temperature"]["type"] == "expression":
+        T_expr = Expression(
+            sp.printing.ccode(
+                parameters["temperature"]['value']), t=0, degree=2)
+        T = interpolate(T_expr, W)
+    else:
+        # Define variational problem for heat transfers
+        T = Function(W, name="T")
+        T_n = Function(W)
+        vT = TestFunction(W)
+        if parameters["temperature"]["type"] == "solve_transient":
+            T_n = sp.printing.ccode(
+                parameters["temperature"]["initial_condition"])
+            T_n = Expression(T_n, degree=2, t=0)
+            T_n = interpolate(T_n, W)
+        bcs_T, expressions_bcs_T = \
+            FESTIM.boundary_conditions.define_dirichlet_bcs_T(
+                parameters, W, surface_markers)
+        FT, expressions_FT = \
+            FESTIM.formulations.define_variational_problem_heat_transfers(
+                parameters, [T, vT, T_n], [dx, ds], dt)
+        if parameters["temperature"]["type"] == "solve_stationary":
+            print("Solving stationary heat equation")
+            solve(FT == 0, T, bcs_T)
 
     # Define functions
     u, solutions = FESTIM.functionspaces_and_functions.define_functions(V)
-
+    extrinsic_traps = \
+        FESTIM.functionspaces_and_functions.define_functions_extrinsic_traps(
+            W, parameters["traps"])
     testfunctions_concentrations, testfunctions_traps = \
         FESTIM.functionspaces_and_functions.define_test_functions(
-            V, W, 0)
-
+            V, W, len(extrinsic_traps))
     # Create function solubility
     V_DG0 = FunctionSpace(mesh, "DG", 0)
     V_DG1 = FunctionSpace(mesh, "DG", 1)
@@ -139,13 +168,16 @@ def run(parameters, log_level=40):
         E_S.vector()[cell.index()] = value_E_S
     solubility = S_0*exp(-E_S/FESTIM.k_B/T)
     # Initialising the solutions
-    initial_conditions = []
+    if "initial_conditions" in parameters.keys():
+        initial_conditions = parameters["initial_conditions"]
+    else:
+        initial_conditions = []
     u_n, previous_solutions_concentrations = \
         FESTIM.initialise_solutions.initialising_solutions(
             V, initial_conditions)
     previous_solutions_traps = \
         FESTIM.initialise_solutions.initialising_extrinsic_traps(
-            W, 0)
+            W, len(extrinsic_traps))
 
     # Boundary conditions
     print('Defining boundary conditions')
@@ -159,7 +191,7 @@ def run(parameters, log_level=40):
     # Define variational problem H transport
     print('Defining variational problem')
     F, expressions_F = formulation(
-        parameters, [],
+        parameters, extrinsic_traps,
         solutions, testfunctions_concentrations,
         previous_solutions_concentrations, dt, dx, T, transient=transient)
     F += fluxes
@@ -167,54 +199,104 @@ def run(parameters, log_level=40):
     du = TrialFunction(u.function_space())
     J = derivative(F, u, du)  # Define the Jacobian
 
+    # Define variational problem for extrinsic traps
+    if transient:
+        extrinsic_formulations, expressions_form = \
+            FESTIM.formulations.formulation_extrinsic_traps(
+                parameters["traps"], extrinsic_traps, testfunctions_traps,
+                previous_solutions_traps, dt)
+
     # Solution files
     files = []
     append = False
-    files = FESTIM.export.define_xdmf_files(parameters["exports"])
+    if "xdmf" in parameters["exports"].keys():
+        files = FESTIM.export.define_xdmf_files(parameters["exports"])
 
-    derived_quantities_global = \
-        [FESTIM.post_processing.header_derived_quantities(parameters)]
+    derived_quantities_global = []
+    if "derived_quantities" in parameters["exports"].keys():
+        derived_quantities_global = \
+            [FESTIM.post_processing.header_derived_quantities(parameters)]
 
     t = 0  # Initialising time to 0s
     timer = Timer()  # start timer
 
-    #  Time-stepping
-    print('Time stepping...')
-    while t < Time:
+    if transient:
+        #  Time-stepping
+        print('Time stepping...')
+        while t < Time:
+            # Update current time
+            t += float(dt)
+            expressions = FESTIM.helpers.update_expressions(
+                expressions, t)
+            expressions_form = FESTIM.helpers.update_expressions(
+                expressions_form, t)
+            expressions_F = FESTIM.helpers.update_expressions(
+                expressions_F, t)
+            expressions_fluxes = FESTIM.helpers.update_expressions(
+                expressions_fluxes, t)
+            if parameters["temperature"]["type"] != "expression":
+                expressions_FT = FESTIM.helpers.update_expressions(
+                    expressions_FT, t)
+                expressions_bcs_T = FESTIM.helpers.update_expressions(
+                    expressions_bcs_T, t)
 
-        # Update current time
-        t += float(dt)
-        expressions = FESTIM.helpers.update_expressions(
-            expressions, t)
-        expressions_F = FESTIM.helpers.update_expressions(
-            expressions_F, t)
-        expressions_fluxes = FESTIM.helpers.update_expressions(
-            expressions_fluxes, t)
-        expressions_bcs_T = FESTIM.helpers.update_expressions(
-            expressions_bcs_T, t)
+            else:
+                T_expr.t = t
+                T.assign(interpolate(T_expr, W))
 
-        # Display time
-        print(str(round(t/Time*100, 2)) + ' %        ' +
-              str(round(t, 1)) + ' s' +
-              "    Ellapsed time so far: %s s" %
-              round(timer.elapsed()[0], 1),
-              end="\r")
+            # Display time
+            print(str(round(t/Time*100, 2)) + ' %        ' +
+                  str(round(t, 1)) + ' s' +
+                  "    Ellapsed time so far: %s s" %
+                  round(timer.elapsed()[0], 1),
+                  end="\r")
 
-        # Solve heat transfers
-        dT = TrialFunction(T.function_space())
-        JT = derivative(FT, T, dT)  # Define the Jacobian
-        problem = NonlinearVariationalProblem(FT, T, bcs_T, JT)
-        solver = NonlinearVariationalSolver(problem)
-        solver.parameters["newton_solver"]["absolute_tolerance"] = \
-            1e-3
-        solver.parameters["newton_solver"]["relative_tolerance"] = \
-            1e-10
-        solver.solve()
-        T_n.assign(T)
+            # Solve heat transfers
+            if parameters["temperature"]["type"] == "solve_transient":
+                dT = TrialFunction(T.function_space())
+                JT = derivative(FT, T, dT)  # Define the Jacobian
+                problem = NonlinearVariationalProblem(FT, T, bcs_T, JT)
+                solver = NonlinearVariationalSolver(problem)
+                solver.parameters["newton_solver"]["absolute_tolerance"] = \
+                    1e-3
+                solver.parameters["newton_solver"]["relative_tolerance"] = \
+                    1e-10
+                solver.solve()
+                T_n.assign(T)
 
-        # Solve main problem
-        FESTIM.solving.solve_it(
-            F, u, J, bcs, t, dt, parameters["solving_parameters"])
+            # Solve main problem
+            FESTIM.solving.solve_it(
+                F, u, J, bcs, t, dt, parameters["solving_parameters"])
+
+            # Solve extrinsic traps formulation
+            for j in range(len(extrinsic_formulations)):
+                solve(extrinsic_formulations[j] == 0, extrinsic_traps[j], [])
+
+            # Post processing
+            FESTIM.post_processing.run_post_processing(
+                parameters,
+                transient,
+                u, T,
+                [volume_markers, surface_markers],
+                W,
+                t,
+                dt,
+                files,
+                append,
+                [D_0, E_diff, thermal_cond, G, S, solubility],
+                derived_quantities_global)
+            append = True
+            # Update previous solutions
+            u_n.assign(u)
+            for j in range(len(previous_solutions_traps)):
+                previous_solutions_traps[j].assign(extrinsic_traps[j])
+    else:
+        # Solve steady state
+        print('Solving steady state problem...')
+
+        du = TrialFunction(u.function_space())
+        FESTIM.solving.solve_once(
+            F, u, J, bcs, parameters["solving_parameters"])
 
         # Post processing
         FESTIM.post_processing.run_post_processing(
@@ -227,20 +309,30 @@ def run(parameters, log_level=40):
             dt,
             files,
             append,
-            [D_0, E_diff, thermal_cond, G, S, solubility],
+            [D_0, E_diff, thermal_cond, G, S],
             derived_quantities_global)
-        if t in parameters["solving_parameters"]["times"]:
-            append = True
 
-        # Update previous solutions
-        u_n.assign(u)
+    # Store data in output
+    output = dict()  # Final output
 
-    FESTIM.export.write_to_csv(parameters["exports"]["derived_quantities"],
-                               derived_quantities_global)
+    # Compute error
+    if u.function_space().num_sub_spaces() == 0:
+        res = [u]
+    else:
+        res = list(u.split())
+    if "error" in parameters["exports"].keys():
+        error = FESTIM.post_processing.compute_error(
+            parameters["exports"]["error"], t, [*res, T], mesh)
+        output["error"] = error
+    output["parameters"] = parameters
+    output["mesh"] = mesh
+    if "derived_quantities" in parameters["exports"].keys():
+        output["derived_quantities"] = derived_quantities_global
+        FESTIM.export.write_to_csv(parameters["exports"]["derived_quantities"],
+                                   derived_quantities_global)
 
     # End
-    print('\007s')
-    return
+    print('\007')
+    return output
 
-
-run(parameters, log_level=40)  # 20 all convergence info ; 40 only %
+run(parameters, log_level=20)  # 20 all convergence info ; 40 only %
--- Cas_test_solubility/parameters_solubility.py
+++ Cas_test_solubility/parameters_solubility.py
@@ -3,35 +3,31 @@ import sympy as sp
 
 
 # Storage folder
-folder = 'results/05_ITER_case_theta_sol2_99950/'
+folder = 'results/'
 
 # Dict parameters
 parameters = {
     "mesh_parameters": {
         "size": 1,
-        "initial_number_of_cells": 20,
-        "refinements": []
+        "initial_number_of_cells": 200,
+        "refinements": [],
         },
     "materials": [
         {
-            "D_0": 2.9e-7,
-            "E_diff": 0.39,
+            "D_0": 1,
+            "E_diff": 0,
             "S_0": 1,  # at/m3.Pa0.5 (from Grislia 2015)
-            "E_S": 0.34,  # eV
-            #"S_0": 1.0,  # case without solubility
-            #"E_S": 0.0,  # case without solubility
+            "E_S": 0,  # eV
             "alpha": 1,  # 1.0000 coef H/D/T
             "beta": 1,
             "borders": [0, 0.5],
             "id": 1,
         },
         {
-            "D_0": 6.6e-7,
-            "E_diff": 0.387,
-            "S_0": 3.12e28,  # at/m3.Pa0.5 (from ITER)
-            "E_S": 0.572,  # eV
-            #"S_0": 1.0,  # case without solubility
-            #"E_S": 0.0,  # case without solubility
+            "D_0": 1,
+            "E_diff": 0,
+            "S_0": 1,  # at/m3.Pa0.5 (from ITER)
+            "E_S": 0,  # eV
             "alpha": 1,
             "beta": 1,
             "borders": [0.5, 1],
@@ -43,12 +39,12 @@ parameters = {
     "boundary_conditions": [
         {
             "type": "dc",
-            "surface": 2,
+            "surface": 1,
             "value": 4
         },
         {
             "type": "dc",
-            "surface": 1,
+            "surface": 2,
             "value": 4
         },
         ],
@@ -57,27 +53,28 @@ parameters = {
         "value": 300
         },
     "solving_parameters": {
-        "final_time": 1,
-        "times": [],
-        "initial_stepsize": 0.1,
+        "type": "solve_stationary",
+        "final_time": 0.2,
+        "times": [15],
+        "initial_stepsize": 0.001,
         "adaptive_stepsize": {
             "stepsize_change_ratio": 1,
-            "t_stop": 1,
+            "t_stop": 111,
             "stepsize_stop_max": 0.1,
             "dt_min": 1e-8,
             },
         "newton_solver": {
-            "absolute_tolerance": 1e10,
+            "absolute_tolerance": 1e-10,
             "relative_tolerance": 1e-9,
             "maximum_iterations": 10,
         }
         },
     "exports": {
         "xdmf": {
-            "functions": ['0'],
-            "labels": ['theta'],
+            "functions": ['0', 'retention'],
+            "labels": ['theta', 'solute'],
             "folder": folder,
-            "all_timesteps": False,
+            "all_timesteps": True,
         },
     }
 }
