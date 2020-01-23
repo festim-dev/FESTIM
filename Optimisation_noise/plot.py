527f7d52e4b5e71d9a84a05fd145a6e6cbed0930
remithewarrior <delaporte.mathurin@laposte.net>
2020-01-23 14:58:17 +0100

    a few changes

--- Optimisation_noise/plot.py
+++ Optimisation_noise/plot.py
@@ -7,22 +7,39 @@ from matplotlib import rc
 import numpy as np
 import csv
 
-T = []
-d = []
-t = []
+T_exp = []
+d_exp = []
 
-with open('experimental data/data.csv', 'r') as csvfile:
+with open('2e+17/ref.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        if 't (s)' not in row:
+        if 't(s)' not in row:
 
-            T.append(float(row[1]))
-            d.append(float(row[2]))
-
-
-plt.plot(T, d, label="exp", linestyle='dashed')
+            T_exp.append(float(row[0]))
+            d_exp.append(float(row[1]))
 
 
+plt.scatter(T_exp, d_exp, label="exp", s=10)
+
+T_sim = []
+ret = []
+t = []
+with open('2e+17/derived_quantities/0.872753866921_0.00132263650983_1.10346176556_0.00048996148925.csv', 'r') as csvfile:
+    plots = csv.reader(csvfile, delimiter=',')
+    for row in plots:
+        if 't(s)' not in row:
+            if float(row[0]) > 450:
+                t.append(float(row[0]))
+                T_sim.append(float(row[1]))
+                ret.append(float(row[2]))
+
+d = []
+for i in range(len(ret)-1):
+    d.append(-(ret[i+1] - ret[i])/(t[i+1] - t[i]))
+T_sim.pop(0)
+plt.plot(T_sim, d, label="sim", linestyle='dashed', color='orange')
+
+# plt.xlim(300, 700)
 plt.xlabel('T(K)')
 plt.ylabel(r'desorption(D/m$ ^{2}$/s)')
 plt.title('TDS')
