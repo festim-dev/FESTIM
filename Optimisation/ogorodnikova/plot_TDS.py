2d385f307f2b7c1da83c1810ecb66f57f50cf7d4
remithewarrior <delaporte.mathurin@laposte.net>
2020-02-10 11:00:32 +0100

    Added transparent lines

--- Optimisation/ogorodnikova/plot_TDS.py
+++ Optimisation/ogorodnikova/plot_TDS.py
@@ -19,54 +19,7 @@ with open('ref.csv', 'r') as csvfile:
             d_exp.append(float(row[1]))
 
 
-plt.scatter(T_exp, d_exp, label="exp", s=10)
-
-# T_sim = []
-# ret = []
-# t = []
-# with open('cost_function_non_pondered_average/derived_quantities/0.86_0.0015.csv', 'r') as csvfile:
-#     plots = csv.reader(csvfile, delimiter=',')
-#     for row in plots:
-#         if 't(s)' not in row:
-#             if float(row[0]) > 450:
-#                 t.append(float(row[0]))
-#                 T_sim.append(float(row[1]))
-#                 ret.append(float(row[2]))
-
-# d = []
-# for i in range(len(ret)-1):
-#     d.append(-(ret[i+1] - ret[i])/(t[i+1] - t[i]))
-# T_sim.pop(0)
-# plt.plot(T_sim, d, label="sim unpondered", linestyle='dashed', color='orange')
-
-
-T_sim = []
-ret = []
-t = []
-with open('optimisation_5D/derived_quantities/0.849199704993_1.08753443745_0.967984862256_5.24800051466_1.37441734167.csv', 'r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
-    for row in plots:
-        if 't(s)' not in row:
-            if float(row[0]) > 450:
-                t.append(float(row[0]))
-                T_sim.append(float(row[1]))
-                ret.append(float(row[2]))
-
-d = []
-for i in range(len(ret)-1):
-    d.append(-(ret[i+1] - ret[i])/(t[i+1] - t[i]))
-T_sim.pop(0)
-plt.plot(T_sim, d, label="sim pondered", linestyle='dashed', color='grey')
-
-# plt.xlim(300, 700)
-plt.xlabel('T(K)')
-plt.ylabel(r'desorption(D/m$ ^{2}$/s)')
-plt.minorticks_on()
-plt.grid(which='minor', alpha=0.3)
-plt.grid(which='major', alpha=0.7)
-# plt.title('TDS')
-plt.legend()
-plt.show()
+plt.scatter(T_exp, d_exp, label="Exp", s=10)
 
 ## Plot TDS with each trap
 T_sim = []
@@ -76,7 +29,7 @@ trap2 = []
 trap3 = []
 solute = []
 t = []
-with open('optimisation_5D/derived_quantities/0.849199704993_1.08753443745_0.967984862256_5.24800051466_1.37441734167.csv', 'r') as csvfile:
+with open('optimisation_trap3/derived_quantities/1.41310524454_4.82892447131_8.69682813914.csv', 'r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
         if 't(s)' not in row:
@@ -101,13 +54,17 @@ T_sim.pop(0)
 for i in range(0, len(derivatives)):
     if i != 0:
         style = "dashed"
-        plt.fill_between(T_sim, 0, derivatives[i], facecolor='grey', alpha=0.3)
+        width = 0.8
+        plt.fill_between(T_sim, 0, derivatives[i], facecolor='grey', alpha=0.1)
     else:
         style = "-"
-    plt.plot(T_sim, derivatives[i], linestyle=style, label=legends[i])
-plt.scatter(T_exp, d_exp, label="exp", s=10)
+        width = 1.7
+    if i != 1:
+        plt.plot(T_sim, derivatives[i], linewidth=width, linestyle=style, label=legends[i], alpha=1)
+
+
 plt.xlabel('T(K)')
-plt.ylabel(r'time derivative (m$^{-2}$ s$^{-1}$)')
+plt.ylabel(r'Time derivative (m$^{-2}$ s$^{-1}$)')
 plt.minorticks_on()
 plt.grid(which='minor', alpha=0.3)
 plt.grid(which='major', alpha=0.7)
