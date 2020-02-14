2de7f0a52b534aa6ee7d74469044c178813bb066
remithewarrior <delaporte.mathurin@laposte.net>
2020-02-14 16:50:50 +0100

    Added a new dict containing descriptions of keys + a help function

--- FESTIM/helpers.py
+++ FESTIM/helpers.py
@@ -46,3 +46,94 @@ field_types = [
     "retention",
     "T"
 ]
+parameters = {
+    "materials": {
+        "E_D": {
+            "description": "Diffusion coefficient activation energy",
+            "unit": "eV"
+            },
+        "D_0": {
+            "description": "Diffusion coefficient pre exponential factor",
+            "unit": "(m^2/s)"
+            },
+        "S_0": {
+            "description": "Solubility coefficient pre exponential factor",
+            "unit": "(m^2/s)"
+            },
+        "E_S": {
+            "description": "Solubility coefficient activation energy",
+            "unit": "eV"
+            },
+        "borders": {
+            "description": "1D only: delimitations of the domain. \
+            Exemple: [0, 0.5]",
+            "unit": "m"
+            },
+        "thermal_cond": {
+            "description": "required if heat equation is solved or if thermal\
+                 flux is computed. thermal conductivity",
+            "unit": "W/m/K"
+            },
+        "heat_capacity": {
+            "description": "required if heat equation is solved. \
+                Heat capacity",
+            "unit": "J/K/kg"
+            },
+        "rho": {
+            "description": "required if heat equation is solved. \
+                Density",
+            "unit": "kg/m^3"
+            },
+        "id": {
+            "description": "id of the domain. If a marked mesh is given,\
+                id must correspond to the volume_markers_file",
+            "unit": "None"
+            },
+    }
+    "traps": {
+        "E_k": {
+            "description": "Trapping rate activation energy",
+            "unit": "eV"
+            },
+        "k_0": {
+            "description": "Trapping rate pre-exponential factor",
+            "unit": "m^3/s"
+            },
+        "E_p": {
+            "description": "Detrapping rate activation energy",
+            "unit": "eV"
+            },
+        "p_0": {
+            "description": "Detrapping rate pre-exponential factor",
+            "unit": "s^-1"
+            },
+        "density": {
+            "description": "denisty of the trap. Can be float or an expression\
+                 (ex: (1 + FESTIM.x)*FESTIM.t<100)",
+            "unit": "m^-3"
+            },
+        "materials": {
+            "description": "ids of the domains where the trap is present.\
+                 Can be int or list of int",
+            "unit": "None"
+            },
+        "source_term": {
+            "description": "Volumetric source term for the trapped population",
+            "unit": "m^-3.s^-1"
+            },
+        },
+    }
+
+
+def help_key(key):
+    def find_dict(key, var, res=[]):
+        if key in var:
+            res.append(var[key])
+        else:
+            for k in var.keys():
+                if type(var[k]) == dict:
+                    find_dict(key, var=var[k], res=res)
+        return res
+    d = find_dict(key, parameters)
+    for k in d:
+        print(k)
