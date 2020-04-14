90065ddd9f720fc23c977cd279ea1adcffab764b
J. Mougenot <45436052+mougenj@users.noreply.github.com>
2020-04-14 12:05:09 +0200

    MAJ sim_monoblock_1D calqée sur TMAP
    Valeurs qui collent au cas test ITER
    Attention il y avait une inversion géométrique Cu/CrZrCu
    Reste à régler les CL de H (141) au top et CI de temp (180

--- monoblock_1D/sim_monoblock_1D.py
+++ monoblock_1D/sim_monoblock_1D.py
@@ -2,9 +2,9 @@ from context import FESTIM
 from FESTIM.generic_simulation import run
 
 # atom_density  =  density(g/m3)*Na(/mol)/M(g/mol)
-atom_density_W = 6.3222e28  # atomic density m^-3
-atom_density_Cu = 8.4912e28  # atomic density m^-3
-atom_density_CuCrZr = 2.6096e28  # atomic density m^-3
+atom_density_W = 6.28e28 #6.3222e28  # atomic density m^-3
+atom_density_Cu = 8.43e28 # 8.4912e28  # atomic density m^-3
+atom_density_CuCrZr = 8.43e28 #2.6096e28  # atomic density m^-3
 
 # IDs for edges and surfaces (must be the same as in xdmf files)
 id_W = 8
@@ -17,111 +17,120 @@ id_coolant_surf = 2  #10
 
 folder = "Solution_high_mesh_mu"
 
-
+# OK TMAP
 def rhoCp_W(T):
     return 5.15356e-6*T**3-8.30703e-2*T**2+5.98312e2*T+2.48160e6
 
-
+# OK TMAP
 def thermal_cond_W(T):
     return -7.84154e-9*T**3+5.03006e-5*T**2-1.07335e-1*T+1.75214e2
 
-
+# OK TMAP
 def rhoCp_Cu(T):
     return 1.68402e-4*T**3+6.14079e-2*T**2+4.67353e2*T+3.45899e6
 
-
+# OK TMAP
 def thermal_cond_Cu(T):
     return -7.84154e-9*T**3+5.03006e-5*T**2-1.07335e-1*T+1.75214e2
 
-
+# OK TMAP
 def rhoCp_CuCrZr(T):
     return -1.79134e-4*T**3+1.51383e-1*T**2+6.22091e2*T+3.46007e6
 
-
+# OK TMAP
 def thermal_cond_CuCrZr(T):
     return 5.25780e-7*T**3-6.45110e-4*T**2+2.57678e-01*T+3.12969e2
 
-size = 8.5e-3
+size = 8.5e-3 # OK TMAP
 parameters = {
     "mesh_parameters": {
         "size": size,
-        "initial_number_of_cells": 6000,
+        "initial_number_of_cells": 60000,
     },
     "materials": [
         {
             # Tungsten
-            "D_0": 2.9e-7,
-            "E_D": 0.39,
-            "S_0": atom_density_W*1.3e-4,  # at/m3.Pa0.5 (from Grislia 2015)
-            "E_S": 0.34,  # eV
+            "D_0": 2.9e-7*0.8165, # OK TMAP eq(6)
+            "E_D": 0.39,  # OK TMAP eq(6)
+            "S_0": 1.87e24,  # OK MUYI mais attention units ? at/m3.Pa0.5 
+            "E_S": 1.04,  # OK MUYI (eV)
             #"S_0": 1.0,  # case without solubility
             #"E_S": 0.0,  # case without solubility
             "thermal_cond": thermal_cond_W,
             "heat_capacity": 1,
             "rho": rhoCp_W,
-            "borders": [0, 6e-3],
+            "borders": [0, 6e-3], # OK TMAP
             "id": id_W,
         },
         {
             # Cu
-            "D_0": 6.6e-7,
-            "E_D": 0.387,
-            "S_0": 3.12e28,  # at/m3.Pa0.5 (from ITER)
-            "E_S": 0.572,  # eV
+            "D_0": 6.6e-7,  # OK TMAP eq(9) 
+            "E_D": 0.387,  # OK TMAP eq(9) 
+            "S_0": 3.14e24,  # OK TMAP eq(8) at/m3.Pa0.5
+            "E_S": 0.572,  # OK TMAP eq(8) eV
             #"S_0": 1.0,  # case without solubility
             #"E_S": 0.0,  # case without solubility
             "thermal_cond": thermal_cond_Cu,
             "heat_capacity": 1,
             "rho": rhoCp_Cu,
-            "borders": [6e-3, 7e-3],
+            "borders": [7e-3, size], # OK TMAP
             "id": id_Cu,
         },
         {
             # CuCrZr
-            "D_0": 3.92e-7,
-            "E_D": 0.418,
-            "S_0": 4.28e23,  # at/m3.Pa0.5 (from ITER)
-            "E_S": 0.387,  # eV
+            "D_0": 3.92e-7, # OK TMAP eq(13)
+            "E_D": 0.418, # OK TMAP eq(13)
+            "S_0": 4.28e23,  # OK TMAP eq(12) at/m3.Pa0.5 (from ITER)
+            "E_S": 0.387,  # OK TMAP eq(12) eV
             #"S_0": 1.0,  # case without solubility
             #"E_S": 0.0,  # case without solubility
             "thermal_cond": thermal_cond_CuCrZr,
             "heat_capacity": 1,
             "rho": rhoCp_CuCrZr,
-            "borders": [7e-3, size],
+            "borders": [6e-3, 7e-3], # OK TMAP
             "id": id_CuCrZr,
         },
         ],
         "traps": [
         {
-            "E_k": 0.39,
-            "k_0": 2.9e-7/(1.1e-10**2)*0.8165/atom_density_W,
-            "E_p": 1,
-            "p_0": 8.4e12,
-            "density": 5e-4*atom_density_W,
+            "E_k": 0.39, # OK TMAP eq (3)
+            "k_0": 2.9e12*0.8165/atom_density_W, # OK TMAP eq (3)
+            #"k_0": 2.9e-7/(1.1e-10**2)*0.8165/atom_density_W, 
+            "E_p": 1.2, # OK TMAP eq (4)
+            "p_0": 8.4e12, # OK TMAP eq (4)
+            "density": 5e-4*atom_density_W, # OK TMAP ligne (72)
             "materials": [id_W]
         },
-        # {
-        #     "E_k": 0.39,
-        #     "k_0": 2.9e-7/(1.1e-10**2)*0.8165/atom_density_W,
-        #     "E_p": 1.4,
-        #     "p_0": 8.4e12,
-        #     "density": 5e-3*atom_density_W*(FESTIM.y > 0.014499),
-        #     "materials": [id_W]
-        # },
         {
-            "E_k": 0.387,
-            "k_0": 6.6e-7/(3.61e-10**2)/atom_density_Cu,
-            "E_p": 0.5,
-            "p_0": 7.98e13,
-            "density": 5e-5*atom_density_Cu,
+             "E_k": 0.39, # OK TMAP eq (3)
+             "k_0": 2.9e12*0.8165/atom_density_W, # OK TMAP eq (3)
+             "E_p": 1.4, # OK TMAP eq (5)
+             "p_0": 8.4e12, # OK TMAP eq (5)
+             "density": 5e-3*atom_density_W*(FESTIM.y < 1e-6), # OK TMAP ligne (75)
+             "materials": [id_W]
+         },
+        {
+            "E_k": 0.387, # OK TMAP eq (10)
+            "k_0": 6.6e-7/(3.61e-10**2)/atom_density_Cu, # OK TMAP eq (10)
+            "E_p": 0.5, # OK TMAP eq (11)
+            "p_0": 7.98e13, # OK TMAP eq (11)
+            "density": 5e-5*atom_density_Cu, # OK TMAP ligne (89)
             "materials": [id_Cu]
         },
         {
-            "E_k": 0.418,
-            "k_0": 3.92e-7/(3.61e-10**2)/atom_density_CuCrZr,
-            "E_p": 0.85,
-            "p_0": 7.98e13,
-            "density": 5e-5*atom_density_CuCrZr,
+            "E_k": 0.418, # OK TMAP eq (15)
+            "k_0": 3.92e-7/(3.61e-10**2)/atom_density_CuCrZr,  # OK TMAP eq (15)
+            "E_p": 0.5,  # OK TMAP eq (11)
+            "p_0": 7.98e13,  # OK TMAP eq (11)
+            "density": 5e-5*atom_density_CuCrZr, # OK TMAP ligne (103)
+            "materials": [id_CuCrZr]
+        },
+                {
+            "E_k": 0.418, # OK TMAP eq (15)
+            "k_0": 3.92e-7/(3.61e-10**2)/atom_density_CuCrZr,  # OK TMAP eq (15)
+            "E_p": 0.83,  # OK TMAP eq (16)
+            "p_0": 7.98e13,  # OK TMAP eq (16)
+            "density": 0.04*atom_density_CuCrZr, # OK TMAP ligne (104)
             "materials": [id_CuCrZr]
         },
         ],
@@ -129,13 +138,13 @@ parameters = {
         {
             "type": "dc",
             "surfaces": id_top_surf,
-            "value": 5e22*(FESTIM.t>20) + (5e22*(FESTIM.t/20))*(FESTIM.t<20)
+            "value": 5e23 (flux) + recombinaison (kr0=3.2e-15 : Ekr=-1.16 ) + pression (ligne 168)
         },
         {
             "type": "recomb",
             "surfaces": id_coolant_surf,
-            "Kr_0": 2.9e-14,
-            "E_Kr": 1.92,
+            "Kr_0": 2.9e-14, # OK TMAP eq(13)
+            "E_Kr": 1.92, # OK TMAP eq(14)
             "order": 2,
         },
         # {
@@ -156,22 +165,24 @@ parameters = {
         "boundary_conditions": [
             {
                 "type": "dc",
-                "value": 1200,
+                "value": 1200, # OK TMAP phase 1 tab(3) ligne 172/173
                 "surfaces": id_top_surf
             },
             {
                 "type": "dc",
-                "value": 370,
+                "value": 373, # OK TMAP phase 1 tab(3) ligne 182/183
                 "surfaces": id_coolant_surf
             }
             ],
         "source_term": [
         ],
-        "initial_condition": 273.15+200
+        # en toute rigueur il faudrait une rampe tempd ligne 37 ; 47 ; 57
+        #"initial_condition": 273.15+200  
+        "initial_condition": (1200-373)*(size-FESTIM.y)/size +373 
         },
     "solving_parameters": {
         # "type": "solve_stationary",
-        "final_time": 1e7,
+        "final_time": 2400000, # OK TMAP ligne 183
         "initial_stepsize": 100000,
         "adaptive_stepsize": {
             "stepsize_change_ratio": 1.05,
@@ -179,7 +190,7 @@ parameters = {
             "stepsize_stop_max": 1e7,
             "dt_min": 1e-8,
             },
-        "times": [],
+        #"times": [],
         "newton_solver": {
             "absolute_tolerance": 1e11,
             "relative_tolerance": 1e-10,
@@ -187,11 +198,17 @@ parameters = {
         }
         },
     "exports": {
-        "xdmf": {
-            "functions": ['T', '0', '1', '2', '3', 'retention'],
-            "labels": ['T', 'solute', '1', '2', '3', 'retention'],
+        "txt": {
+            "times": [1e7],
+            "functions": ['T','0', '1', '2', '3', 'retention'],
+            "labels": ['T','0','1', '2', '3', 'retention'],
             "folder": folder,
-            "all_timesteps": True,
+        },
+         "xdmf": {
+             "functions": ['T', '0', '1', '2', '3', 'retention'],
+             "labels": ['T', 'solute', '1', '2', '3', 'retention'],
+             "folder": folder,
+             "all_timesteps": True,
         },
         "derived_quantities": {
             "total_volume": [
