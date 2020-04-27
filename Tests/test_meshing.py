be1b3d520759482a4c094b9b1c65466334418c5b
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-04-27 09:35:26 +0200

    renamed function + added 1 integration test

--- Tests/test_meshing.py
+++ Tests/test_meshing.py
@@ -234,7 +234,7 @@ def test_subdomains_inbuilt():
         assert mf_facets[cell] == mf_facets_2[cell]
 
 
-def test_generate_mesh_from_coordinates():
+def test_generate_mesh_from_vertices():
     '''
     Test the function generate_mesh_from_vertices
     '''
@@ -245,7 +245,7 @@ def test_generate_mesh_from_coordinates():
     assert mesh.num_cells() == len(points) - 1
 
 
-def test_create_mesh_coordinates():
+def test_create_mesh_vertices():
     '''
     Test the function create_mesh with vertices key
     '''
@@ -261,3 +261,43 @@ def test_create_mesh_coordinates():
         for v in fenics.vertices(cell):
             print(v.point().x())
             assert v.point().x() in points
+
+
+def test_integration_mesh_from_vertices_subdomains():
+    '''
+    Integration test for meshing and subdomain 1D
+    when parsing a list of vertices
+    Checks that the cells are marked correctly
+    '''
+    points = [0, 1, 2, 5, 12, 24]
+    mesh_parameters = {
+        "vertices": points
+    }
+    materials = [
+        {
+            "borders": [0, 2],
+            "id": 1
+        },
+        {
+            "borders": [2, 24],
+            "id": 2
+        }
+    ]
+    parameters = {
+        "mesh_parameters": mesh_parameters,
+        "materials": materials
+    }
+    mesh = meshing.create_mesh(mesh_parameters)
+    vm, sm = meshing.subdomains(mesh, parameters)
+
+    # Testing
+    for cell in fenics.cells(mesh):
+        if cell.midpoint().x() < 2:
+            assert vm[cell] == 1
+        elif cell.midpoint().x() > 2:
+            assert vm[cell] == 2
+    for facet in fenics.facets(mesh):
+        if facet.midpoint().x() == 0:
+            assert sm[facet] == 1
+        if facet.midpoint().x() == max(points):
+            assert sm[facet] == 2
