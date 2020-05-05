c6efe12b83ff7ddf0c52e883ed7dd234e55e23bc
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-05-05 11:05:40 +0200

    added unweighted

--- Optimisation/Hollingsworth eurofer/plot.py
+++ Optimisation/Hollingsworth eurofer/plot.py
@@ -17,6 +17,7 @@ tds_time = (1000 - 300)/ramp
 
 samples = ["S7", "S14", "S10"]
 legends = ["0 dpa", "0.01 dpa", "0.1 dpa"]
+colors = ["tab:blue", "tab:orange", "tab:green"]
 for i in range(0, 3):
     T_exp = []
     d_exp = []
@@ -26,7 +27,28 @@ for i in range(0, 3):
             T_exp.append(float(row[0]))
             d_exp.append(float(row[1]))
 
-    plt.scatter(T_exp, d_exp, s=10)
+    plt.scatter(T_exp, d_exp, s=10, color=colors[i])
+
+    T_sim = []
+    solute = []
+    ret = []
+    t = []
+    trap1 = []
+    with open(samples[i] + '/Results/derived_quantities/last_unpondered.csv', 'r') as csvfile:
+        plots = csv.reader(csvfile, delimiter=',')
+        for row in plots:
+            if 't(s)' not in row:
+                if float(row[0]) >= implantation_time + resting_time:
+                    t.append(float(row[0]))
+                    T_sim.append(float(row[1]))
+                    ret.append(float(row[2]))
+                    solute.append(float(row[3]))
+                    trap1.append(float(row[4]))
+    flux = []
+    for j in range(len(ret)-1):
+        flux.append(-(ret[j+1] - ret[j])/(t[j+1] - t[j]))
+    T_sim.pop(0)
+    plt.plot(T_sim, flux, alpha=1, color=colors[i], linestyle="dashed")
 
     T_sim = []
     solute = []
@@ -47,11 +69,12 @@ for i in range(0, 3):
     for j in range(len(ret)-1):
         flux.append(-(ret[j+1] - ret[j])/(t[j+1] - t[j]))
     T_sim.pop(0)
-    plt.plot(T_sim, flux, label=legends[i], alpha=1)
+    plt.plot(T_sim, flux, label=legends[i], alpha=1, color=colors[i])
+
 plt.minorticks_on()
 plt.grid(which='minor', alpha=0.3)
 plt.grid(which='major', alpha=0.7)
-# plt.xlim(300, 800)
+plt.xlim(right=900)
 plt.xlabel('T (K)')
 plt.ylabel(r'Desorption flux (D m$^{-2}$ s $^{-1}$)')
 # plt.title('TDS')
