68a31ddfa70c8372365740eb34ec401678def188
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-05-11 11:52:28 +0200

    added docstrings

--- FESTIM/solving.py
+++ FESTIM/solving.py
@@ -3,6 +3,21 @@ from fenics import *
 
 
 def solve_it(F, u, J, bcs, t, dt, solving_parameters):
+    """Solves the problem during time stepping.
+
+    Arguments:
+        F {fenics.Form()} -- Formulation to be solved
+        u {fenics.Function()} -- Function for concentrations
+        J {fenics.Function()} -- Jacobian
+        bcs {list} -- contains boundary conditions (fenics.DirichletBC())
+        t {float} -- time
+        dt {fenics.Constant()} -- stepsize
+        solving_parameters {dict} -- solving parameters
+
+    Returns:
+        fenics.Function() -- function for concentrations
+        fenics.Constant() -- stepsize
+    """
     converged = False
     u_ = Function(u.function_space())
     u_.assign(u)
@@ -29,6 +44,20 @@ def solve_it(F, u, J, bcs, t, dt, solving_parameters):
 
 
 def solve_once(F, u, J, bcs, solving_parameters):
+    """Solves non linear problem
+
+    Arguments:
+        F {fenics.Form()} -- Formulation to be solved
+        u {fenics.Function()} -- Function for concentrations
+        J {fenics.Function()} -- Jacobian
+        bcs {list} -- contains boundary conditions (fenics.DirichletBC())
+        solving_parameters {dict} -- solving parameters
+
+    Returns:
+        fenics.Function() -- function for concentrations
+        int -- number of iterations for reaching convergence
+        bool -- True if converged, else False
+    """
     problem = NonlinearVariationalProblem(F, u, bcs, J)
     solver = NonlinearVariationalSolver(problem)
     solver.parameters["newton_solver"]["error_on_nonconvergence"] = False
@@ -45,21 +74,19 @@ def solve_once(F, u, J, bcs, solving_parameters):
 
 def adaptive_stepsize(nb_it, converged, dt, dt_min,
                       stepsize_change_ratio, t):
-    '''
-    Adapts the stepsize as function of the number of iterations of the
+    """Adapts the stepsize as function of the number of iterations of the
     solver.
+
     Arguments:
-    - solver : FEniCS NonlinearVariationalSolver
-    - nb_it : int, number of iterations
-    - dt : Constant(), fenics object
-    - dt_min : float, stepsize minimum value
-    - stepsize_change_ration : float, stepsize change ratio
-    - t : float, time
-    - t_stop : float, time where adaptive time step stops
-    - stepsize_stop_max : float, maximum stepsize after stop
-    Returns:
-    - dt : Constant(), fenics object
-    '''
+        nb_it {int} -- number of iterations
+        converged {bool} -- True if converged, else False
+        dt {fenics.Constant()} -- stepsize
+        dt_min {float} -- minimum stepsize
+        stepsize_change_ratio {float} -- stepsize change ratio for adaptive
+            stepsize
+        t {float} -- time
+    """
+
     if converged is False:
         dt.assign(float(dt)/stepsize_change_ratio)
         if float(dt) < dt_min:
