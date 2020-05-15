f7398496d1d8b10e34b1dd6d0a96724ae46600e1
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-05-15 14:04:10 +0200

    other algs are transparent

--- Optimisation/ogorodnikova/cost_function_pondered_average/find_minimum.py
+++ Optimisation/ogorodnikova/cost_function_pondered_average/find_minimum.py
@@ -2,6 +2,7 @@ from scipy.interpolate import interp2d, griddata
 from scipy.optimize import minimize
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import to_rgb
 import time
 from cycler import cycler
 E = []
@@ -36,36 +37,35 @@ x0_E_sorted = [x for _,x in sorted(zip(norm_to_optimal, x0_E))]
 x0_n_sorted = [x for _,x in sorted(zip(norm_to_optimal, x0_n))]
 
 
-results = []
+results = [[] for i in range(4)]
 for i in range(0, N):
     x0 = [x0_E_sorted[i], x0_n_sorted[i]]
-    results.append([])
 
     res = minimize(fun, x0, method="Nelder-Mead", options={"disp": False})
     if res.x[0] < 0.87 and res.x[0] > 0.85 and res.x[1] < 1.25 and res.x[1] > 1.22:
-        results[-1].append(res.nfev)
+        results[0].append(res.nfev)
     else:
-        results[-1].append(0)
+        results[0].append(0)
 
     res = minimize(fun, x0, method="Powell", options={"disp": False})
     if res.x[0] < 0.87 and res.x[0] > 0.85 and res.x[1] < 1.25 and res.x[1] > 1.22:
-        results[-1].append(res.nfev)
+        results[1].append(res.nfev)
     else:
-        results[-1].append(0)
+        results[1].append(0)
 
     i = 0
     res = minimize(fun, x0, method="TNC", options={"disp": False, 'gtol':1e-5})
     if res.x[0] < 0.87 and res.x[0] > 0.85 and res.x[1] < 1.25 and res.x[1] > 1.22:
-        results[-1].append(i)
+        results[2].append(i)
     else:
-        results[-1].append(0)
+        results[2].append(0)
 
     i = 0
     res = minimize(fun, x0, method="CG", options={"disp": False, 'gtol':1e-5})
     if res.x[0] < 0.87 and res.x[0] > 0.85 and res.x[1] < 1.25 and res.x[1] > 1.22:
-        results[-1].append(i)
+        results[3].append(i)
     else:
-        results[-1].append(0)
+        results[3].append(0)
     # results[-1][1].append(res.x)
 
 
@@ -73,8 +73,7 @@ print(results)
 
 labels = ['Nelder-Mead', 'Powell', "TNC", 'CG']
 results = np.asarray(results)
-x = np.arange(len(labels))
-gap = 0.05
+gap = 0.1
 x = np.asarray([i*(gap+1) for i in range(len(labels))])
 width = (len(labels) - 7*gap)/(len(labels)*N)  # the width of the bars
 print(width)
@@ -82,9 +81,16 @@ colors = ['#E89005', '#EC7505', '#D95D39', '#D84A05', '#B8141C']
 custom_cycler = cycler(color=colors)
 fig, ax = plt.subplots()
 ax.set_prop_cycle(custom_cycler)
-for i in range(N):
-    color = colors[int(i*5/N)]
-    ax.bar(x + i*width - (len(results)-1)/2*width, results[i], width, color=color,  edgecolor=color, label=str(i), align='center')
+j = 0
+for res in results:
+    for i in range(len(res)):
+        color = colors[int(i*5/N)]
+        if j == 0:
+            alpha = 1
+        else:
+            alpha = 0.3
+        ax.bar(x[j] + i*width - (len(res)-1)/2*width, res[i], width, color=(*to_rgb(color), alpha),  edgecolor=(*to_rgb(color), alpha), label=str(i), align='center')
+    j += 1
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Number of function evaluation')
