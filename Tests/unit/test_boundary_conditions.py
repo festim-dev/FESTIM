d7c79f190c4cc8ec2964f406438e6e8f61120117
RemiTheWarrior <delaporte.mathurin@laposte.net>
2020-07-02 13:29:33 +0200

    Added 3 UTs for testing apply_boundary_conditions()

--- Tests/unit/test_boundary_conditions.py
+++ Tests/unit/test_boundary_conditions.py
@@ -226,3 +226,226 @@ def test_apply_boundary_conditions_fail():
         apply_boundary_conditions(
             {"boundary_conditions": boundary_conditions}, V,
             markers, T)
+
+
+def test_bc_recomb():
+    """Test the function boundary_conditions.apply_boundary_conditions
+    with bc type dc_recomb
+    """
+    phi = 3
+    R_p = 5
+    D_0 = 2
+    E_D = 0.5
+    K_0 = 2
+    E_K = 0.35
+    parameters = {
+        "materials": [
+            {
+                "id": 1
+                },
+            {
+                "id": 2
+                }
+        ],
+        "boundary_conditions": [
+            {
+                "type": "dc_recomb",
+                "implanted_flux": phi,
+                "implantation_depth": R_p,
+                "D_0": D_0,
+                "E_D": E_D,
+                "K_0": K_0,
+                "E_K": E_K,
+                "surfaces": [1, 2]
+            },
+        ]
+    }
+
+    mesh = fenics.UnitSquareMesh(4, 4)
+    V = fenics.FunctionSpace(mesh, 'P', 1)
+    u = fenics.Function(V)
+    temp = fenics.Expression("200 + (x[0] + 1)*t", t=1, degree=1)
+
+    sm = fenics.MeshFunction("size_t", mesh, 1, 0)
+    left = fenics.CompiledSubDomain('x[0] < 0.0001')
+    left.mark(sm, 1)
+    right = fenics.CompiledSubDomain('x[0] > 0.99999999')
+    right.mark(sm, 2)
+
+    bcs, expressions = \
+        apply_boundary_conditions(
+            parameters, V, [None, sm], temp)
+    for i in range(0, 3):
+        temp.t = i
+        expressions[0].t = i
+
+        T_left = 200 + i
+        T_right = 200 + 2*i
+        D_left = D_0*np.exp(-E_D/FESTIM.k_B/T_left)
+        D_right = D_0*np.exp(-E_D/FESTIM.k_B/T_right)
+        K_left = K_0*np.exp(-E_K/FESTIM.k_B/T_left)
+        K_right = K_0*np.exp(-E_K/FESTIM.k_B/T_right)
+        # Test that the expression is correct at vertices
+        expr = fenics.interpolate(expressions[0], V)
+
+        assert np.isclose(
+            expr(0, 0.5),
+            phi*R_p/D_left + (phi/K_left)**0.5)
+
+        assert np.isclose(
+            expr(1, 0.5),
+            phi*R_p/D_right + (phi/K_right)**0.5)
+
+
+def test_bc_recomb():
+    """Test the function boundary_conditions.apply_boundary_conditions
+    with bc type dc_recomb (with instantaneous recombination)
+    """
+    phi = 3
+    R_p = 5
+    D_0 = 2
+    E_D = 0.5
+    parameters = {
+        "materials": [
+            {
+                "id": 1
+                },
+            {
+                "id": 2
+                }
+        ],
+        "boundary_conditions": [
+            {
+                "type": "dc_recomb",
+                "implanted_flux": phi,
+                "implantation_depth": R_p,
+                "D_0": D_0,
+                "E_D": E_D,
+                "surfaces": [1, 2]
+            },
+        ]
+    }
+    # Set up
+    mesh = fenics.UnitSquareMesh(4, 4)
+    V = fenics.FunctionSpace(mesh, 'P', 1)
+    temp = fenics.Expression("200 + (x[0] + 1)*t", t=1, degree=1)
+
+    sm = fenics.MeshFunction("size_t", mesh, 1, 0)
+    left = fenics.CompiledSubDomain('x[0] < 0.0001')
+    left.mark(sm, 1)
+    right = fenics.CompiledSubDomain('x[0] > 0.99999999')
+    right.mark(sm, 2)
+
+    bcs, expressions = \
+        apply_boundary_conditions(
+            parameters, V, [None, sm], temp)
+    for i in range(0, 3):
+        temp.t = i
+        expressions[0].t = i
+
+        T_left = 200 + i
+        T_right = 200 + 2*i
+        D_left = D_0*np.exp(-E_D/FESTIM.k_B/T_left)
+        D_right = D_0*np.exp(-E_D/FESTIM.k_B/T_right)
+        # Test that the expression is correct at vertices
+        expr = fenics.interpolate(expressions[0], V)
+
+        assert np.isclose(
+            expr(0, 0.5),
+            phi*R_p/D_left)
+
+        assert np.isclose(
+            expr(1, 0.5),
+            phi*R_p/D_right)
+
+
+def test_bc_recomb_chemical_pot():
+    """Tests the function boundary_conditions.apply_boundary_conditions()
+    with type dc_recomb and conservation of chemical potential
+    """
+    phi = 3
+    R_p = 5
+    D_0 = 2
+    E_D = 0.5
+    K_0 = 2
+    E_K = 0.5
+    S_01 = 2
+    S_02 = 3
+    E_S1 = 0.1
+    E_S2 = 0.2
+    parameters = {
+        "materials": [
+            {
+                "S_0": S_01,
+                "E_S": E_S1,
+                "id": 1
+                },
+            {
+                "S_0": S_02,
+                "E_S": E_S2,
+                "id": 2
+                }
+        ],
+        "boundary_conditions": [
+            {
+                "type": "dc_recomb",
+                "implanted_flux": phi,
+                "implantation_depth": R_p,
+                "D_0": D_0,
+                "E_D": E_D,
+                "K_0": K_0,
+                "E_K": E_K,
+                "surfaces": [1, 2]
+            },
+        ]
+    }
+
+    mesh = fenics.UnitSquareMesh(4, 4)
+    V = fenics.FunctionSpace(mesh, 'P', 1)
+
+    temp = fenics.Expression("200 + (x[0] + 1)*t", t=1, degree=1)
+
+    vm = fenics.MeshFunction("size_t", mesh, 2, 1)
+    left = fenics.CompiledSubDomain('x[0] < 0.5')
+    right = fenics.CompiledSubDomain('x[0] >= 0.5')
+    left.mark(vm, 1)
+    right.mark(vm, 2)
+
+    sm = fenics.MeshFunction("size_t", mesh, 1, 0)
+    left = fenics.CompiledSubDomain('x[0] < 0.0001')
+    left.mark(sm, 1)
+    right = fenics.CompiledSubDomain('x[0] > 0.99999999')
+    right.mark(sm, 2)
+
+    bcs, expressions = \
+        apply_boundary_conditions(
+            parameters, V, [vm, sm], temp)
+
+    # Set up formulation
+    u = fenics.Function(V)
+    v = fenics.TestFunction(V)
+    F = fenics.dot(fenics.grad(u), fenics.grad(v))*fenics.dx
+
+    for i in range(0, 3):
+        temp.t = i
+        expressions[0]._bci.t = i
+
+        T_left = 200 + i
+        T_right = 200 + 2*i
+        D_left = D_0*np.exp(-E_D/FESTIM.k_B/T_left)
+        D_right = D_0*np.exp(-E_D/FESTIM.k_B/T_right)
+        K_left = K_0*np.exp(-E_K/FESTIM.k_B/T_left)
+        K_right = K_0*np.exp(-E_K/FESTIM.k_B/T_right)
+        S_left = S_01*np.exp(-E_S1/FESTIM.k_B/temp(0, 0.5))
+        S_right = S_02*np.exp(-E_S2/FESTIM.k_B/temp(1, 0.5))
+
+        # Test that the BCs can be applied to a problem
+        # and gives the correct values
+        fenics.solve(F == 0, u, bcs[0])
+        assert np.isclose(
+            u(0.25, 0.5),
+            (phi*R_p/D_left + (phi/K_left)**0.5)/S_left)
+        fenics.solve(F == 0, u, bcs[1])
+        assert np.isclose(
+            u(0.25, 0.5),
+            (phi*R_p/D_right + (phi/K_right)**0.5)/S_right)
