e2aaef9709bf5cab61f8861213f37c925e02cf5a
RemDelaporteMathurin <delaporte.mathurin@laposte.net>
2020-11-04 17:13:10 +0100

    fixed integration tests

--- Tests/integration/test_postprocessing_integration.py
+++ Tests/integration/test_postprocessing_integration.py
@@ -98,9 +98,6 @@ def test_run_post_processing(tmpdir):
 
     t = 0
     dt = 1
-    transient = True
-    append = True
-    markers = [volume_markers, surface_markers]
 
     files = define_xdmf_files(parameters["exports"])
     tab = \
@@ -108,16 +105,27 @@ def test_run_post_processing(tmpdir):
     properties = \
         create_properties(
             mesh, parameters["materials"], volume_markers, T)
+    my_sim = FESTIM.Simulation(parameters)
+    my_sim.transient = True
+    my_sim.u = u
+    my_sim.T = T
+    my_sim.volume_markers, my_sim.surface_markers = \
+        volume_markers, surface_markers
+    my_sim.V_CG1, my_sim.V_DG1 = V, V_DG1
+    my_sim.dt = dt
+    my_sim.files = files
+    my_sim.append = False
+    my_sim.D, my_sim.thermal_cond, my_sim.cp, my_sim.rho, \
+        my_sim.H, my_sim.S = properties
+    my_sim.derived_quantities_global = []
     for i in range(1, 3):
         t += dt
-        derived_quantities_global, dt = \
-            run_post_processing(
-                parameters, transient, u, T, markers, W, V_DG1, t, dt, files,
-                append=append, properties=properties,
-                derived_quantities_global=tab)
-        append = True
-    assert len(derived_quantities_global) == i + 1
-    assert derived_quantities_global[i][0] == t
+        my_sim.t = t
+        my_sim.derived_quantities_global, dt = \
+            run_post_processing(my_sim)
+        my_sim.append = True
+    assert len(my_sim.derived_quantities_global) == i + 1
+    assert my_sim.derived_quantities_global[i][0] == t
 
 
 def test_run_post_processing_pure_diffusion(tmpdir):
@@ -193,31 +201,39 @@ def test_run_post_processing_pure_diffusion(tmpdir):
 
     t = 0
     dt = 1
-    transient = True
-    append = True
-    markers = [volume_markers, surface_markers]
-
     files = define_xdmf_files(parameters["exports"])
     tab = \
         [header_derived_quantities(parameters)]
     properties = \
         create_properties(
             mesh, parameters["materials"], volume_markers, T)
+    my_sim = FESTIM.Simulation(parameters)
+    my_sim.transient = True
+    my_sim.u = u
+    my_sim.T = T
+    my_sim.volume_markers, my_sim.surface_markers = \
+        volume_markers, surface_markers
+    my_sim.V_CG1, my_sim.V_DG1 = V, V_DG1
+    my_sim.dt = dt
+    my_sim.files = files
+    my_sim.append = False
+    my_sim.D, my_sim.thermal_cond, my_sim.cp, my_sim.rho, \
+        my_sim.H, my_sim.S = properties
+    my_sim.derived_quantities_global = []
+
     for i in range(1, 3):
         t += dt
-        derived_quantities_global, dt = \
-            run_post_processing(
-                parameters, transient, u, T, markers, W, V_DG1, t, dt, files,
-                append=append, properties=properties,
-                derived_quantities_global=tab)
-        append = True
-        assert len(derived_quantities_global) == i + 1
-        assert derived_quantities_global[i][0] == t
-        assert derived_quantities_global[i][1] == 10
-        assert derived_quantities_global[i][2] == 20
-        assert round(derived_quantities_global[i][3]) == 11
-        assert derived_quantities_global[i][4] == 11
-        assert derived_quantities_global[i][5] == 1
+        my_sim.t = t
+        my_sim.derived_quantities_global, my_sim.dt = \
+            run_post_processing(my_sim)
+        my_sim.append = True
+        assert len(my_sim.derived_quantities_global) == i + 1
+        assert my_sim.derived_quantities_global[i][0] == t
+        assert my_sim.derived_quantities_global[i][1] == 10
+        assert my_sim.derived_quantities_global[i][2] == 20
+        assert round(my_sim.derived_quantities_global[i][3]) == 11
+        assert my_sim.derived_quantities_global[i][4] == 11
+        assert my_sim.derived_quantities_global[i][5] == 1
 
 
 def test_run_post_processing_flux(tmpdir):
@@ -282,11 +298,21 @@ def test_run_post_processing_flux(tmpdir):
         create_properties(
             mesh, parameters["materials"], volume_markers, T)
     t += dt
+    my_sim = FESTIM.Simulation(parameters)
+    my_sim.transient = True
+    my_sim.u = u
+    my_sim.T = T
+    my_sim.volume_markers, my_sim.surface_markers = volume_markers, surface_markers
+    my_sim.V_CG1, my_sim.V_DG1 = V, V_DG1
+    my_sim.dt = dt
+    my_sim.t = t
+    my_sim.files = None
+    my_sim.append = False
+    my_sim.D, my_sim.thermal_cond, my_sim.cp, my_sim.rho, \
+        my_sim.H, my_sim.S = properties
+    my_sim.derived_quantities_global = []
     derived_quantities_global, dt = \
-        run_post_processing(
-            parameters, transient, u, T, markers, V, V_DG1, t, dt, None,
-            append=append, properties=properties,
-            derived_quantities_global=tab)
+        run_post_processing(my_sim)
     print(derived_quantities_global[0])
     print(derived_quantities_global[1])
     assert np.isclose(derived_quantities_global[1][1],
