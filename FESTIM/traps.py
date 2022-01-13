7ee9a4b43c853d336aa6c7ed176c2eaaa87921c7
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-01-13 14:41:54 -0500

    added sub_expressions and F to traps

--- FESTIM/traps.py
+++ FESTIM/traps.py
@@ -256,6 +256,8 @@ class Trap(Concentration):
 class Traps:
     def __init__(self, traps=[]) -> None:
         self.traps = traps
+        self.F = None
+        self.sub_expressions = []
 
         # add ids if unspecified
         for i, trap in enumerate(self.traps, 1):
@@ -264,9 +266,12 @@ class Traps:
 
     def create_forms(self, mobile, materials, T, dx, dt=None,
                      chemical_pot=False):
+        self.F = 0
         for trap in self.traps:
             trap.create_form(mobile, materials, T, dx, dt=dt,
                              chemical_pot=chemical_pot)
+            self.F += trap.F
+            self.sub_expressions += trap.sub_expressions
 
     def get_trap(self, id):
         for trap in self.traps:
