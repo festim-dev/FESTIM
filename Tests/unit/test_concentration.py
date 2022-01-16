6ad5970c0635163f52a9370ccb55b82ca2a2aeec
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-01-15 23:59:01 -0500

    100% coverage of concentration

--- Tests/unit/test_concentration.py
+++ Tests/unit/test_concentration.py
@@ -43,3 +43,48 @@ class TestGetComp:
         # test
         for x in [0, 0.1, 0.2, 0.5, 0.6]:
             assert comp(x) == 1 + 0 - x
+
+
+class TestInitialise:
+    mesh = f.UnitIntervalMesh(10)
+    V = f.FunctionSpace(mesh, 'P', 1)
+    u = f.Function(V)
+    my_conc = FESTIM.Concentration(previous_solution=u)
+
+    def test_with_expression(self):
+        # build
+        value = 1 + 2*FESTIM.x**4
+        expected_sol = self.my_conc.get_comp(self.V, value)
+
+        # run
+        self.my_conc.initialise(self.V, value)
+
+        # test
+        for x in [0, 0.5, 0.3, 0.6]:
+            assert self.my_conc.previous_solution(x) == expected_sol(x)
+
+    def test_with_xdmf(self, tmpdir):
+        # build
+        value = 1 + 2*FESTIM.x**4
+
+        u = f.Expression(sp.printing.ccode(value), degree=1, t=0)
+        u = f.interpolate(u, self.V)
+
+        d = tmpdir.mkdir("folder")
+        time = 2
+        label = "label"
+        filename = str(Path(d.join("u_out.xdmf")))
+        with f.XDMFFile(filename) as file:
+            file.write_checkpoint(
+                u, label, time,
+                f.XDMFFile.Encoding.HDF5,
+                append=False)
+
+        expected_sol = self.my_conc.get_comp(self.V, filename, label=label, time_step=0)
+
+        # run
+        self.my_conc.initialise(self.V, value)
+
+        # test
+        for x in [0, 0.5, 0.3, 0.6]:
+            assert self.my_conc.previous_solution(x) == expected_sol(x)
