222bb0b3c7c0c322bca65c29e8453cf718554fa4
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-01-16 21:54:10 -0500

    removed unused function

--- FESTIM/initialising.py
+++ FESTIM/initialising.py
@@ -2,25 +2,6 @@ from fenics import *
 import sympy as sp
 
 
-def read_from_xdmf(filename, label, time_step, V):
-    """Reads function from XDMF
-
-    Args:
-        filename (str): the filename
-        label (str): the label of the function in the file
-        time_step (int): the timestep at which the file is read
-        V (fenics.FunctionSpace): The function space of the function
-
-    Returns:
-        [fenics.Function]: the function
-    """
-    comp = Function(V)
-    with XDMFFile(filename) as f:
-        f.read_checkpoint(comp, label, time_step)
-        #  only works if meshes are the same
-    return comp
-
-
 def check_no_duplicates(initial_conditions):
     components = []
     for e in initial_conditions:
