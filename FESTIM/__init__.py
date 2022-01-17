2b89209f4fcac3bdf8fd3818cdd28c0d8625c2a6
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-01-17 17:55:27 -0500

    test_for_maximum_volume

--- FESTIM/__init__.py
+++ FESTIM/__init__.py
@@ -16,7 +16,8 @@ from .solving import solve_it, solve_once, adaptive_stepsize
 
 from .export import treat_value, export_parameters, Export, Exports
 from .exports.xdmf_export import XDMFExport, XDMFExports
-from .exports.derived_quantities_export import DerivedQuantities, SurfaceFlux, AverageVolume, MinimumVolume
+from .exports.derived_quantities_export import DerivedQuantities, \
+    SurfaceFlux, AverageVolume, MinimumVolume, MaximumVolume
 from .exports.txt_export import TXTExport, TXTExports
 
 from .post_processing import is_export_derived_quantities, is_export_xdmf, \
