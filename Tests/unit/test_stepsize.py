5ac9dc45dbd48b0fc17c8eb380ec15908bca0501
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-01-19 14:22:35 -0500

    added additional tests

--- Tests/unit/test_stepsize.py
+++ Tests/unit/test_stepsize.py
@@ -11,8 +11,14 @@ class TestAdapt():
         with pytest.raises(SystemExit):
             my_stepsize.adapt(t=2, nb_it=3, converged=False)
 
-    def test_value_is_changed(self, my_stepsize):
+    def test_value_is_increased(self, my_stepsize):
         old_value = float(my_stepsize.value)
         my_stepsize.adapt(t=6, converged=True, nb_it=1)
         new_value = float(my_stepsize.value)
         assert new_value == old_value*my_stepsize.adaptive_stepsize["stepsize_change_ratio"]
+
+    def test_value_is_reduced(self, my_stepsize):
+        old_value = float(my_stepsize.value)
+        my_stepsize.adapt(t=6, converged=True, nb_it=6)
+        new_value = float(my_stepsize.value)
+        assert new_value == old_value/my_stepsize.adaptive_stepsize["stepsize_change_ratio"]
