6a614aeff32fc328bd423719e558c094c9df3b00
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-01-20 17:41:46 -0500

    fixed error message

--- FESTIM/exports/txt_export.py
+++ FESTIM/exports/txt_export.py
@@ -42,7 +42,7 @@ class TXTExport(FESTIM.Export):
                     break
                 except OSError as err:
                     print("OS error: {0}".format(err))
-                    print("The file " + filename + ".txt might currently be busy."
+                    print("The file " + filename + " might currently be busy."
                           "Please close the application then press any key.")
                     input()
 
