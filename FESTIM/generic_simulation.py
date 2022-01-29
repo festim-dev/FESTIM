e05c9ca20e4359223ee53f0bc2b5b24c246ad3cd
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-01-28 23:31:13 -0500

    if isinstance(flux) instead of dirichlet

--- FESTIM/generic_simulation.py
+++ FESTIM/generic_simulation.py
@@ -415,7 +415,7 @@ class Simulation:
 
         for bc in self.boundary_conditions:
             if bc.component != "T":
-                if not isinstance(bc, FESTIM.DirichletBC):
+                if isinstance(bc, FESTIM.FluxBC):
                     bc.create_form(self.T.T, solute)
                     # TODO : one day we will get rid of this huge expressions list
                     expressions += bc.sub_expressions
