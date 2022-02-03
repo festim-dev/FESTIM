0ad750cadc3f8b00847caa35e3523065c37498fa
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-03 12:21:24 -0500

    added docstrings

--- FESTIM/concentration/theta.py
+++ FESTIM/concentration/theta.py
@@ -31,6 +31,18 @@ class Theta(Mobile):
         f.assign(self.previous_solution, comp)
 
     def get_concentration_for_a_given_material(self, material, T):
+        """Returns the concentration (and previous concentration) for a given
+        material
+
+        Args:
+            material (FESTIM.Material): the material with attributes S_0 and
+                E_S
+            T (FESTIM.Temperature): the temperature with attributest T and T_n
+
+        Returns:
+            fenics.Product, fenics.Product: the current concentration and
+                previous concentration
+        """
         # TODO this needs changing for Henry
         E_S = material.E_S
         S_0 = material.S_0
@@ -39,9 +51,22 @@ class Theta(Mobile):
         return c_0, c_0_n
 
     def mobile_concentration(self):
+        """Returns the hydrogen concentration as c=theta*S
+        Where S is FESTIM.ArheniusCoeff defines on all materials.
+        This is needed when adding neuman or robin BCs to the form.
+
+        Returns:
+            fenics.Product: the hydrogen mobile concentration
+        """
         # TODO this needs changing for Henry
         return self.solution*self.S
 
     def post_processing_solution_to_concentration(self):
+        """Converts the post_processing_solution from theta to mobile
+        concentration.
+        c = theta * S.
+        The attribute post_processing_solution is fenics.Product (if self.S is
+        FESTIM.ArheniusCoeff)
+        """
         # TODO this needs changing for Henry
         self.post_processing_solution *= self.S
