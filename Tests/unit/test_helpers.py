7f04793ee4782989888cc537d1a01a6fe5bafbf5
jhdark <james.dark.95@gmail.com>
2022-02-08 16:59:49 +0100

    added negative values to test

--- Tests/unit/test_helpers.py
+++ Tests/unit/test_helpers.py
@@ -12,7 +12,7 @@ def test_help_key():
 
 
 def test_energy_converter():
-    test_values = [2, 20, 30, 20.5]
+    test_values = [2, 30, 20.5, -2, -12.2]
     for energy_value in test_values:
         energy_in_eV = kJmol_to_eV(energy_value)
         expected_value = k_B*energy_value*1e3/R
