ce605e7fb7b22671339c0dcbe7d909441eff1f21
James laptop <james.dark.95@gmail.com>
2022-02-09 18:10:14 +0100

    shortened test and variable names clearer

--- Tests/unit/test_sources.py
+++ Tests/unit/test_sources.py
@@ -1,36 +1,32 @@
 import FESTIM
 import sympy as sp
 import numpy as np
-import pytest
 
 
 def test_implantation_flux_attributes():
     """
-    Checks all attributes of the implantation flux class
+    Checks all attributes of the ImplantationFlux class
     """
-    flux_val = 1
-    imp_depth_val = 5e-9
-    width_val = 5e-9
-    distribution = 1/(width_val*(2*np.pi)**0.5) * \
-        sp.exp(-0.5*((FESTIM.x-imp_depth_val)/width_val)**2)
-    expected_value = flux_val*distribution
+    flux = 1
+    imp_depth = 5e-9
+    width = 5e-9
+    distribution = 1/(width*(2*np.pi)**0.5) * \
+        sp.exp(-0.5*((FESTIM.x-imp_depth)/width)**2)
+    expected_value = flux*distribution
 
-    my_source = FESTIM.ImplantationFlux(flux=flux_val, imp_depth=imp_depth_val,
-                                        width=width_val, volume=1)
-    flux = my_source.flux
-    imp_depth = my_source.imp_depth
-    width = my_source.width
-    value = my_source.value
+    my_source = FESTIM.ImplantationFlux(flux=flux, imp_depth=imp_depth,
+                                        width=width, volume=1)
 
-    assert flux == flux_val
-    assert imp_depth == imp_depth_val
-    assert width == width_val
-    assert expected_value == value
+    assert my_source.flux == flux
+    assert my_source.imp_depth == imp_depth
+    assert my_source.width == width
+    assert my_source.value == expected_value
 
 
 def test_implantation_flux_with_time_dependancy():
     """
-    Test to check flux can be defined with a time dependance
+    Checks that ImplantationFlux has the correct value attribute when using
+    time dependdant arguments
     """
     flux = 1*(FESTIM.t < 10)
     imp_depth = 5e-9
