597409afe44dce96d18039a6903892f8e0dee08d
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-18 02:02:46 +0000

    updated docstrings

--- FESTIM/sources/source.py
+++ FESTIM/sources/source.py
@@ -6,8 +6,8 @@ class Source:
     """Volumetric source term.
 
     Attributes:
-        value (fenics.Expression, fenics.Constant): the value of the
-            volumetric source term
+        value (fenics.Expression, fenics.UserExpression, fenics.Constant): the
+            value of the volumetric source term
         volume (int): the volume in which the source is applied
         field (str): the field on which the source is applied ("0", "solute",
             "1", "T")
@@ -17,7 +17,8 @@ class Source:
 
         Args:
             value (sympy.Expr, float, int, fenics.Expression,
-                fenics.Function): the value of the volumetric source term
+                fenics.UserExpression, fenics.Function): the value of the
+                volumetric source term
             volume (int): the volume in which the source is applied
             field (str): the field on which the source is applied ("0",
                 "solute", "1", "T")
