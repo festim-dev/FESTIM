3c7f57ba0084661f68c1fa4434dda7546367f379
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-02-18 01:37:58 +0000

    added tests

--- Tests/unit/test_sources.py
+++ Tests/unit/test_sources.py
@@ -1,4 +1,5 @@
 import FESTIM
+import fenics as f
 import sympy as sp
 import numpy as np
 
@@ -39,3 +40,31 @@ def test_implantation_flux_with_time_dependancy():
                                         imp_depth=5e-9, width=5e-9, volume=1)
 
     assert my_source.value._cppcode == expected_value
+
+
+def test_source_with_float_value():
+    """
+    Tests that Source can be created with a float value and that the .value
+    attribute is Constant
+    """
+    source = FESTIM.Source(2.0, volume=1, field="solute")
+    assert isinstance(source.value, f.Constant)
+
+
+def test_source_with_int_value():
+    """
+    Tests that Source can be created with a int value and that the .value
+    attribute is Constant
+    """
+    source = FESTIM.Source(2, volume=1, field="solute")
+    assert isinstance(source.value, f.Constant)
+
+
+def test_source_with_expression_value():
+    """
+    Tests that Source can be created with a fenics.Expression value and that
+    the .value attribute is fenics.Expression
+    """
+    value = f.Expression("2", degree=1)
+    source = FESTIM.Source(value, volume=1, field="solute")
+    assert isinstance(source.value, f.Expression)
