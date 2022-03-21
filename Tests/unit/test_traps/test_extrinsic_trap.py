7fb0b68fe45432e9e22dbf937a7c410b8c62c3d9
jhdark <james.dark.95@gmail.com>
2022-03-21 16:13:27 +0100

    test modified for class modifications

--- Tests/unit/test_traps/test_extrinsic_trap.py
+++ Tests/unit/test_traps/test_extrinsic_trap.py
@@ -18,6 +18,8 @@ class TestExtrinsicTrap:
     my_trap.density = [f.Function(V)]
     my_trap.density_previous_solution = f.Function(V)
     my_trap.density_test_function = f.TestFunction(V)
+    my_temp = FESTIM.Temperature(value=100)
+    my_temp.T = f.Function(V, name="T")
     dx = f.dx()
     dt = FESTIM.Stepsize(initial_value=1)
 
@@ -28,13 +30,14 @@ class TestExtrinsicTrap:
     def test_create_form_density(self):
         form_prms = self.my_trap.form_parameters
         density = self.my_trap.density[0]
+        T = self.my_temp
         expected_form = (density - self.my_trap.density_previous_solution)/self.dt.value * self.my_trap.density_test_function*self.dx
         expected_form += -form_prms["phi_0"] * (
             (1 - density/form_prms["n_amax"])*form_prms["eta_a"]*form_prms["f_a"] +
             (1 - density/form_prms["n_bmax"])*form_prms["eta_b"]*form_prms["f_b"] ) * \
             self.my_trap.density_test_function*self.dx
 
-        self.my_trap.create_form_density(self.dx, self.dt)
+        self.my_trap.create_form_density(self.dx, self.dt, T)
         print(expected_form)
         print(self.my_trap.form_density)
         assert self.my_trap.form_density.equals(expected_form)
