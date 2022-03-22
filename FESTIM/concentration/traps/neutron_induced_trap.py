d4d8c2b4291997b0f8987c2cf335d5fe309779e4
jhdark <james.dark.95@gmail.com>
2022-03-22 14:28:22 +0100

    better formulation and docs

--- FESTIM/concentration/traps/neutron_induced_trap.py
+++ FESTIM/concentration/traps/neutron_induced_trap.py
@@ -1,13 +1,19 @@
 from FESTIM import ExtrinsicTrap, k_B
 from fenics import *
-import sympy as sp
 
 
 class NeutronInducedTrap(ExtrinsicTrap):
-    def __init__(self, k_0, E_k, p_0, E_p, materials, form_parameters, id=None,
-                 type=None):
+    """
+    Class for neutron induced trap creation with annealing.
+    The temporal evolution of the trap density is given by:
+
+    dn_t/dt = phi*K*(1 - n_t/n_max) + A_0*exp(-E_A/(k_B*T))*n_t
+
+    """
+    def __init__(self, k_0, E_k, p_0, E_p, materials, form_parameters,
+                 id=None):
         """
-        Inits Neutron induced traps
+        Inits NeutronInducedTrap
 
         Args:
             E_k (float, list): trapping pre-exponential factor (m2/s)
@@ -15,32 +21,19 @@ class NeutronInducedTrap(ExtrinsicTrap):
             p_0 (float, list): detrapping pre-exponential factor (m2/s)
             E_p (float, list): detrapping activation energy (eV)
             materials (list or int): the materials ids the trap is living in
-            density_parameters (dict): dict with keys ["phi", "K", "n_max",
-                "A_0", "E_A", "n_0"]
+            form_parameters (dict): dict with keys ["phi", "K", "n_max",
+                "A_0", "E_A"].
+                phi: damage rate (dpa s-1),
+                K: trap creation factor (m-3 s-1),
+                n_max: maximum trap density (m-3),
+                A_0: trap_annealing_factor (s-1),
+                E_A: annealing activation energy (eV).
+                All variables in form_parameters dict can be floats or
+                sympy.Expr
             id (int, optional): The trap id. Defaults to None.
-
-        Notes:
-        For an analytical solution for trap density with the following
-        formulation:
-
-        n_t = F*n_max/(F + A*n_max) + (F*n_0 - F*n_max + A*n_0*n_max) *
-            np.exp(t*(-F/n_max - A))/(F + A*n_max)
-        Where:
-        F = phi*K
-        A = A_0*np.exp(-E_A/(k_B*T))
-
-            phi (float, sympy.Expr): damage rate (dpa s-1)
-            K (float): trap creation factor (m-3 s-1)
-            n_max (float): maximum trap density (m-3)
-            A_0 (float): trap_annealing_factor (s-1)
-            E_A (float): annealing activation energy (eV)
-            n_0 (float): number of initial traps (m-3)
         """
         super().__init__(k_0, E_k, p_0, E_p, materials, form_parameters,
-                         id=None, type=None)
-        self.form_parameters = form_parameters
-        self.density_previous_solution = None
-        self.density_test_function = None
+                         id=id)
 
     def create_form_density(self, dx, dt, T):
         """
@@ -57,16 +50,12 @@ class NeutronInducedTrap(ExtrinsicTrap):
         n_max = self.form_parameters["n_max"]
         A_0 = self.form_parameters["A_0"]
         E_A = self.form_parameters["E_A"]
-        n_0 = self.form_parameters["n_0"]
         density = self.density[0]
         T = T.T
 
         F = ((density - self.density_previous_solution)/dt.value) * \
             self.density_test_function*dx
-        F += -(phi*K*n_max/(phi*K + A_0*exp(-E_A/(k_B*T))*n_max) +
-               (phi*K*n_0 - phi*K*n_max + A_0*exp(-E_A/(k_B*T))*n_0*n_max) *
-               exp(dt.value*(-phi*K/n_max - (A_0*exp(-E_A/(k_B*T))))) /
-               (phi*K + A_0*exp(-E_A/(k_B*T))*n_max)) *\
-            self.density_test_function*dx
+        F += -phi*K*(1 - (density/n_max)) * self.density_test_function*dx
+        F += A_0*exp(-E_A/(k_B*T))*density * self.density_test_function*dx
 
         self.form_density = F
