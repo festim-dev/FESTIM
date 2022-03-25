b822a581727a10c4c5be94f0d22e5971cc899431
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-03-25 14:02:05 +0100

    added tests

--- Tests/unit/test_meshing.py
+++ Tests/unit/test_meshing.py
@@ -5,6 +5,7 @@ from FESTIM import Mesh, Mesh1D, MeshFromRefinements, MeshFromVertices, \
 import fenics
 import pytest
 from pathlib import Path
+import numpy as np
 
 
 def test_mesh_and_refine_meets_refinement_conditions():
@@ -45,26 +46,77 @@ def test_mesh_and_refine_meets_refinement_conditions():
         assert nb_cell_2 >= refinement[1]["cells"]
 
 
-def test_subdomains_1D():
-    '''
-    Test that subdomains are assigned properly
-    '''
-    mesh = fenics.UnitIntervalMesh(20)
+class TestDefineMarkers:
+    def test_2_materials_2_subdomains(self):
+        '''
+        Test that subdomains are assigned properly
+        '''
+        mesh = fenics.UnitIntervalMesh(20)
 
-    materials = [
-        Material(id=1, D_0=None, E_D=None, borders=[0, 0.5]),
-        Material(id=2, D_0=None, E_D=None, borders=[0.5, 1]),
-        ]
-    my_mats = Materials(materials)
-    my_mesh = Mesh1D()
-    my_mesh.mesh = mesh
-    my_mesh.size = 1
-    my_mesh.define_markers(my_mats)
-    for cell in fenics.cells(mesh):
-        if cell.midpoint().x() < 0.5:
-            assert my_mesh.volume_markers[cell] == 1
-        else:
-            assert my_mesh.volume_markers[cell] == 2
+        materials = [
+            Material(id=1, D_0=None, E_D=None, borders=[0, 0.5]),
+            Material(id=2, D_0=None, E_D=None, borders=[0.5, 1]),
+            ]
+        my_mats = Materials(materials)
+        my_mesh = Mesh1D()
+        my_mesh.mesh = mesh
+        my_mesh.size = 1
+        my_mesh.define_markers(my_mats)
+        for cell in fenics.cells(mesh):
+            if cell.midpoint().x() < 0.5:
+                assert my_mesh.volume_markers[cell] == 1
+            else:
+                assert my_mesh.volume_markers[cell] == 2
+
+    def test_1_material_2_subdomains(self):
+        my_mats = Materials([
+            Material([1, 2], 1, 0, borders=[[0, 0.5], [0.5, 1]])
+        ])
+        my_mesh = Mesh1D()
+        my_mesh.mesh = fenics.UnitIntervalMesh(19)
+        my_mesh.size = 1
+
+        my_mesh.define_markers(my_mats)
+        for cell in fenics.cells(my_mesh.mesh):
+            if cell.midpoint().x() < 0.5:
+                assert my_mesh.volume_markers[cell] == 1
+            else:
+                assert my_mesh.volume_markers[cell] == 2
+
+    def test_2_materials_3_subdomains(self):
+        my_mats = Materials([
+            Material([1, 2], 1, 0, borders=[[0, 0.25], [0.25, 0.5]]),
+            Material(3, 1, 0, borders=[0.5, 1])
+        ])
+        my_mesh = Mesh1D()
+        my_mesh.mesh = fenics.UnitIntervalMesh(19)
+        my_mesh.size = 1
+
+        my_mesh.define_markers(my_mats)
+        for cell in fenics.cells(my_mesh.mesh):
+            if cell.midpoint().x() < 0.25:
+                assert my_mesh.volume_markers[cell] == 1
+            elif cell.midpoint().x() < 0.5:
+                assert my_mesh.volume_markers[cell] == 2
+            else:
+                assert my_mesh.volume_markers[cell] == 3
+
+    def test_1_material_1_id_2_borders(self):
+        my_mats = Materials([
+            Material(1, 1, 0, borders=[[0, 0.5], [0.7, 1]])
+        ])
+
+        my_mesh = Mesh1D()
+        my_mesh.mesh = fenics.UnitIntervalMesh(19)
+        my_mesh.size = 1
+
+        my_mesh.define_markers(my_mats)
+
+        for cell in fenics.cells(my_mesh.mesh):
+            if 0 < cell.midpoint().x() < 0.5:
+                assert my_mesh.volume_markers[cell] == 1
+            elif 0.7 < cell.midpoint().x() < 1:
+                assert my_mesh.volume_markers[cell] == 1
 
 
 def test_create_mesh_xdmf(tmpdir):
