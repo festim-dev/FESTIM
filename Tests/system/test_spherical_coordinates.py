17ae723e6155146f8a61869f8ce21f69510987d9
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-03-31 17:03:28 +0000

    added a test

--- Tests/system/test_spherical_coordinates.py
+++ Tests/system/test_spherical_coordinates.py
@@ -10,14 +10,13 @@ def test_run_MMS():
     '''
     r = FESTIM.x
 
-    u = 1 + sp.sin(2*fenics.pi*r)
+    u = 1 + r**2 #+ sp.sin(2*fenics.pi*r)
     T = 700 + 30*r
-    E_D = 0*0.1
-    D_0 = 1#2
+    E_D = 0.1
+    D_0 = 2
     k_B = FESTIM.k_B
     D = D_0 * sp.exp(-E_D/k_B/T)
-    f = D * -1/(r**2) * sp.diff(r**2 * sp.diff(u, r), r)
-    # f = D * -sp.diff(sp.diff(u, r), r)
+    f = -1/(r**2) * sp.diff(D * r**2 * sp.diff(u, r), r)
 
     my_materials = FESTIM.Materials(
         [
@@ -25,7 +24,7 @@ def test_run_MMS():
         ]
     )
 
-    my_mesh = FESTIM.MeshFromVertices(np.linspace(0, 2, 50))
+    my_mesh = FESTIM.MeshFromVertices(np.linspace(1, 2, 500), type="spherical")
 
     my_bcs = [
         FESTIM.DirichletBC(surfaces=[1, 2], value=u, component=0),
@@ -63,5 +62,4 @@ def test_run_MMS():
     u_expr = fenics.Expression(sp.printing.ccode(u), degree=3)
     expected_solution = fenics.interpolate(u_expr, my_sim.h_transport_problem.V)
     error_L2 = fenics.errornorm(expected_solution, produced_solution, 'L2')
-    print(error_L2)
-    assert False
+    assert error_L2 < 1e-7
