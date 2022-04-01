6d7bfcb82abb090a05acd188ca70756ac6d27cc3
James Dark (vsc) <james.dark.95@gmail.com>
2022-04-01 14:54:08 +0000

    new test

--- Tests/system/test_system_from_objects.py
+++ Tests/system/test_system_from_objects.py
@@ -1064,3 +1064,37 @@ def test_completion_tone():
     )
     my_model.initialise()
     my_model.run()
+
+
+def test_run_outputs():
+    """Checks that when the outputs flag in run is true, an output is produced
+    """
+    my_model = FESTIM.Simulation(log_level=20)
+    my_model.mesh = FESTIM.MeshFromRefinements(10, size=1)
+    my_model.materials = FESTIM.Materials(
+        [
+            FESTIM.Material(id=1, D_0=1, E_D=0)
+        ]
+    )
+    my_model.T = FESTIM.Temperature(100)
+    my_model.boundary_conditions = [
+        FESTIM.DirichletBC(surfaces=[1, 2], value=0),
+    ]
+    my_stepsize = FESTIM.Stepsize(1, stepsize_change_ratio=1.1, dt_min=1e-8)
+    my_derived_quantities = FESTIM.DerivedQuantities()
+    my_derived_quantities.derived_quantities = [
+        FESTIM.TotalVolume("solute", 1),
+    ]
+    my_model.exports = FESTIM.Exports([
+        my_derived_quantities
+        ]
+    )
+    my_model.dt = my_stepsize
+    my_model.settings = FESTIM.Settings(
+        absolute_tolerance=1e-9,
+        relative_tolerance=1e-9,
+        final_time=1,
+    )
+    my_model.initialise()
+    out = my_model.run(output=True)
+    assert out["derived_quantities"]
