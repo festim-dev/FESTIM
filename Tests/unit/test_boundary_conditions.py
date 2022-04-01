b113dab2cdb92130938a8524e99492f8fce6afba
ehodille <etienne.hodille@gmail.com>
2022-04-01 14:56:09 +0200

    test for Henry -> to PR

--- Tests/unit/test_boundary_conditions.py
+++ Tests/unit/test_boundary_conditions.py
@@ -329,6 +329,77 @@ def test_sievert_bc_varying_temperature():
     T.assign(1000)
     assert my_bc.expression(0) == pytest.approx(expected(0))
 
+def test_henry_bc_varying_time():
+	"""Checks the method DircirchletBC.create_expression with type solubility Henry
+	"""
+	# build
+	T = fenics.Constant(300)
+	pressure_expr = 1e5*(1 + FESTIM.t)
+	H_0_expr = 100
+	E_H_expr = 0.5
+	my_bc = FESTIM.HenrysBC(surfaces = 1, pressure = pressure_expr, H_0 = H_0_expr, E_H = E_H_expr)
+	pressure_expr = fenics.Expression(sp.printing.ccode(pressure_expr),
+										t=0,
+										degree=1)
+	H_0_expr = fenics.Expression(sp.printing.ccode(H_0_expr),
+										t=0,
+										degree=1)
+	E_H_expr = fenics.Expression(sp.printing.ccode(E_H_expr),
+										t=0,
+										degree=1)
+	T_expr = fenics.Expression(sp.printing.ccode(T),
+										t=0,
+										degree=1)
+	#run
+	my_bc.create_expression(T)
+	#test
+	def henrys(T, H_0, E_H, pressure):
+		H = H_0*fenics.exp(-E_H/FESTIM.k_B/T)
+		return H*pressure
+	prms = {"H_0": H_0_expr, "E_H": E_H_expr, "pressure": pressure_expr}
+
+	expected = FESTIM.BoundaryConditionExpression(T_expr, eval_function = henrys, pressure = pressure_expr, H_0 = H_0_expr, E_H = E_H_expr)
+	assert my_bc.expression(0) == pytest.approx(expected(0))
+
+	for prm in my_bc.sub_expressions:
+		if hasattr(prm, "t"):
+			prm.t += 10
+	for prm in prms.values():
+		prm.t += 10
+	assert my_bc.expression(0) == pytest.approx(expected(0))
+
+def test_henry_bc_varying_temperature():
+	"""Checks the method DirichletBC.create_expression with type solubility Henry
+	"""
+	# build
+	T = fenics.Constant(300)
+	pressure_expr = 1e5*(1 + FESTIM.t)
+	H_0_expr = 100
+	E_H_expr = 0.5
+	my_bc = FESTIM.HenrysBC(surfaces = 1, pressure = pressure_expr, H_0 = H_0_expr, E_H = E_H_expr)
+
+	pressure_expr = fenics.Expression(sp.printing.ccode(pressure_expr),
+										t = 0,
+										degree = 1)
+	H_0_expr = fenics.Expression(sp.printing.ccode(H_0_expr),
+										t = 0,
+										degree = 1)
+	E_H_expr = fenics.Expression(sp.printing.ccode(E_H_expr),
+										t = 0,
+										degree = 1)
+
+	# run
+	my_bc.create_expression(T)
+	# test
+
+	def henrys(T, H_0, E_H, pressure):
+		H = H_0*fenics.exp(-E_H/FESTIM.k_B/T)
+		return H*pressure
+	expected = FESTIM.BoundaryConditionExpression(T, eval_function=henrys, H_0=H_0_expr, E_H=E_H_expr, pressure=pressure_expr)
+	assert my_bc.expression(0) == pytest.approx(expected(0))
+
+	T.assign(1000)
+	assert my_bc.expression(0) == pytest.approx(expected(0))
 
 def test_create_expression_dc_custom():
     """Creates a dc_custom bc and checks create_expression returns
