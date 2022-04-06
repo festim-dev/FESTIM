a1c3fb0363d55042cca0a7774b48d11e1aba345f
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-04-06 18:31:06 +0000

    formatting

--- FESTIM/boundary_conditions/dirichlets/henrys_bc.py
+++ FESTIM/boundary_conditions/dirichlets/henrys_bc.py
@@ -4,15 +4,15 @@ import sympy as sp
 
 
 def henrys_law(T, H_0, E_H, pressure):
-    H = H_0*f.exp(-E_H/k_B/T)
-    return H*pressure
+    H = H_0 * f.exp(-E_H / k_B / T)
+    return H * pressure
 
 
 class HenrysBC(DirichletBC):
-    """Subclass of DirichletBC for Henry's law: cm = H*pressure
-    """
+    """Subclass of DirichletBC for Henry's law: cm = H*pressure"""
+
     def __init__(self, surfaces, H_0, E_H, pressure) -> None:
-        """ Inits HenrysBC
+        """Inits HenrysBC
 
         Args:
             surfaces (list or int): the surfaces on which the BC is applied
@@ -26,13 +26,13 @@ class HenrysBC(DirichletBC):
         self.pressure = pressure
 
     def create_expression(self, T):
-        pressure = f.Expression(
-                sp.printing.ccode(self.pressure),
-                t=0, degree=1)
+        pressure = f.Expression(sp.printing.ccode(self.pressure), t=0, degree=1)
         value_BC = BoundaryConditionExpression(
-                T, henrys_law,
-                H_0=self.H_0, E_H=self.E_H,
-                pressure=pressure,
+            T,
+            henrys_law,
+            H_0=self.H_0,
+            E_H=self.E_H,
+            pressure=pressure,
         )
         self.expression = value_BC
         self.sub_expressions = [pressure]
