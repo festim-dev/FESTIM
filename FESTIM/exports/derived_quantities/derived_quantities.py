4e4367d9a28917633706e42205a3022f449b7d66
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-04-07 13:31:09 +0000

    added a t attribute to DerivedQuantities + updated demo_ogorodnikova

--- FESTIM/exports/derived_quantities/derived_quantities.py
+++ FESTIM/exports/derived_quantities/derived_quantities.py
@@ -30,6 +30,7 @@ class DerivedQuantities:
         # TODO remove this
         self.make_derived_quantities(derived_quantities)
         self.data = [self.make_header()]
+        self.t = []
 
     def make_derived_quantities(self, derived_quantities):
         for derived_quantity, list_of_prms_dicts in derived_quantities.items():
@@ -96,6 +97,7 @@ class DerivedQuantities:
             quantity.t.append(t)
             row.append(value)
         self.data.append(row)
+        self.t.append(t)
 
     def write(self):
         if self.file is not None:
