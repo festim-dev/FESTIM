6c506305d410d88179893e5a84279b2d8882e653
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-04-07 10:08:30 +0000

    changed ExtrinsicTrap to ExtrinsicTrapBase

--- FESTIM/concentration/traps/traps.py
+++ FESTIM/concentration/traps/traps.py
@@ -28,9 +28,9 @@ class Traps:
         raise ValueError("Couldn't find trap {}".format(id))
 
     def initialise_extrinsic_traps(self, V):
-        """Add functions to ExtrinsicTrap objects for density form"""
+        """Add functions to ExtrinsicTrapBase objects for density form"""
         for trap in self.traps:
-            if isinstance(trap, FESTIM.ExtrinsicTrap):
+            if isinstance(trap, FESTIM.ExtrinsicTrapBase):
                 trap.density = [f.Function(V)]
                 trap.density_test_function = f.TestFunction(V)
                 trap.density_previous_solution = f.project(f.Constant(0), V)
@@ -47,17 +47,17 @@ class Traps:
         self.extrinsic_formulations = []
         expressions_extrinsic = []
         for trap in self.traps:
-            if isinstance(trap, FESTIM.ExtrinsicTrap):
+            if isinstance(trap, FESTIM.ExtrinsicTrapBase):
                 trap.create_form_density(dx, dt, T)
                 self.extrinsic_formulations.append(trap.form_density)
         self.sub_expressions.extend(expressions_extrinsic)
 
     def solve_extrinsic_traps(self):
         for trap in self.traps:
-            if isinstance(trap, FESTIM.ExtrinsicTrap):
+            if isinstance(trap, FESTIM.ExtrinsicTrapBase):
                 f.solve(trap.form_density == 0, trap.density[0], [])
 
     def update_extrinsic_traps_density(self):
         for trap in self.traps:
-            if isinstance(trap, FESTIM.ExtrinsicTrap):
+            if isinstance(trap, FESTIM.ExtrinsicTrapBase):
                 trap.density_previous_solution.assign(trap.density[0])
