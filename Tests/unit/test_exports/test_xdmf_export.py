fb926b4824090a59afbbd0c68fcf974ea7e936a7
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-04-08 13:52:14 +0000

    updated xdmf_export tests

--- Tests/unit/test_exports/test_xdmf_export.py
+++ Tests/unit/test_exports/test_xdmf_export.py
@@ -11,28 +11,27 @@ def folder(tmpdir):
 
 def test_define_file_upon_construction(folder):
     mesh = f.UnitIntervalMesh(10)
-    label = "my_label"
-    my_xdmf = XDMFExport("solute", label, folder)
+    filename = "{}/my_filename.xdmf".format(folder)
+    my_xdmf = XDMFExport("solute", label="solute_label", filename=filename)
 
     my_xdmf.file.write(mesh)
 
     mesh_in = f.Mesh()
-    f.XDMFFile(folder + "/" + label + ".xdmf").read(mesh_in)
+    f.XDMFFile(filename).read(mesh_in)
 
 
 class TestDefineFile:
     mesh = f.UnitIntervalMesh(10)
-    my_xdmf = XDMFExport("solute", "foo", "foo")
+    my_xdmf = XDMFExport("solute", "my_label", "my_filename.xdmf")
 
     def test_file_exists(self, folder):
-        label = "my_label"
-        self.my_xdmf.label = label
-        self.my_xdmf.folder = folder
+        filename = "{}/my_filename.xdmf".format(folder)
+        self.my_xdmf.filename = filename
         self.my_xdmf.define_xdmf_file()
         self.my_xdmf.file.write(self.mesh)
 
         mesh_in = f.Mesh()
-        f.XDMFFile(folder + "/" + label + ".xdmf").read(mesh_in)
+        f.XDMFFile(filename).read(mesh_in)
 
 
 class TestWrite:
@@ -42,7 +41,8 @@ class TestWrite:
     label_to_function = {"solute": u}
 
     def test_no_checkpoint_no_error(self, folder):
-        my_xdmf = XDMFExport("solute", "foo", folder)
+        filename = folder + "/coucou.xdmf"
+        my_xdmf = XDMFExport("solute", "foo", filename)
         my_xdmf.function = self.u
 
         my_xdmf.write(t=2)
@@ -51,7 +51,8 @@ class TestWrite:
         """checks that without checkpointing one cannot read the
         xdmf file
         """
-        my_xdmf = XDMFExport("solute", "foo", folder, checkpoint=False)
+        filename = folder + "/coucou.xdmf"
+        my_xdmf = XDMFExport("solute", "foo", filename, checkpoint=False)
         my_xdmf.function = self.u
 
         my_xdmf.write(t=2)
@@ -61,7 +62,8 @@ class TestWrite:
 
     def test_checkpointing(self, folder):
         """checks that the xdmf file can be read with checkpointing"""
-        my_xdmf = XDMFExport("solute", "foo", folder)
+        filename = folder + "/coucou.xdmf"
+        my_xdmf = XDMFExport("solute", "foo", filename)
         my_xdmf.function = self.u
 
         my_xdmf.write(t=2)
@@ -70,47 +72,48 @@ class TestWrite:
 
     def test_write_attribute(self, folder):
         """Checks that the file is written with the appropriate attribute"""
-        my_xdmf = XDMFExport("solute", "coucou", folder)
+        filename = folder + "/coucou.xdmf"
+        my_xdmf = XDMFExport("solute", "coucou", filename)
         my_xdmf.function = self.u
         my_xdmf.write(t=0)
 
-        labels = extract_xdmf_labels(folder + "/coucou.xdmf")
+        labels = extract_xdmf_labels(filename)
 
         assert len(labels) == 1
         assert labels[0] == "coucou"
 
 
-def test_error_folder_empty_str():
-    with pytest.raises(ValueError, match="empty string"):
-        XDMFExport("solute", "solute", "")
+def test_error_folder_endswith_xdmf():
+    with pytest.raises(ValueError, match="must end with .xdmf"):
+        XDMFExport("solute", "solute", "coucou")
 
 
 def test_error_folder_not_a_str():
-    with pytest.raises(TypeError, match="type str"):
+    with pytest.raises(TypeError, match="must be a string"):
         XDMFExport("solute", "solute", 2)
 
 
 def test_error_checkpoint_wrong_type():
     with pytest.raises(TypeError, match="checkpoint should be a bool"):
-        XDMFExport("solute", "solute", "my_folder", checkpoint=2)
+        XDMFExport("solute", "solute", "my_filename.xdmf", checkpoint=2)
 
 
 def test_wrong_argument_for_mode():
     accepted_values_msg = "accepted values for mode are int and 'last'"
     # test wrong type
     with pytest.raises(ValueError, match=accepted_values_msg):
-        XDMFExport("solute", "mobile", "out", mode=1.2)
+        XDMFExport("solute", "mobile", "out.xdmf", mode=1.2)
     with pytest.raises(ValueError, match=accepted_values_msg):
-        XDMFExport("solute", "mobile", "out", mode=[1, 2, 3])
+        XDMFExport("solute", "mobile", "out.xdmf", mode=[1, 2, 3])
 
     # test wrong string
     with pytest.raises(ValueError, match=accepted_values_msg):
-        XDMFExport("solute", "mobile", "out", mode="foo")
+        XDMFExport("solute", "mobile", "out.xdmf", mode="foo")
 
     # test negative integer
     with pytest.raises(ValueError, match="mode must be positive"):
-        XDMFExport("solute", "mobile", "out", mode=-1)
+        XDMFExport("solute", "mobile", "out.xdmf", mode=-1)
 
     # test mode=0
     with pytest.raises(ValueError, match="mode must be positive"):
-        XDMFExport("solute", "mobile", "out", mode=0)
+        XDMFExport("solute", "mobile", "out.xdmf", mode=0)
