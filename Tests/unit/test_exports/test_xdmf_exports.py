d7e7439ba9c556124b832daa2db6a8747661734b
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-04-11 11:01:25 +0000

    uncommented two tests

--- Tests/unit/test_exports/test_xdmf_exports.py
+++ Tests/unit/test_exports/test_xdmf_exports.py
@@ -1,34 +1,34 @@
-# from FESTIM import XDMFExports
-# import pytest
+from FESTIM import XDMFExports
+import pytest
 
 
-# def test_error_different_lengths_functions_labels():
-#     with pytest.raises(
-#         ValueError, match="doesn't match number of labels in xdmf exports"
-#     ):
-#         XDMFExports(["solute", "T"], ["solute"], "my_folder")
+def test_error_different_lengths_functions_labels():
+    with pytest.raises(
+        ValueError, match="doesn't match number of labels in xdmf exports"
+    ):
+        XDMFExports(["solute", "T"], ["solute"], "my_folder")
 
 
-# class TestListXDMFExports:
-#     labels = ["T", "solute", "1"]
-#     functions = ["T", "solute", 1]
-#     folder = "my_folder"
-#     my_exports = XDMFExports(functions, labels, file=folder)
+class TestListXDMFExports:
+    labels = ["T", "solute", "1"]
+    functions = ["T", "solute", 1]
+    folder = "my_folder"
+    my_exports = XDMFExports(functions, labels, folder=folder)
 
-#     def test_length(self):
-#         assert len(self.my_exports.xdmf_exports) == len(self.functions)
+    def test_length(self):
+        assert len(self.my_exports.xdmf_exports) == len(self.functions)
 
-#     def test_function_attributes(self):
-#         for export, function in zip(self.my_exports.xdmf_exports, self.functions):
-#             assert export.field == function
+    def test_function_attributes(self):
+        for export, function in zip(self.my_exports.xdmf_exports, self.functions):
+            assert export.field == function
 
-#     def test_label_attributes(self):
-#         for export, label in zip(self.my_exports.xdmf_exports, self.labels):
-#             assert export.label == label
+    def test_label_attributes(self):
+        for export, label in zip(self.my_exports.xdmf_exports, self.labels):
+            assert export.label == label
 
-#     def test_folder_attributes(self):
-#         for export in self.my_exports.xdmf_exports:
-#             assert export.folder == self.folder
+    def test_folder_attributes(self):
+        for export in self.my_exports.xdmf_exports:
+            assert export.folder == self.folder
 
 
 # def test_deprecation_warning():
