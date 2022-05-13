87d3a5713a8ae137bf2a958ebca4a8642cf8d181
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-13 11:50:09 +0000

    deleted test file that wasn't run...

--- Tests/simulation/test_parameters.py
+++
@@ -1,7 +0,0 @@
-import FESTIM
-import pytest
-
-
-def error_unknown_solving_type():
-    with pytest.raises(ValueError, match="unknwon"):
-        FESTIM.Simulation({"solving_parameters": {"type": "coucou"}})
