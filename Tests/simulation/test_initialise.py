7695f7ccf8d87be653441ea8df48973a1817032d
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2022-05-17 11:44:34 +0000

    added test that catches the bug

--- Tests/simulation/test_initialise.py
+++ Tests/simulation/test_initialise.py
@@ -49,3 +49,27 @@ def test_initialise_sets_t_to_zero():
 
     # check that my_model.t is reinitialised to zero
     assert my_model.t == 0
+
+
+def test_initialise_initialise_dt():
+    """Creates a Simulation object and checks that .initialise() sets
+    the value attribute of the dt attribute to dt.initial_value
+    """
+    # build
+    my_model = F.Simulation()
+    my_model.mesh = F.MeshFromVertices([1, 2, 3])
+    my_model.materials = F.Material(id=1, D_0=1, E_D=0, thermal_cond=1)
+    my_model.T = F.Temperature(100)
+    my_model.dt = F.Stepsize(initial_value=3)
+    my_model.settings = F.Settings(
+        absolute_tolerance=1e-10, relative_tolerance=1e-10, final_time=4
+    )
+
+    # run
+    my_model.initialise()
+    my_model.run()
+
+    my_model.initialise()
+
+    # test
+    assert my_model.dt.value(2) == 3
