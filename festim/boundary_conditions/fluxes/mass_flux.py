7259302d60bd75faae550dccc5fde5cbce12bcb1
Rémi Delaporte-Mathurin <40028739+RemDelaporteMathurin@users.noreply.github.com>
2022-11-24 10:11:10 -0500

    black format

--- festim/boundary_conditions/fluxes/mass_flux.py
+++ festim/boundary_conditions/fluxes/mass_flux.py
@@ -13,16 +13,16 @@ class MassFlux(FluxBC):
         c_ext (float or sp.Expr): external concentration (1/m3)
         surfaces (list or int): the surfaces of the BC
 
-    Reference: Bergman, T. L., Bergman, T. L., Incropera, F. P., Dewitt, D. P., 
+    Reference: Bergman, T. L., Bergman, T. L., Incropera, F. P., Dewitt, D. P.,
     & Lavine, A. S. (2011). Fundamentals of heat and mass transfer. John Wiley & Sons.
     """
 
     def __init__(self, h_coeff, c_ext, surfaces) -> None:
         self.h_coeff = h_coeff
         self.c_ext = c_ext
-        super().__init__(surfaces=surfaces, field=0) 
+        super().__init__(surfaces=surfaces, field=0)
 
-    def create_form(self, T, solute): 
+    def create_form(self, T, solute):
         h_coeff = f.Expression(sp.printing.ccode(self.h_coeff), t=0, degree=1)
         c_ext = f.Expression(sp.printing.ccode(self.c_ext), t=0, degree=1)
 
