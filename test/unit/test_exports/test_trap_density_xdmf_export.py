1dad15e74b9ae75f380441a677fc104843e5cc15
J Dark <james.dark.95@gmail.com>
2023-08-11 14:02:56 +0000

    altered fenics test, new test

--- test/unit/test_exports/test_trap_density_xdmf_export.py
+++ test/unit/test_exports/test_trap_density_xdmf_export.py
@@ -2,6 +2,7 @@ import festim
 from fenics import *
 import sympy as sp
 from pathlib import Path
+import numpy as np
 
 
 def test_trap_density_xdmf_export_intergration_with_simultion(tmpdir):
@@ -69,7 +70,8 @@ def test_trap_density_xdmf_export_write(tmpdir):
     dx = Measure("dx", domain=mesh, subdomain_data=volume_markers)
 
     density_file = tmpdir.join("density1.xdmf")
-    trap_1 = festim.Trap(1, 0, 1, 0, materials="1", density=density_expr)
+    mat = festim.Material(1, 1, 1)
+    trap_1 = festim.Trap(1, 0, 1, 0, materials=mat, density=density_expr)
     my_export = festim.TrapDensityXDMF(
         trap=trap_1,
         label="density1",
@@ -86,3 +88,53 @@ def test_trap_density_xdmf_export_write(tmpdir):
     error_L2 = errornorm(density_expected, density_read, "L2")
     print(error_L2)
     assert error_L2 < 1e-10
+
+
+def test_trap_density_xdmf_export_traps_materials_mixed(tmpdir):
+    """Integration test for TrapDensityXDMF.write() with a trap material given
+    as a string. Creates a festim simulation and exports the trap density as an
+    .xmdf file. An equivalent fenics function is created and is compared to that
+    that read from the .xdmf file created. Ensures compatability with
+    festim.Simulation()
+    """
+    density_expr = 2e06 + festim.x**2
+
+    my_model = festim.Simulation(log_level=20)
+    vertices = np.linspace(0, 3e-06, num=100)
+    my_model.mesh = festim.MeshFromVertices(vertices=vertices)
+
+    mat_2 = festim.Material(D_0=2, E_D=0, id=2)
+    my_model.materials = festim.Materials(
+        [festim.Material(D_0=1, E_D=0, id=1, name="1"), mat_2]
+    )
+    trap_1 = festim.Trap(
+        k_0=1, E_k=0, p_0=1, E_p=0, density=density_expr, materials="1"
+    )
+    trap_2 = festim.Trap(k_0=1, E_k=0, p_0=1, E_p=0, density=3, materials=["1", mat_2])
+    my_model.traps = festim.Traps([trap_1, trap_2])
+    my_model.T = festim.Temperature(value=300)
+    my_model.settings = festim.Settings(
+        transient=False,
+        absolute_tolerance=1e06,
+        relative_tolerance=1e-08,
+    )
+    density_file = tmpdir.join("density1.xdmf")
+    my_export = festim.TrapDensityXDMF(
+        trap=trap_1,
+        label="density1",
+        filename=str(Path(density_file)),
+    )
+    my_exports = festim.Exports([my_export])
+    my_model.exports = my_exports
+    my_model.initialise()
+    my_model.run()
+
+    V = FunctionSpace(my_model.mesh.mesh, "CG", 1)
+
+    density_expected = interpolate(festim.as_expression(density_expr), V)
+
+    density_read = Function(V)
+    XDMFFile(str(Path(density_file))).read_checkpoint(density_read, "density1", -1)
+
+    l2_error = errornorm(density_expected, density_read, "L2")
+    assert l2_error < 2e-3
