5b9aad88314bdba23c866e3d3ac413d101571202
Gabriele Ferrero <116028237+gabriele-ferrero@users.noreply.github.com>
2023-09-18 13:17:54 -0400

    changed path to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,7 @@ html_theme_options = {
     "use_repository_button": True,
     "use_edit_page_button": True,
     "repository_branch": "main",
-    "path_to_docs": "./docs",
+    "path_to_docs": "./docs/source",
 }
 
 html_title = "FESTIM"
