6fb6fbbaf2cb164b1c03638b88fbefe5cb7a47ab
J Dark <james.dark.95@gmail.com>
2023-10-05 11:58:33 -0400

    hydrogen transport model class

---
+++ festim/hydrogen_transport_problem.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+class HydrogenTransportProblem:
+    """
+    Hydrogen Transport Problem.
+    Used internally in festim.Model
+
+    Args:
+        geometry (festim.Geometry): the geometry of the model (mesh, function
+            spaces and subdomains)
+
+    Attributes:
+        geometry (festim.Geometry): the geometry of the model (mesh, function
+            spaces and subdomains)
+
+    """
+
+    def __init__(
+        self,
+        geometry=None,
+        species=[],
+        temperature=None,
+        boundary_conditions=[],
+        solver_parameters=None,
+        exports=[],
+    ) -> None:
+        self.geometry = geometry
+        self.species = species
+        self.temperature = temperature
+        self.boundary_conditions = boundary_conditions
+        self.solver_parameters = solver_parameters
+        self.exports = exports
+
+    def initialise(self):
+        """Initialise the model. Creates suitable function
+        spaces, the subdomains...
+        """
+
+        # geometry
+        self.geometry.define_function_space()
+        self.geometry.define_subdomains()
