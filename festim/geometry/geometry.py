e39f1c7379eb1076ed8261209d446af33391ec05
J Dark <james.dark.95@gmail.com>
2023-10-06 08:49:01 -0400

    remove attributes and inline comments

--- festim/geometry/geometry.py
+++ festim/geometry/geometry.py
@@ -1,6 +1,5 @@
 from dolfinx import fem
 import ufl
-import festim
 
 
 class Geometry:
@@ -19,15 +18,9 @@ class Geometry:
         self.mesh = mesh
         self.subdomains = subdomains
 
-        self.function_space = None
-        self.dx = None
-        self.ds = None
-
-    # create function space
     def define_function_space(self):
         elements = ufl.FiniteElement("CG", self.mesh.mesh.ufl_cell(), 1)
         self.function_space = fem.FunctionSpace(self.mesh.mesh, elements)
 
-    # create subdomains
     def define_subdomains(self):
         self.mesh.define_measures(self.function_space)
