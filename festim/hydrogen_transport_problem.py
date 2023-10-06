312cdf9e6b76001ebc6bad41fdbc50f530d6f60b
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-10-06 14:06:54 -0400

    added usage and attributes to hydrogenTransportProblem

--- festim/hydrogen_transport_problem.py
+++ festim/hydrogen_transport_problem.py
@@ -12,10 +12,40 @@ class HydrogenTransportProblem:
     Args:
         mesh (festim.Mesh): the mesh of the model
         subdomains (list of festim.Subdomain): the subdomains of the model
+        species (list of festim.Species): the species of the model
 
     Attributes:
         mesh (festim.Mesh): the mesh of the model
+        subdomains (list of festim.Subdomain): the subdomains of the model
+        species (list of festim.Species): the species of the model
+        temperature (dolfinx.Function): the temperature of the model
+        boundary_conditions (list of festim.BoundaryCondition): the boundary conditions of the model
+        solver_parameters (dict): the solver parameters of the model
+        exports (list of festim.Export): the exports of the model
+        dx (dolfinx.fem.dx): the volume measure of the model
+        ds (dolfinx.fem.ds): the surface measure of the model
+        function_space (dolfinx.fem.FunctionSpace): the function space of the model
+        facet_tags (dolfinx.cpp.mesh.MeshTags): the facet tags of the model
+        volume_tags (dolfinx.cpp.mesh.MeshTags): the volume tags of the model
 
+
+    Usage:
+        >>> import festim as F
+        >>> my_model = F.HydrogenTransportProblem()
+        >>> my_model.mesh = F.Mesh(...)
+        >>> my_model.subdomains = [F.Subdomain(...)]
+        >>> my_model.species = [F.Species(name="H"), F.Species(name="Trap")]
+        >>> my_model.initialise()
+
+        or
+
+        >>> my_model = F.HydrogenTransportProblem(
+        ...     mesh=F.Mesh(...),
+        ...     subdomains=[F.Subdomain(...)],
+        ...     species=[F.Species(name="H"), F.Species(name="Trap")],
+        ... )
+        >>> my_model.initialise()
+        
     """
 
     def __init__(
