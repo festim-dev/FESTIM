7d0dfc4976d9a3e01dfc8be4e4c106f785882b29
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-10-06 12:08:22 -0400

    added some docstrings

--- festim/species.py
+++ festim/species.py
@@ -1,13 +1,29 @@
 class Species:
-    def __init__(self, name=None) -> None:
+    """
+    Hydrogen Transport Problem.
+
+    Args:
+        name (str, optional): a name given to the species. Defaults to None.
+
+    Attributes:
+        name (str): a name given to the species.
+        solution (dolfinx.Function or ...): the solution for the current timestep
+        prev_solution (dolfinx.Function or ...): the solution for the previous timestep
+        test_function (ufl.TestFunction or ...): the testfunction associated with this species
+    """
+    def __init__(self, name:str=None) -> None:
+        """_summary_
+
+        Args:
+            name (str, optional): a name given to the species. Defaults to None.
+        """
         self.name = name
 
         self.solution = None
         self.prev_solution = None
         self.test_function = None
-        self.form = None
 
     
 class Trap(Species):
-    def __init__(self, name=None) -> None:
+    def __init__(self, name:str=None) -> None:
         super().__init__(name)
