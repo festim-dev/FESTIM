b2b0a2a5fbe8ee061d00116bc2652abfd7599e28
J Dark <james.dark.95@gmail.com>
2023-10-10 15:32:26 -0400

    meshtags and measures not made by mesh

--- festim/mesh/mesh_1d.py
+++ festim/mesh/mesh_1d.py
@@ -36,34 +36,3 @@ class Mesh1D(Mesh):
         cells = np.stack((indexes[:-1], indexes[1:]), axis=-1)
 
         return mesh.create_mesh(MPI.COMM_WORLD, cells, mesh_points, domain)
-
-    def create_meshtags(self, function_space):
-        """Creates the meshtags for a given function space
-        Args:
-            function_space (dolfinx.fem.function.FunctionSpace): the function
-                space of the model
-
-        Returns:
-            dolfinx.mesh.MeshTagsMetaClass: the tags containing the facet
-                and volume tags
-        """
-
-        dofs_L = fem.locate_dofs_geometrical(
-            function_space, lambda x: np.isclose(x[0], min(self.vertices))
-        )
-        dofs_R = fem.locate_dofs_geometrical(
-            function_space, lambda x: np.isclose(x[0], max(self.vertices))
-        )
-
-        dofs_facets = np.array([dofs_L[0], dofs_R[0]], dtype=np.int32)
-        tags_facets = np.array([1, 2], dtype=np.int32)
-
-        facet_dimension = self.mesh.topology.dim - 1
-        mesh_tags_facets = mesh.meshtags(
-            self.mesh, facet_dimension, dofs_facets, tags_facets
-        )
-
-        # TODO implement this
-        mesh_tags_volumes = None
-
-        return mesh_tags_facets, mesh_tags_volumes
