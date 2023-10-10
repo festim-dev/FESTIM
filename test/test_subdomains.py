26dde3b2580a604dae7052470a4965e5ba0fe5f3
J Dark <james.dark.95@gmail.com>
2023-10-10 16:39:35 -0400

    test ids in surface domains

--- test/test_subdomains.py
+++ test/test_subdomains.py
@@ -2,6 +2,24 @@ import numpy as np
 import festim as F
 
 
+def test_different_surface_ids():
+    my_model = F.HydrogenTransportProblem()
+
+    L = 3e-02
+    my_model.mesh = F.Mesh1D(np.linspace(0, L, num=3))
+
+    surfacec_subdomains_ids = [3, 8]
+    surface_subdomain_1 = F.SurfaceSubdomain1D(id=surfacec_subdomains_ids[0], x=0)
+    surface_subdomain_2 = F.SurfaceSubdomain1D(id=surfacec_subdomains_ids[1], x=L)
+    my_model.subdomains = [surface_subdomain_1, surface_subdomain_2]
+
+    my_model.define_function_space()
+    my_model.define_markers_and_measures()
+
+    for surf_id in surfacec_subdomains_ids:
+        assert surf_id in np.array(my_model.facet_meshtags.values)
+
+
 def test_different_volume_ids():
     my_model = F.HydrogenTransportProblem()
 
