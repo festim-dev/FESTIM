c61f5a2b4aba34d83fb6134a188fed6f062b45ce
J Dark <james.dark.95@gmail.com>
2023-10-11 14:52:03 -0400

    added detail to docstrings, required args

--- festim/subdomain/surface_subdomain.py
+++ festim/subdomain/surface_subdomain.py
@@ -13,16 +13,29 @@ class SurfaceSubdomain1D:
     Attributes:
         id (int): the id of the surface subdomain
         x (float): the x coordinate of the surface subdomain
+        dofs (np.array): the dofs of the surface subdomain
 
+    Usage:
+        >>> surf_subdomain = F.SurfaceSubdomain1D(id=1, x=1)
     """
 
-    def __init__(self, id=None, x=None) -> None:
+    def __init__(self, id, x) -> None:
         self.id = id
         self.x = x
+
         self.dofs = None
 
     def locate_dof(self, function_space):
-        """Locates the dof of the surface subdomain within the function space"""
+        """Locates the dof of the surface subdomain within the function space
+
+        Args:
+            function_space (dolfinx.fem.FunctionSpace): the function space of
+                the model
+
+        Returns:
+            dof (np.array): the first value in the list of dofs of the surface
+                subdomain
+        """
         dofs = fem.locate_dofs_geometrical(
             function_space, lambda x: np.isclose(x[0], self.x)
         )
--- festim/subdomain/volume_subdomain.py
+++ festim/subdomain/volume_subdomain.py
@@ -17,8 +17,8 @@ class VolumeSubdomain1D:
         material (festim.Material): the material of the volume subdomain
 
     Usage:
-        >>> import festim as F
-        >>> vol_subdomain = F.VolumeSubdomain1D(id=1, borders=[0, 1], material=F.Material(...))
+        >>> vol_subdomain = F.VolumeSubdomain1D(id=1, borders=[0, 1],
+                material=F.Material(...))
     """
 
     def __init__(self, id, borders, material) -> None:
@@ -27,7 +27,16 @@ class VolumeSubdomain1D:
         self.id = id
 
     def locate_subdomain_entities(self, mesh, vdim):
-        """Locates all cells in subdomain within domain"""
+        """Locates all cells in subdomain borders within domain
+
+        Args:
+            mesh (dolfinx.cpp.mesh.Mesh): the mesh of the model
+            vdim (int): the dimension of the volumes of the mesh,
+                for 1D this is always 1
+
+        Returns:
+            entities (np.array): the entities of the subdomain
+        """
         entities = locate_entities(
             mesh,
             vdim,
