27156916443ebc7b310b590aca71e4cef7527058
J Dark <james.dark.95@gmail.com>
2023-10-11 16:44:50 -0400

    doc strings and floats

--- test/test_temperature.py
+++ test/test_temperature.py
@@ -6,9 +6,9 @@ import ufl
 
 
 def test_temperature_value():
-    # Test that the temperature value is correctly set
+    """Test that the temperature value is correctly set"""
     my_model = F.HydrogenTransportProblem()
-    my_model.mesh = F.Mesh1D(vertices=np.array([0, 1, 3, 4]))
+    my_model.mesh = F.Mesh1D(vertices=np.array([0.0, 1.0, 2.0, 3.0, 4.0]))
     my_model.species = [F.Species("H")]
     my_mat = F.Material(1, 1, "1")
     my_subdomain = F.VolumeSubdomain1D(id=1, borders=[0, 4], material=my_mat)
@@ -21,8 +21,8 @@ def test_temperature_value():
 
 
 def test_temperature_type():
-    # Test that the temperature type is correctly set
-    my_mesh = F.Mesh1D(vertices=np.array([0, 1, 3, 4]))
+    """Test that the temperature type is correctly set"""
+    my_mesh = F.Mesh1D(vertices=np.array([0.0, 1.0, 2.0, 3.0, 4.0]))
     my_mesh.generate_mesh()
     values = [int(1), fem.Constant(my_mesh.mesh, 1.0), float(1.0)]
 
