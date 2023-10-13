2eafcab0ab65a14b66fcedae3385c78fcd0b0df5
J Dark <james.dark.95@gmail.com>
2023-10-13 14:09:28 -0400

    move method to Mesh1D

--- festim/mesh/mesh_1d.py
+++ festim/mesh/mesh_1d.py
@@ -1,4 +1,4 @@
-from dolfinx import mesh
+from dolfinx import mesh as dolfin_mesh
 from mpi4py import MPI
 import ufl
 import numpy as np
@@ -32,4 +32,32 @@ class Mesh1D(Mesh):
         indexes = np.arange(self.vertices.shape[0])
         cells = np.stack((indexes[:-1], indexes[1:]), axis=-1)
 
-        return mesh.create_mesh(MPI.COMM_WORLD, cells, mesh_points, domain)
+        return dolfin_mesh.create_mesh(MPI.COMM_WORLD, cells, mesh_points, domain)
+
+    def check_borders(self, volume_subdomains):
+        """Checks that the borders of the subdomain are within the domain
+
+        Args:
+            mesh (festim.Mesh): the mesh of the model
+
+        Raises:
+            Value error: if borders outside the domain
+        """
+        # check that subdomains are connected
+        all_borders = [border for vol in volume_subdomains for border in vol.borders]
+        sorted_borders = np.sort(all_borders)
+        for start, end in zip(sorted_borders[1:-2:2], sorted_borders[2:-1:2]):
+            if start != end:
+                raise ValueError("Subdomain borders don't match to each other")
+
+        # check volume subdomain is defined
+        # TODO this possible by default
+        if len(all_borders) == 0:
+            raise ValueError("No volume subdomains defined")
+
+        # check that subdomains are within the domain
+        if (
+            sorted_borders[0] != self.vertices[0]
+            or sorted_borders[-1] != self.vertices[-1]
+        ):
+            raise ValueError("borders dont match domain borders")
