54d9f21d333a3c6a7fe5c10d3062221bd2c06c5e
J Dark <james.dark.95@gmail.com>
2023-10-13 11:18:26 -0400

    fixed test

--- test/test_mesh.py
+++ test/test_mesh.py
@@ -1,14 +1,14 @@
 import festim as F
-from dolfinx import mesh
+from dolfinx import mesh as fenics_mesh
 from mpi4py import MPI
 import pytest
 
-mesh_1D = mesh.create_unit_interval(MPI.COMM_WORLD, 10)
-mesh_2D = mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
-mesh_3D = mesh.create_unit_cube(MPI.COMM_WORLD, 10, 10, 10)
+mesh_1D = fenics_mesh.create_unit_interval(MPI.COMM_WORLD, 10)
+mesh_2D = fenics_mesh.create_unit_square(MPI.COMM_WORLD, 10, 10)
+mesh_3D = fenics_mesh.create_unit_cube(MPI.COMM_WORLD, 10, 10, 10)
 
 
-@pytest.parametrize("mesh", [mesh_1D, mesh_2D, mesh_3D])
+@pytest.mark.parametrize("mesh", [mesh_1D, mesh_2D, mesh_3D])
 def test_get_fdim(mesh):
     my_mesh = F.Mesh(mesh)
 
@@ -23,8 +23,8 @@ def test_fdim_changes_when_mesh_changes():
         assert my_mesh.fdim == mesh.topology.dim - 1
 
 
-@pytest.parametrize("mesh", [mesh_1D, mesh_2D, mesh_3D])
-def test_get_vdim():
+@pytest.mark.parametrize("mesh", [mesh_1D, mesh_2D, mesh_3D])
+def test_get_vdim(mesh):
     my_mesh = F.Mesh(mesh)
 
     assert my_mesh.vdim == mesh.topology.dim
