39e0d2d6d004be13c202e64ea8cb75f8d14610a5
J Dark <james.dark.95@gmail.com>
2023-10-18 08:10:54 -0400

    function takes arguments mesh size and time

--- test/test_permeation_problem.py
+++ test/test_permeation_problem.py
@@ -11,9 +11,9 @@ import numpy as np
 import festim as F
 
 
-def test_permeation_problem():
+def test_permeation_problem(final_time=50, mesh_size=1001):
     L = 3e-04
-    vertices = np.linspace(0, L, num=1001)
+    vertices = np.linspace(0, L, num=mesh_size)
 
     my_mesh = F.Mesh1D(vertices)
 
@@ -31,8 +31,6 @@ def test_permeation_problem():
 
     temperature = Constant(my_mesh.mesh, 500.0)
     my_model.temperature = temperature
-
-    my_model.exports = [F.VTXExport("test.bp", field=mobile_H)]
 
     my_model.initialise()
 
@@ -73,8 +71,6 @@ def test_permeation_problem():
     opts[f"{option_prefix}pc_type"] = "gamg"
     opts[f"{option_prefix}pc_factor_mat_solver_type"] = "mumps"
     ksp.setFromOptions()
-
-    final_time = 50
 
     times, flux_values = my_model.run(final_time=final_time)
 
