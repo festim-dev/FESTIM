42f6cb548889702f135a1abd1ba189a1ac4f6377
J Dark <james.dark.95@gmail.com>
2023-10-20 15:12:59 -0400

    more tests and doc strings

--- test/test_material.py
+++ test/test_material.py
@@ -1,5 +1,6 @@
 import festim as F
 import numpy as np
+import pytest
 
 test_mesh = F.Mesh1D(vertices=np.array([0.0, 1.0, 2.0, 3.0, 4.0]))
 
@@ -16,7 +17,9 @@ def test_define_diffusion_coefficient():
     assert np.isclose(float(D), D_analytical)
 
 
-def test_multispecies_dict():
+def test_multispecies_dict_strings():
+    """Test that the diffusion coefficient is correctly defined when keys are
+    strings"""
     T = 500
     D_0_A, D_0_B = 1, 2
     E_D_A, E_D_B = 0.1, 0.2
@@ -32,3 +35,35 @@ def test_multispecies_dict():
     D_analytical = [D_A_analytical, D_B_analytical]
 
     assert np.isclose(D, D_analytical).all()
+
+
+def test_multispecies_dict_objects():
+    """Test that the diffusion coefficient is correctly defined when keys are
+    festim.Species objects"""
+    T = 500
+    D_0_A, D_0_B = 1, 2
+    E_D_A, E_D_B = 0.1, 0.2
+
+    A = F.Species("A")
+    B = F.Species("B")
+
+    my_mat = F.Material(D_0={A: D_0_A, B: D_0_B}, E_D={A: E_D_A, B: E_D_B})
+    D_A = my_mat.get_diffusion_coefficient(test_mesh.mesh, T, species=A)
+    D_B = my_mat.get_diffusion_coefficient(test_mesh.mesh, T, species=B)
+    D = [float(D_A), float(D_B)]
+
+    D_A_analytical = D_0_A * np.exp(-E_D_A / F.k_B / T)
+    D_B_analytical = D_0_B * np.exp(-E_D_B / F.k_B / T)
+
+    D_analytical = [D_A_analytical, D_B_analytical]
+
+    assert np.isclose(D, D_analytical).all()
+
+
+def test_multispecies_dict_different_lengths():
+    """Test that a value error is rasied when the length of the D_0 and E_D
+    are not the same"""
+    my_mat = F.Material(D_0={"A": 1, "B": 2}, E_D={"A": 0.1, "B": 0.2, "C": 0.3})
+
+    with pytest.raises(ValueError):
+        D = my_mat.get_diffusion_coefficient(test_mesh.mesh, 500, species="A")
