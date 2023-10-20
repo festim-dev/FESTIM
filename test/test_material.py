441aeeaec358c2f8418cc2b00bae1b797de44366
J Dark <james.dark.95@gmail.com>
2023-10-20 14:48:23 -0400

    added tests

--- test/test_material.py
+++ test/test_material.py
@@ -14,3 +14,21 @@ def test_define_diffusion_coefficient():
     D_analytical = D_0 * np.exp(-E_D / F.k_B / T)
 
     assert np.isclose(float(D), D_analytical)
+
+
+def test_multispecies_dict():
+    T = 500
+    D_0_A, D_0_B = 1, 2
+    E_D_A, E_D_B = 0.1, 0.2
+
+    my_mat = F.Material(D_0={"A": D_0_A, "B": D_0_B}, E_D={"A": E_D_A, "B": E_D_B})
+    D_A = my_mat.get_diffusion_coefficient(test_mesh.mesh, T, species="A")
+    D_B = my_mat.get_diffusion_coefficient(test_mesh.mesh, T, species="B")
+    D = [float(D_A), float(D_B)]
+
+    D_A_analytical = D_0_A * np.exp(-E_D_A / F.k_B / T)
+    D_B_analytical = D_0_B * np.exp(-E_D_B / F.k_B / T)
+
+    D_analytical = [D_A_analytical, D_B_analytical]
+
+    assert np.isclose(D, D_analytical).all()
---
+++ test/test_multispecies_problem.py
@@ -0,0 +1,28 @@
+import numpy as np
+import festim as F
+
+
+def test_multispecies_problem_initialisation():
+    """Test that the multispecies problem is correctly initialised"""
+    my_model = F.HydrogenTransportProblem()
+    L = 1e-04
+    my_model.mesh = F.Mesh1D(np.linspace(0, L, num=1001))
+    my_mat = F.Material(
+        D_0={"H": 1.9e-7, "D": 1.9e-07}, E_D={"H": 0.2, "D": 0.2}, name="my_mat"
+    )
+    my_subdomain = F.VolumeSubdomain1D(id=1, borders=[0, L], material=my_mat)
+    left_surface = F.SurfaceSubdomain1D(id=1, x=0)
+    right_surface = F.SurfaceSubdomain1D(id=2, x=L)
+    my_model.subdomains = [my_subdomain, left_surface, right_surface]
+    mobile_H = F.Species("H")
+    mobile_D = F.Species("D")
+    my_model.species = [mobile_H, mobile_D]
+    my_model.temperature = 600
+    my_model.boundary_conditions = [
+        F.DirichletBC(subdomain=left_surface, value=1e18, species="H"),
+        F.DirichletBC(subdomain=right_surface, value=1e18, species="D"),
+    ]
+    my_model.settings = F.Settings(atol=1e10, rtol=1e-10)
+    my_model.settings.stepsize = 0.1
+
+    my_model.initialise()
