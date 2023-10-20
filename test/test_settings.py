b9a0023660a6da3aabe0d35df2216c4f819df2fc
J Dark <james.dark.95@gmail.com>
2023-10-20 11:37:23 -0400

    updated test

--- test/test_settings.py
+++ test/test_settings.py
@@ -3,19 +3,20 @@ import numpy as np
 import pytest
 
 
-@pytest.mark.parametrize("value", [1, F.Stepsize(initial_value=23.0), 1.0, "coucou"])
-def test_stepsize_value(value):
+@pytest.mark.parametrize("test_type", [int, F.Stepsize, float])
+def test_stepsize_value(test_type):
     """Test that the stepsize is correctly set"""
-    my_model = F.HydrogenTransportProblem()
-    my_model.settings = F.Settings(atol=1, rtol=0.1)
+    test_value = 23.0
+    my_settings = F.Settings(atol=1, rtol=0.1)
+    my_settings.stepsize = test_type(test_value)
 
-    if isinstance(value, (int, float)):
-        my_model.settings.stepsize = value
-        assert isinstance(my_model.settings.stepsize, F.Stepsize)
-        assert np.isclose(my_model.settings.stepsize.initial_value, value)
-    elif isinstance(value, F.Stepsize):
-        my_model.settings.stepsize = value
-        assert np.isclose(my_model.settings.stepsize.initial_value, 23)
-    else:
-        with pytest.raises(TypeError):
-            my_model.settings.stepsize = value
+    assert isinstance(my_settings.stepsize, F.Stepsize)
+    assert np.isclose(my_settings.stepsize.initial_value, test_value)
+
+
+def test_stepsize_value_wrong_type():
+    """Checks that an error is raised when the wrong type is given"""
+    my_settings = F.Settings(atol=1, rtol=0.1)
+
+    with pytest.raises(TypeError):
+        my_settings.stepsize = "coucou"
