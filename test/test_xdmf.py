6e32e79b80d2fc547e1ff760eecb9ccb953bf223
J Dark <james.dark.95@gmail.com>
2023-10-23 11:33:15 -0400

    xdmf accept field as str

--- test/test_xdmf.py
+++ test/test_xdmf.py
@@ -65,7 +65,7 @@ def test_field_attribute_is_always_list():
     assert isinstance(my_export.field, list)
 
 
-@pytest.mark.parametrize("field", ["H", 1, [F.Species("H"), 1]])
+@pytest.mark.parametrize("field", [["H", 2], 1, [F.Species("H"), 1]])
 def test_field_attribute_raises_error_when_invalid_type(field):
     """Test that the field attribute raises an error if the type is not festim.Species or list"""
     with pytest.raises(TypeError):
@@ -82,3 +82,22 @@ def test_filename_raises_error_when_wrong_type():
     """Test that the filename attribute raises an error if the file is not str"""
     with pytest.raises(TypeError):
         F.XDMFExport(1, field=[F.Species("H")])
+
+
+def test_xdmf_field_as_string_found_in_species(tmpdir):
+    my_model = F.HydrogenTransportProblem()
+    my_model.mesh = F.Mesh1D(vertices=np.array([0.0, 1.0, 2.0, 3.0, 4.0]))
+    my_mat = F.Material(D_0=1, E_D=0, name="mat")
+    my_model.subdomains = [
+        F.VolumeSubdomain1D(1, borders=[0.0, 4.0], material=my_mat),
+    ]
+    my_model.species = [F.Species("H")]
+    my_model.temperature = 500
+
+    filename = str(tmpdir.join("my_export.xdmf"))
+    my_export = F.XDMFExport(filename, field="H")
+    my_model.exports = [my_export]
+    my_model.settings = F.Settings(atol=1, rtol=0.1)
+    my_model.settings.stepsize = F.Stepsize(initial_value=1)
+
+    my_model.initialise()
