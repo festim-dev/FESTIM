45ed02d0341bd6ea8aa88c2a564270530225089d
J Dark <james.dark.95@gmail.com>
2023-11-01 16:17:02 -0400

    store data and t, move setters to parent

--- festim/exports/surface_flux.py
+++ festim/exports/surface_flux.py
@@ -20,11 +20,10 @@ class SurfaceFlux(F.SurfaceQuantity):
         surface_subdomain: int or F.SurfaceSubdomain1D,
         filename: str = None,
     ) -> None:
-        self.field = field
-        self.surface_subdomain = surface_subdomain
-        self.filename = filename
+        super().__init__(field, surface_subdomain, filename)
 
-        self.D = None
+        self.t = []
+        self.data = []
 
         self.title = "Flux surface {}: {}".format(
             self.surface_subdomain.id, self.field.name
@@ -34,51 +33,6 @@ class SurfaceFlux(F.SurfaceQuantity):
             with open(self.filename, mode="w", newline="") as file:
                 writer = csv.writer(file)
                 writer.writerow(["Time", f"{self.title}"])
-
-    @property
-    def filename(self):
-        return self._filename
-
-    @filename.setter
-    def filename(self, value):
-        if value is None:
-            self._filename = None
-        if not isinstance(value, str):
-            raise TypeError("filename must be of type str")
-        if not value.endswith(".csv"):
-            raise ValueError("filename must end with .csv")
-        self._filename = value
-
-    @property
-    def surface_subdomain(self):
-        return self._surface_subdomain
-
-    @surface_subdomain.setter
-    def surface_subdomain(self, value):
-        if not isinstance(value, (int, F.SurfaceSubdomain1D)) or isinstance(
-            value, bool
-        ):
-            raise TypeError("surface should be an int or F.SurfaceSubdomain1D")
-        self._surface_subdomain = value
-
-    @property
-    def field(self):
-        return self._field
-
-    @field.setter
-    def field(self, value):
-        # check that field is festim.Species
-        if not isinstance(value, (F.Species, str)):
-            raise TypeError("field must be of type festim.Species")
-
-        self._field = value
-
-    @property
-    def write_to_file(self):
-        if self.filename is None:
-            return False
-        else:
-            return True
 
     def compute(self, mesh, ds):
         self.value = fem.assemble_scalar(
