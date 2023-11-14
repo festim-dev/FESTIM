ee1b519bbbc56f2219590f19d3a373bfc262619e
J Dark <james.dark.95@gmail.com>
2023-11-14 16:23:30 -0500

    update init

--- festim/__init__.py
+++ festim/__init__.py
@@ -36,8 +36,10 @@ from .source import Source
 
 from .species import Species, Trap, ImplicitSpecies, find_species_from_name
 
-from .subdomain.surface_subdomain import SurfaceSubdomain1D
-from .subdomain.volume_subdomain import VolumeSubdomain1D, find_volume_from_id
+from .subdomain.surface_subdomain import SurfaceSubdomain, find_surface_from_id
+from .subdomain.surface_subdomain_1d import SurfaceSubdomain1D
+from .subdomain.volume_subdomain import VolumeSubdomain, find_volume_from_id
+from .subdomain.volume_subdomain_1d import VolumeSubdomain1D
 
 from .stepsize import Stepsize
 
