525476c46514f6b23d8b8a2dd268180c179a671e
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-11-27 15:09:58 -0500

    initial adapt method

--- festim/stepsize.py
+++ festim/stepsize.py
@@ -17,3 +17,15 @@ class Stepsize:
         initial_value,
     ) -> None:
         self.initial_value = initial_value
+        self.growth_factor = 1.2
+        self.cutback_factor = 0.8
+        self.target_nb_iterations = 4
+
+    def adapt(self, value, nb_iterations):
+        new_value = value
+        if nb_iterations < self.target_nb_iterations:
+            new_value = value * self.growth_factor
+        elif nb_iterations > self.target_nb_iterations:
+            new_value = value * self.cutback_factor
+
+        return new_value
