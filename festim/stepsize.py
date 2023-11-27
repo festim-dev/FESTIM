ef910028efc09bdc7b38b04bf9a3408af618f5c0
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2023-11-27 15:23:11 -0500

    added is_adapt + documentation

--- festim/stepsize.py
+++ festim/stepsize.py
@@ -10,6 +10,13 @@ class Stepsize:
 
     Attributes:
         initial_value (float, int): initial stepsize.
+        growth_factor (float): factor by which the stepsize is
+            increased when adapting
+        cutback_factor (float): factor by which the stepsize is
+            decreased when adapting
+        target_nb_iterations (int): number of Newton iterations
+            over (resp. under) which the stepsize is increased
+            (resp. decreased)
     """
 
     def __init__(
@@ -21,11 +28,26 @@ class Stepsize:
         self.cutback_factor = 0.8
         self.target_nb_iterations = 4
 
-    def adapt(self, value, nb_iterations):
-        new_value = value
+    def modify_value(self, value, nb_iterations, t=None):
+        if not self.is_adapt(t):
+            return value
+
         if nb_iterations < self.target_nb_iterations:
             new_value = value * self.growth_factor
         elif nb_iterations > self.target_nb_iterations:
             new_value = value * self.cutback_factor
 
         return new_value
+
+    def is_adapt(self, t):
+        """
+        Methods that defines if the stepsize should be
+        adapted or not
+
+        Args:
+            t (float): the current time
+
+        Returns:
+            bool: True if needs to adapt, False otherwise.
+        """
+        return True
