8e80fb87cb0313571568819139a449349ce141a4
J Dark <james.dark.95@gmail.com>
2023-12-04 10:40:12 +0100

    flux bc

--- festim/__init__.py
+++ festim/__init__.py
@@ -21,6 +21,7 @@ from .helpers import as_fenics_constant
 from .boundary_conditions.dirichlet_bc import DirichletBC
 from .boundary_conditions.sieverts_bc import SievertsBC
 from .boundary_conditions.henrys_bc import HenrysBC
+from .boundary_conditions.flux_bc import FluxBC
 
 from .material import Material
 
