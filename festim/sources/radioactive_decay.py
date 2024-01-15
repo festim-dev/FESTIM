f003704baa5542d92cd92bb9f973f7bc0f917c32
Rémi Delaporte-Mathurin <40028739+RemDelaporteMathurin@users.noreply.github.com>
2024-01-15 21:34:05 +0000

    added setter and getter for decay constant

--- festim/sources/radioactive_decay.py
+++ festim/sources/radioactive_decay.py
@@ -6,5 +6,17 @@ class RadioactiveDecay(Source):
         self.decay_constant = decay_constant
         super().__init__(value=None, volume=volume, field=field)
 
+    @property
+    def decay_constant(self):
+        return self._decay_constant
+
+    @decay_constant.setter
+    def decay_constant(self, value):
+        if not isinstance(value, (float, int)):
+            raise TypeError("decay_constant must be a float or an int")
+        if value <= 0:
+            raise ValueError("decay_constant must be positive")
+        self._decay_constant = value
+
     def form(self, concentration):
         return self.decay_constant * concentration
