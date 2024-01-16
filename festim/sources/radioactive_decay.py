569a86b26ef07093df7fee315258a0b2d371f0ff
Rémi Delaporte-Mathurin <40028739+RemDelaporteMathurin@users.noreply.github.com>
2024-01-16 16:17:41 +0000

    added documentation

--- festim/sources/radioactive_decay.py
+++ festim/sources/radioactive_decay.py
@@ -2,6 +2,19 @@ from festim import Source
 
 
 class RadioactiveDecay(Source):
+    """
+    A radioactive decay source.
+    dc/dt = ... - lambda * c
+    where lambda is the decay constant in 1/s.
+
+    Args:
+        decay_constant (float): The decay constant of the source in 1/s.
+        volume (int): The volume of the source.
+        field (str, optional): The field to which the source is
+            applied. If "all" the decay will be applied to all
+            concentrations. Defaults to "all".
+    """
+
     def __init__(self, decay_constant, volume, field="all") -> None:
         self.decay_constant = decay_constant
         super().__init__(value=None, volume=volume, field=field)
