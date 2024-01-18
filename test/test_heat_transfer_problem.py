c293c1766b34924ffe24b18a15b651c005b35b89
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-01-18 14:39:53 -0500

    function to automate differenciation

--- test/test_heat_transfer_problem.py
+++ test/test_heat_transfer_problem.py
@@ -5,6 +5,27 @@ import ufl
 import mpi4py.MPI as MPI
 
 
+def source_from_exact_solution(
+    exact_solution, thermal_conductivity, density, heat_capacity
+):
+    import sympy as sp
+    from sympy.vector import CoordSys3D, divergence, gradient
+
+    R = CoordSys3D("R")
+    x = [R.x, R.y, R.z]
+    t = sp.symbols("t")
+
+    u = exact_solution(x, t)
+    density = density(x, t)
+    heat_capacity = heat_capacity(x, t)
+    thermal_cond = thermal_conductivity(x, t)
+    source = density * heat_capacity * sp.diff(u, t) - divergence(
+        thermal_cond * gradient(u, x), x
+    )
+
+    return sp.lambdify([x, t], source)
+
+
 def error_L2(u_computed, u_exact, degree_raise=3):
     # Create higher order function space
     degree = u_computed.function_space.ufl_element().degree()
@@ -178,6 +199,75 @@ def test_heat_transfer_transient():
     final_time_sim = (
         my_problem.t.value
     )  # we use the exact final time of the simulation which may differ from the one specified in the settings
+    exact_solution_end = lambda x: exact_solution(x, final_time_sim)
+    L2_error = error_L2(computed_solution, exact_solution_end)
+    assert L2_error < 1e-7
+
+
+def test_sympify():
+    exact_solution = lambda x, t: 2 * x[0] ** 2 + 20 * t
+
+    density = lambda T: 0.2 * T + 2
+    heat_capacity = lambda T: 0.2 * T + 3
+    thermal_conductivity = lambda T: 0.1 * T + 4
+
+    mms_source_from_sp = source_from_exact_solution(
+        exact_solution,
+        density=lambda x, t: density(exact_solution(x, t)),
+        heat_capacity=lambda x, t: heat_capacity(exact_solution(x, t)),
+        thermal_conductivity=lambda x, t: thermal_conductivity(exact_solution(x, t)),
+    )
+    mms_source = lambda x, t: mms_source_from_sp((x[0], None, None), t)
+
+    my_problem = F.HeatTransferProblem()
+
+    my_problem.mesh = F.Mesh1D(vertices=np.linspace(2, 3, 2100))
+    left = F.SurfaceSubdomain1D(id=1, x=2)
+    right = F.SurfaceSubdomain1D(id=2, x=3)
+    my_problem.surface_subdomains = [left, right]
+    mat = F.Material(D_0=None, E_D=None)
+    mat.thermal_conductivity = thermal_conductivity
+    mat.density = density
+    mat.heat_capacity = heat_capacity
+
+    my_problem.volume_subdomains = [
+        F.VolumeSubdomain1D(id=1, borders=[2, 3], material=mat)
+    ]
+
+    # NOTE: it's good to check that without the IC the solution is not the exact one
+    my_problem.initial_condition = F.InitialTemperature(lambda x: exact_solution(x, 0))
+
+    my_problem.boundary_conditions = [
+        F.FixedTemperatureBC(subdomain=left, value=exact_solution),
+        F.FixedTemperatureBC(subdomain=right, value=exact_solution),
+    ]
+
+    my_problem.sources = [
+        F.HeatSource(value=mms_source, volume=my_problem.volume_subdomains[0])
+    ]
+
+    my_problem.settings = F.Settings(
+        atol=1e-8,
+        rtol=1e-10,
+        final_time=1,  # final time shouldn't be too long so that a potential error at the initial timestep is not negligible
+    )
+
+    # Forward euler isn't great so dt should be small
+    # although it's ok here since the time derivative is constant
+    my_problem.settings.stepsize = F.Stepsize(0.05)
+
+    my_problem.exports = [
+        F.VTXExportForTemperature(filename="test_transient_heat_transfer.bp")
+    ]
+
+    my_problem.initialise()
+    my_problem.run()
+
+    computed_solution = my_problem.u
+    final_time_sim = (
+        my_problem.t.value
+    )  # we use the exact final time of the simulation which may differ from the one specified in the settings
+
     exact_solution_end = lambda x: exact_solution(x, final_time_sim)
     L2_error = error_L2(computed_solution, exact_solution_end)
     assert L2_error < 1e-7
