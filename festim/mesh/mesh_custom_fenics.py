02f0591b1154b029115e748bff3551a7aea94824
J Dark <james.dark.95@gmail.com>
2024-01-19 16:21:51 +0100

    remove if None

--- festim/mesh/mesh_custom_fenics.py
+++ festim/mesh/mesh_custom_fenics.py
@@ -23,9 +23,7 @@ class CustomFenicsMesh(F.Mesh):
 
     @mesh.setter
     def mesh(self, value):
-        if value is None:
-            self._mesh = value
-        elif isinstance(value, dolfinx.mesh.Mesh):
+        if isinstance(value, dolfinx.mesh.Mesh):
             self._mesh = value
         else:
             raise TypeError(f"Mesh must be of type dolfinx.mesh.Mesh")
@@ -36,9 +34,7 @@ class CustomFenicsMesh(F.Mesh):
 
     @surface_meshtags.setter
     def surface_meshtags(self, value):
-        if value is None:
-            self._surface_meshtags = value
-        elif isinstance(value, dolfinx.mesh.MeshTags):
+        if isinstance(value, dolfinx.mesh.MeshTags):
             self._surface_meshtags = value
         else:
             raise TypeError(f"value must be of type dolfinx.mesh.MeshTags")
@@ -49,9 +45,7 @@ class CustomFenicsMesh(F.Mesh):
 
     @volume_meshtags.setter
     def volume_meshtags(self, value):
-        if value is None:
-            self._volume_meshtags = value
-        elif isinstance(value, dolfinx.mesh.MeshTags):
+        if isinstance(value, dolfinx.mesh.MeshTags):
             self._volume_meshtags = value
         else:
             raise TypeError(f"value must be of type dolfinx.mesh.MeshTags")
@@ -65,6 +59,5 @@ class CustomFenicsMesh(F.Mesh):
         super().__init__(mesh=mesh)
 
         self.mesh = mesh
-
         self.surface_meshtags = surface_meshtags
         self.volume_meshtags = volume_meshtags
