0c5f7e91b86e73d10aee833e6960f57c926b5fa6
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-01-23 14:41:14 -0500

    docstrings to HydrogenFlux

--- festim/exports/derived_quantities/hydrogen_flux.py
+++ festim/exports/derived_quantities/hydrogen_flux.py
@@ -2,5 +2,7 @@ from festim import SurfaceFlux
 
 
 class HydrogenFlux(SurfaceFlux):
+    """Equivalent to SurfaceFlux("solute", ...)"""
+
     def __init__(self, surface) -> None:
         super().__init__(field="solute", surface=surface)
