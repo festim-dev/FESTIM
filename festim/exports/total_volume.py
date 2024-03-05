5656413d06876be4ed66e26bbbc18ad1d57dcc08
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-03-05 08:20:23 -0500

    volume id

--- festim/exports/total_volume.py
+++ festim/exports/total_volume.py
@@ -12,4 +12,4 @@ class TotalVolume(F.VolumeQuantity):
         super().__init__(field, volume, filename)
 
     def compute(self, dx):
-        return fem.assemble_scalar(fem.form(self.field.solution * dx(self.volume)))
+        return fem.assemble_scalar(fem.form(self.field.solution * dx(self.volume.id)))
