7f241e7566f02797eefbe6d05ebdc647641808d4
James Dark <65899899+jhdark@users.noreply.github.com>
2024-04-08 07:24:39 +0000

    method in parent class

--- festim/boundary_conditions/flux_bc.py
+++ festim/boundary_conditions/flux_bc.py
@@ -61,57 +61,6 @@ class FluxBCBase:
         else:
             return False
 
-    def update(self, t):
-        """Updates the flux bc value
-
-        Args:
-            t (float): the time
-        """
-        if callable(self.value):
-            arguments = self.value.__code__.co_varnames
-            if isinstance(self.value_fenics, fem.Constant) and "t" in arguments:
-                self.value_fenics.value = self.value(t=t)
-            else:
-                self.value_fenics.interpolate(self.bc_expr)
-
-
-class ParticleFluxBC(FluxBCBase):
-    """
-    Particle flux boundary condition class
-
-    Args:
-        subdomain (festim.SurfaceSubdomain): the surface subdomain where the particle flux
-            is applied
-        value (float or fem.Constant): the value of the particle flux
-        species (festim.Species): the species to which the flux is applied
-
-    Attributes:
-        subdomain (festim.SurfaceSubdomain): the surface subdomain where the particle flux
-            is applied
-        value (float or fem.Constant): the value of the particle flux
-        species (festim.Species): the species to which the flux is applied
-        value_fenics (fem.Function or fem.Constant): the value of the particle flux in
-            fenics format
-        bc_expr (fem.Expression): the expression of the particle flux that is used to
-            update the value_fenics
-
-
-    Usage:
-        >>> from festim import ParticleFluxBC
-        >>> ParticleFluxBC(subdomain=my_subdomain, value=1, species="H")
-        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda x: 1 + x[0], species="H")
-        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda t: 1 + t, species="H")
-        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda T: 1 + T, species="H")
-        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda x, t: 1 + x[0] + t, species="H")
-    """
-
-    def __init__(self, subdomain, value, species):
-        super().__init__(subdomain=subdomain, value=value)
-        self.species = species
-
-        self.value_fenics = None
-        self.bc_expr = None
-
     @property
     def temperature_dependent(self):
         if self.value is None:
@@ -175,6 +124,54 @@ class ParticleFluxBC(FluxBCBase):
                 )
                 self.value_fenics.interpolate(self.bc_expr)
 
+    def update(self, t):
+        """Updates the flux bc value
+
+        Args:
+            t (float): the time
+        """
+        if callable(self.value):
+            arguments = self.value.__code__.co_varnames
+            if isinstance(self.value_fenics, fem.Constant) and "t" in arguments:
+                self.value_fenics.value = self.value(t=t)
+            else:
+                self.value_fenics.interpolate(self.bc_expr)
+
+
+class ParticleFluxBC(FluxBCBase):
+    """
+    Particle flux boundary condition class
+
+    Args:
+        subdomain (festim.SurfaceSubdomain): the surface subdomain where the particle flux
+            is applied
+        value (float or fem.Constant): the value of the particle flux
+        species (festim.Species): the species to which the flux is applied
+
+    Attributes:
+        subdomain (festim.SurfaceSubdomain): the surface subdomain where the particle flux
+            is applied
+        value (float or fem.Constant): the value of the particle flux
+        species (festim.Species): the species to which the flux is applied
+        value_fenics (fem.Function or fem.Constant): the value of the particle flux in
+            fenics format
+        bc_expr (fem.Expression): the expression of the particle flux that is used to
+            update the value_fenics
+
+
+    Usage:
+        >>> from festim import ParticleFluxBC
+        >>> ParticleFluxBC(subdomain=my_subdomain, value=1, species="H")
+        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda x: 1 + x[0], species="H")
+        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda t: 1 + t, species="H")
+        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda T: 1 + T, species="H")
+        >>> ParticleFluxBC(subdomain=my_subdomain, value=lambda x, t: 1 + x[0] + t, species="H")
+    """
+
+    def __init__(self, subdomain, value, species):
+        super().__init__(subdomain=subdomain, value=value)
+        self.species = species
+
 
 class HeatFluxBC(FluxBCBase):
     """
@@ -205,54 +202,3 @@ class HeatFluxBC(FluxBCBase):
 
     def __init__(self, subdomain, value):
         super().__init__(subdomain=subdomain, value=value)
-
-        self.value_fenics = None
-        self.bc_expr = None
-
-    def create_value_fenics(
-        self, mesh, function_space: fem.FunctionSpace, t: fem.Constant
-    ):
-        """Creates the value of the boundary condition as a fenics object and sets it to
-        self.value_fenics.
-        If the value is a constant, it is converted to a fenics.Constant.
-        If the value is a function of t, it is converted to a fenics.Constant.
-        Otherwise, it is converted to a fenics.Function and the
-        expression of the function is stored in self.bc_expr.
-
-        Args:
-            mesh (dolfinx.mesh.Mesh) : the mesh
-            function_space (dolfinx.fem.FunctionSpace): the function space
-            t (dolfinx.fem.Constant): the time
-        """
-        x = ufl.SpatialCoordinate(mesh)
-
-        if isinstance(self.value, (int, float)):
-            self.value_fenics = F.as_fenics_constant(mesh=mesh, value=self.value)
-
-        elif callable(self.value):
-            arguments = self.value.__code__.co_varnames
-
-            if "t" in arguments and "x" not in arguments:
-                # only t is an argument
-                if not isinstance(self.value(t=float(t)), (float, int)):
-                    raise ValueError(
-                        f"self.value should return a float or an int, not {type(self.value(t=float(t)))} "
-                    )
-                self.value_fenics = F.as_fenics_constant(
-                    mesh=mesh, value=self.value(t=float(t))
-                )
-            else:
-                self.value_fenics = fem.Function(function_space)
-                kwargs = {}
-                if "t" in arguments:
-                    kwargs["t"] = t
-                if "x" in arguments:
-                    kwargs["x"] = x
-
-                # store the expression of the boundary condition
-                # to update the value_fenics later
-                self.bc_expr = fem.Expression(
-                    self.value(**kwargs),
-                    function_space.element.interpolation_points(),
-                )
-                self.value_fenics.interpolate(self.bc_expr)
