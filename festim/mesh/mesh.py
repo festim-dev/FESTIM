309f9eeba2b7d86fd13cc26db97dcaaff286adc1
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-04-17 13:33:53 -0400

    default behaviour tags everywhere + more generic

--- festim/mesh/mesh.py
+++ festim/mesh/mesh.py
@@ -1,5 +1,7 @@
 import ufl
 import dolfinx
+import numpy as np
+from dolfinx.mesh import meshtags
 
 
 class Mesh:
@@ -52,3 +54,42 @@ class Mesh:
     @property
     def n(self):
         return ufl.FacetNormal(self.mesh)
+
+    def define_meshtags(self, surface_subdomains, volume_subdomains):
+        """Defines the facet and volume meshtags of the mesh
+
+        Args:
+            surface_subdomains (list of festim.SufaceSubdomains): the surface subdomains of the model
+            volume_subdomains (list of festim.VolumeSubdomains): the volume subdomains of the model
+
+        Returns:
+            dolfinx.mesh.MeshTags: the facet meshtags
+            dolfinx.mesh.MeshTags: the volume meshtags
+        """
+        # find all cells in domain and mark them as 0
+        num_cells = self.mesh.topology.index_map(self.vdim).size_local
+        mesh_cell_indices = np.arange(num_cells, dtype=np.int32)
+        tags_volumes = np.full(num_cells, 0, dtype=np.int32)
+
+        # find all facets in domain and mark them as 0
+        mesh_facet_indices = dolfinx.mesh.exterior_facet_indices(self.mesh.topology)
+        tags_facets = np.full_like(mesh_facet_indices, 0, dtype=np.int32)
+
+        for surf in surface_subdomains:
+            entities = surf.locate_boundary_facet_indices(self.mesh, self.fdim)
+
+            # in tags_facets, where the value is in entities, set the value to surf.id
+            mask = np.isin(mesh_facet_indices, entities)
+            tags_facets[mask] = surf.id
+        for vol in volume_subdomains:
+            # find all cells in subdomain and mark them as sub_dom.id
+            entities = vol.locate_subdomain_entities(self.mesh, self.vdim)
+            tags_volumes[entities] = vol.id
+
+        # define mesh tags
+        facet_meshtags = meshtags(self.mesh, self.fdim, mesh_facet_indices, tags_facets)
+        volume_meshtags = meshtags(
+            self.mesh, self.vdim, mesh_cell_indices, tags_volumes
+        )
+
+        return facet_meshtags, volume_meshtags
--- festim/subdomain/surface_subdomain.py
+++ festim/subdomain/surface_subdomain.py
@@ -1,3 +1,7 @@
+import dolfinx.mesh
+import numpy as np
+
+
 class SurfaceSubdomain:
     """
     Surface subdomain class
@@ -8,6 +12,24 @@ class SurfaceSubdomain:
 
     def __init__(self, id):
         self.id = id
+
+    def locate_boundary_facet_indices(self, mesh, fdim):
+        """Locates the dof of the surface subdomain within the function space
+        and return the index of the dof
+
+        Args:
+            mesh (dolfinx.mesh.Mesh): the mesh of the simulation
+            fdim (int): the dimension of the model facets
+
+        Returns:
+            index (np.array): the first value in the list of surface facet
+                indices of the subdomain
+        """
+        # By default, all entities are included
+        indices = dolfinx.mesh.locate_entities_boundary(
+            mesh, fdim, lambda x: np.full(x.shape[1], True, dtype=bool)
+        )
+        return indices[0]
 
 
 def find_surface_from_id(id: int, surfaces: list):
