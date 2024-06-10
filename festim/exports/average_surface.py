Correct average compute and entity/indice as arg

festim/exports/average_surface.py
@@ -1,4 +1,5 @@
 import festim as F
+from dolfinx import fem
 import numpy as np
 
 
@@ -26,10 +27,13 @@ class AverageSurface(F.SurfaceQuantity):
     def title(self):
         return f"Average {self.field.name} surface {self.surface.id}"
 
-    def compute(self):
+    def compute(self, ds):
         """
         Computes the average value of the field on the defined surface
         subdomain, and appends it to the data list
         """
-        self.value = np.mean(self.field.solution.x.array[self.surface.indices])
+
+        self.value = fem.assemble_scalar(
+            fem.form(self.field.solution * ds(self.surface.id))
+        ) / fem.assemble_scalar(fem.form(1 * ds(self.surface.id)))
         self.data.append(self.value)

festim/exports/average_volume.py
@@ -1,4 +1,5 @@
 import festim as F
+from dolfinx import fem
 import numpy as np
 
 
@@ -26,10 +27,12 @@ class AverageVolume(F.VolumeQuantity):
     def title(self):
         return f"Average {self.field.name} volume {self.volume.id}"
 
-    def compute(self):
+    def compute(self, ds):
         """
         Computes the average value of solution function within the defined volume
         subdomain, and appends it to the data list
         """
-        self.value = np.mean(self.field.solution.x.array[self.volume.entities])
+        self.value = fem.assemble_scalar(
+            fem.form(self.field.solution * ds(self.surface.id))
+        ) / fem.assemble_scalar(fem.form(1 * ds(self.surface.id)))
         self.data.append(self.value)

festim/exports/maximum_surface.py
@@ -31,5 +31,5 @@ class MaximumSurface(F.SurfaceQuantity):
         Computes the maximum value of the field on the defined surface
         subdomain, and appends it to the data list
         """
-        self.value = np.max(self.field.solution[self.surface.indices])
+        self.value = np.max(self.field.solution.x.array[self.surface.indices])
         self.data.append(self.value)

festim/subdomain/surface_subdomain_1d.py
@@ -35,7 +35,7 @@ class SurfaceSubdomain1D(F.SurfaceSubdomain):
             index (np.array): the first value in the list of surface facet
                 indices of the subdomain
         """
-        indices = dolfinx.mesh.locate_entities_boundary(
+        self.indices = dolfinx.mesh.locate_entities_boundary(
             mesh, fdim, lambda x: np.isclose(x[0], self.x)
         )
-        return indices[0]
+        return self.indices[0]

test/test_average_surface.py
@@ -1,10 +1,12 @@
 import festim as F
 import numpy as np
 from dolfinx import fem
+import ufl
+from dolfinx.mesh import meshtags
 import pytest
 
 
-def test_average_surface_compute():
+def test_average_surface_compute_1D():
     """Test that the average surface export computes the correct value"""
 
     # BUILD
@@ -12,11 +14,24 @@ def test_average_surface_compute():
     D = 1.5
     my_mesh = F.Mesh1D(np.linspace(0, L, 10000))
     dummy_surface = F.SurfaceSubdomain1D(id=1, x=6)
+    dummy_surface.locate_boundary_facet_indices(mesh=my_mesh.mesh, fdim=my_mesh.fdim)
+
+    # define mesh ds measure
+    facet_indices = np.array(
+        dummy_surface.locate_boundary_facet_indices(my_mesh.mesh, 0),
+        dtype=np.int32,
+    )
+    tags_facets = np.array(
+        [1],
+        dtype=np.int32,
+    )
+    facet_meshtags = meshtags(my_mesh.mesh, 0, facet_indices, tags_facets)
+    ds = ufl.Measure("ds", domain=my_mesh.mesh, subdomain_data=facet_meshtags)
 
     # give function to species
     V = fem.functionspace(my_mesh.mesh, ("CG", 1))
     c = fem.Function(V)
-    c.interpolate(lambda x: x[0] * 0.5 + 1)
+    c.interpolate(lambda x: x[0] * 0.5 + 2)
 
     my_species = F.Species("H")
     my_species.solution = c
@@ -25,10 +40,10 @@ def test_average_surface_compute():
     my_export.D = D
 
     # RUN
-    my_export.compute()
+    my_export.compute(ds)
 
     # TEST
-    expected_value = 2.5
+    expected_value = 5.0
     computed_value = my_export.value
 
     assert np.isclose(computed_value, expected_value, rtol=1e-2)

test/test_maximum_surface.py
@@ -3,7 +3,7 @@ import numpy as np
 from dolfinx import fem
 
 
-def test_maximum_surface():
+def test_maximum_surface_compute_1D():
     """Test that the maximum surface export computes the correct value"""
 
     # BUILD
@@ -11,6 +11,7 @@ def test_maximum_surface():
     D = 1.5
     my_mesh = F.Mesh1D(np.linspace(0, L, 10000))
     dummy_surface = F.SurfaceSubdomain1D(id=1, x=4)
+    dummy_surface.locate_boundary_facet_indices(mesh=my_mesh.mesh, fdim=0)
 
     # give function to species
     V = fem.functionspace(my_mesh.mesh, ("CG", 1))
@@ -27,7 +28,7 @@ def test_maximum_surface():
     my_export.compute()
 
     # TEST
-    expected_value = 9
+    expected_value = 1.0
     computed_value = my_export.value
 
     assert np.isclose(computed_value, expected_value, rtol=1e-2)

test/test_maximum_volume.py
@@ -5,7 +5,7 @@ import ufl
 from dolfinx import fem
 
 
-def test_maximum_volume_compute():
+def test_maximum_volume_compute_1D():
     """Test that the maximum volume export computes the right value"""
 
     # BUILD
@@ -13,6 +13,7 @@ def test_maximum_volume_compute():
     dummy_material = F.Material(D_0=1.5, E_D=1, name="dummy")
     my_mesh = F.Mesh1D(np.linspace(0, L, 10000))
     dummy_volume = F.VolumeSubdomain1D(id=1, borders=[0, L], material=dummy_material)
+    dummy_volume.locate_subdomain_entities(mesh=my_mesh.mesh, vdim=0)
 
     # define mesh dx measure
     num_cells = my_mesh.mesh.topology.index_map(my_mesh.vdim).size_local

test/test_minimum_surface.py
@@ -7,7 +7,7 @@ import pytest
 import os
 
 
-def test_minimum_surface_export_compute():
+def test_minimum_surface_export_compute_1D():
     """Test that the minimum surface export computes the correct value"""
 
     # BUILD
@@ -15,6 +15,7 @@ def test_minimum_surface_export_compute():
     D = 1.5
     my_mesh = F.Mesh1D(np.linspace(0, L, 10000))
     dummy_surface = F.SurfaceSubdomain1D(id=1, x=4)
+    dummy_surface.locate_boundary_facet_indices(mesh=my_mesh.mesh, fdim=my_mesh.fdim)
 
     # give function to species
     V = fem.functionspace(my_mesh.mesh, ("CG", 1))
@@ -31,7 +32,7 @@ def test_minimum_surface_export_compute():
     my_export.compute()
 
     # TEST
-    expected_value = 0
+    expected_value = 4.0
     computed_value = my_export.value
 
     assert np.isclose(computed_value, expected_value, rtol=1e-2)

test/test_total_surface.py
@@ -5,7 +5,7 @@ from dolfinx.mesh import meshtags
 from dolfinx import fem
 
 
-def test_total_surface_compute():
+def test_total_surface_compute_1D():
     """Test that the total surface export computes the correct value"""
 
     # BUILD
@@ -41,7 +41,7 @@ def test_total_surface_compute():
     my_export.compute(ds)
 
     # TEST
-    expected_value = 15
+    expected_value = 4.0
     computed_value = my_export.value
 
     assert np.isclose(computed_value, expected_value, rtol=1e-2)