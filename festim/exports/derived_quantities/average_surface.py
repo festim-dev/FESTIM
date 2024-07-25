fb31540a39fdedd02f7da4af9bb29ef6209c697a
J Dark <james.dark.95@gmail.com>
2024-07-25 23:19:08 +0200

    first effort

--- festim/exports/derived_quantities/average_surface.py
+++ festim/exports/derived_quantities/average_surface.py
@@ -1,5 +1,6 @@
 from festim import SurfaceQuantity
 import fenics as f
+import numpy as np
 
 
 class AverageSurface(SurfaceQuantity):
@@ -43,3 +44,138 @@ class AverageSurface(SurfaceQuantity):
         return f.assemble(self.function * self.ds(self.surface)) / f.assemble(
             1 * self.ds(self.surface)
         )
+
+
+class AverageSurfaceCylindrical(AverageSurface):
+    """
+    Computes the average value of a field on a given surface
+    int(f dS) / int (1 * dS)
+    dS is the surface measure in cylindrical coordinates.
+    dS = r dr dtheta
+
+    Args:
+        field (str, int):  the field ("solute", 0, 1, "T", "retention")
+        surface (int): the surface id
+        title (str): the title of the derived quantity
+        show_units (bool): show the units in the title in the derived quantities
+            file
+        function (dolfin.function.function.Function): the solution function of
+            the field
+        azimuth_range (tuple, optional): Range of the azimuthal angle
+            (theta) needs to be between 0 and 2 pi. Defaults to (0, 2 * np.pi)
+
+    Notes:
+        Units are in H/m3 for hydrogen concentration and K for temperature
+    """
+
+    def __init__(self, field, surface, azimuth_range=(0, 2 * np.pi)) -> None:
+        super().__init__(field=field, surface=surface)
+        self.r = None
+        self.azimuth_range = azimuth_range
+
+    @property
+    def azimuth_range(self):
+        return self._azimuth_range
+
+    @azimuth_range.setter
+    def azimuth_range(self, value):
+        if value[0] < 0 or value[1] > 2 * np.pi:
+            raise ValueError("Azimuthal range must be between 0 and pi")
+        self._azimuth_range = value
+
+    def compute(self):
+
+        if self.r is None:
+            mesh = (
+                self.function.function_space().mesh()
+            )  # get the mesh from the function
+            rthetaz = f.SpatialCoordinate(mesh)  # get the coordinates from the mesh
+            self.r = rthetaz[0]  # only care about r here
+
+        # dS_z = r dr dtheta , assuming axisymmetry dS_z = theta r dr
+        # dS_r = r dz dtheta , assuming axisymmetry dS_r = theta r dz
+        # in both cases the expression with self.dx is the same
+
+        avg_surf = f.assemble(
+            self.function * self.r * self.ds(self.surface)
+        ) / f.assemble(1 * self.r * self.ds(self.surface))
+
+        avg_surf *= self.azimuth_range[1] - self.azimuth_range[0]
+
+        return avg_surf
+
+
+class AverageSurfaceSpherical(AverageSurface):
+    """
+    Computes the average value of a field in a given volume
+    int(f dx) / int (1 * dx)
+    dx is the volume measure in cylindrical coordinates.
+    dx = r dr dtheta
+
+    Args:
+        field (str, int):  the field ("solute", 0, 1, "T", "retention")
+        surface (int): the surface id
+        title (str): the title of the derived quantity
+        show_units (bool): show the units in the title in the derived quantities
+            file
+        function (dolfin.function.function.Function): the solution function of
+            the field
+        azimuth_range (tuple, optional): Range of the azimuthal angle
+            (theta) needs to be between 0 and 2 pi. Defaults to (0, 2 * np.pi)
+        polar_range (tuple, optional): Range of the polar angle
+            (phi) needs to be between - pi and pi. Defaults to (-np.pi, np.pi)
+
+    Notes:
+        Units are in H/m3 for hydrogen concentration and K for temperature
+    """
+
+    def __init__(
+        self, field, surface, azimuth_range=(0, 2 * np.pi), polar_range=(-np.pi, np.pi)
+    ) -> None:
+        super().__init__(field=field, surface=surface)
+        self.r = None
+        self.azimuth_range = azimuth_range
+        self.polar_range = polar_range
+
+    @property
+    def azimuth_range(self):
+        return self._azimuth_range
+
+    @azimuth_range.setter
+    def azimuth_range(self, value):
+        if value[0] < 0 or value[1] > 2 * np.pi:
+            raise ValueError("Azimuthal range must be between 0 and pi")
+        self._azimuth_range = value
+
+    @property
+    def polar_range(self):
+        return self._polar_range
+
+    @polar_range.setter
+    def polar_range(self, value):
+        if value[0] < -np.pi or value[1] > np.pi:
+            raise ValueError("Polar range must be between - pi and pi")
+        self._polar_range = value
+
+    def compute(self):
+
+        if self.r is None:
+            mesh = (
+                self.function.function_space().mesh()
+            )  # get the mesh from the function
+            rthetaz = f.SpatialCoordinate(mesh)  # get the coordinates from the mesh
+            self.r = rthetaz[0]  # only care about r here
+
+        # dV_z = r dr dtheta , assuming axisymmetry dV_z = theta r dr
+        # dV_r = r dz dtheta , assuming axisymmetry dV_r = theta r dz
+        # in both cases the expression with self.dx is the same
+
+        avg_vol = f.assemble(
+            self.function * self.r**2 * self.dx(self.volume)
+        ) / f.assemble(1 * self.r**2 * self.dx(self.volume))
+
+        avg_vol *= (self.polar_range[1] - self.polar_range[0]) * (
+            -np.cos(self.azimuth_range[1]) + np.cos(self.azimuth_range[0])
+        )
+
+        return avg_vol
--- festim/exports/derived_quantities/average_volume.py
+++ festim/exports/derived_quantities/average_volume.py
@@ -1,5 +1,6 @@
 from festim import VolumeQuantity
 import fenics as f
+import numpy as np
 
 
 class AverageVolume(VolumeQuantity):
@@ -42,3 +43,143 @@ class AverageVolume(VolumeQuantity):
         return f.assemble(self.function * self.dx(self.volume)) / f.assemble(
             1 * self.dx(self.volume)
         )
+
+
+class AverageVolumeCylindrical(AverageVolume):
+    """
+    Computes the average value of a field in a given volume
+    int(f dx) / int (1 * dx)
+    dx is the volume measure in cylindrical coordinates.
+    dx = r dr dz dtheta
+
+    Note: for particle fluxes J is given in H/s, for heat fluxes J is given in W
+
+    Args:
+        field (str, int):  the field ("solute", 0, 1, "T", "retention")
+        volume (int): the volume id
+        title (str): the title of the derived quantity
+        show_units (bool): show the units in the title in the derived quantities
+            file
+        function (dolfin.function.function.Function): the solution function of
+            the field
+        azimuth_range (tuple, optional): Range of the azimuthal angle
+            (theta) needs to be between 0 and 2 pi. Defaults to (0, 2 * np.pi).
+    """
+
+    def __init__(self, field, volume, z, azimuth_range=(0, 2 * np.pi)) -> None:
+        super().__init__(field=field, volume=volume)
+        self.r = None
+        self.z = z
+        self.azimuth_range = azimuth_range
+
+    @property
+    def azimuth_range(self):
+        return self._azimuth_range
+
+    @azimuth_range.setter
+    def azimuth_range(self, value):
+        if value[0] < 0 or value[1] > 2 * np.pi:
+            raise ValueError("Azimuthal range must be between 0 and pi")
+        self._azimuth_range = value
+
+    def compute(self):
+
+        if self.r is None:
+            mesh = (
+                self.function.function_space().mesh()
+            )  # get the mesh from the function
+            rthetaz = f.SpatialCoordinate(mesh)  # get the coordinates from the mesh
+            self.r = rthetaz[0]  # only care about r here
+
+        values = f.assemble(self.function * self.r * self.z * self.dx(self.volume)) * (
+            self.azimuth_range[1] - self.azimuth_range[0]
+        )
+
+        # volume = f.assemble(1 * self.r * self.z * self.dx(self.volume)) * (
+        #     self.azimuth_range[1] - self.azimuth_range[0]
+        # )
+        volume = f.assemble(1 * self.r * self.z * self.dx(self.volume)) * (
+            self.azimuth_range[1] - self.azimuth_range[0]
+        )
+
+        avg_vol = values / volume
+        avg_vol *= self.azimuth_range[1] - self.azimuth_range[0]
+
+        return avg_vol
+
+
+class AverageVolumeSpherical(AverageVolume):
+    """
+    Computes the average value of a field in a given volume
+    int(f dx) / int (1 * dx)
+    dx is the volume measure in cylindrical coordinates.
+    dx = rho dtheta dphi
+
+    Note: for particle fluxes J is given in H/s, for heat fluxes J is given in W
+
+    Args:
+        field (str, int):  the field ("solute", 0, 1, "T", "retention")
+        volume (int): the volume id
+        title (str): the title of the derived quantity
+        show_units (bool): show the units in the title in the derived quantities
+            file
+        function (dolfin.function.function.Function): the solution function of
+            the field
+        azimuth_range (tuple, optional): Range of the azimuthal angle
+            (theta) needs to be between 0 and pi. Defaults to (0, np.pi)
+        polar_range (tuple, optional): Range of the polar angle
+            (phi) needs to be between - pi and pi. Defaults to (-np.pi, np.pi)
+    """
+
+    def __init__(
+        self, field, volume, azimuth_range=(0, np.pi), polar_range=(-np.pi, np.pi)
+    ) -> None:
+        super().__init__(field=field, volume=volume)
+        self.r = None
+        self.azimuth_range = azimuth_range
+        self.polar_range = polar_range
+
+    @property
+    def polar_range(self):
+        return self._polar_range
+
+    @polar_range.setter
+    def polar_range(self, value):
+        if value[0] < -np.pi or value[1] > np.pi:
+            raise ValueError("Polar range must be between - pi and pi")
+        self._polar_range = value
+
+    @property
+    def azimuth_range(self):
+        return self._azimuth_range
+
+    @azimuth_range.setter
+    def azimuth_range(self, value):
+        if value[0] < 0 or value[1] > np.pi:
+            raise ValueError("Azimuthal range must be between 0 and pi")
+        self._azimuth_range = value
+
+    def compute(self):
+
+        if self.r is None:
+            mesh = (
+                self.function.function_space().mesh()
+            )  # get the mesh from the function
+            rthetaphi = f.SpatialCoordinate(mesh)  # get the coordinates from the mesh
+            self.r = rthetaphi[0]  # only care about r here
+
+        values = (
+            f.assemble(self.function * self.r**2 * self.dx(self.volume))
+            * (self.polar_range[1] - self.polar_range[0])
+            * (-np.cos(self.azimuth_range[1]) + np.cos(self.azimuth_range[0]))
+        )
+
+        volume = (
+            f.assemble(1 * self.r**2 * self.dx(self.volume))
+            * (self.polar_range[1] - self.polar_range[0])
+            * (-np.cos(self.azimuth_range[1]) + np.cos(self.azimuth_range[0]))
+        )
+
+        avg_vol = values / volume
+
+        return avg_vol
