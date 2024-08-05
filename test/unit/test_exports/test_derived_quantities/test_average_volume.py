5944b682f8ad9dc5237bb944b7d90c89b9ccc940
JDark <james.dark.95@gmail.com>
2024-08-05 15:45:19 +0200

    test average volume

--- test/unit/test_exports/test_derived_quantities/test_average_volume.py
+++ test/unit/test_exports/test_derived_quantities/test_average_volume.py
@@ -1,6 +1,9 @@
-from festim import AverageVolume
+from festim import AverageVolume, AverageVolumeCylindrical, AverageVolumeSpherical, x, y
 import fenics as f
 import pytest
+import numpy as np
+from sympy.printing import ccode
+import math
 
 
 @pytest.mark.parametrize("field,volume", [("solute", 1), ("T", 2)])
@@ -43,3 +46,123 @@ class TestCompute:
         )
         computed = self.my_average.compute()
         assert computed == expected
+
+
+@pytest.mark.parametrize("radius", [2, 4])
+@pytest.mark.parametrize("r0", [3, 5])
+@pytest.mark.parametrize("height", [2, 7])
+@pytest.mark.parametrize("c_top", [8, 9])
+@pytest.mark.parametrize("c_bottom", [10, 11])
+def test_compute_cylindrical(r0, radius, height, c_top, c_bottom):
+    """
+    Test that AverageVolumeCylindrical computes the value correctly on a hollow cylinder
+
+    Args:
+        r0 (float): internal radius
+        radius (float): cylinder radius
+        height (float): cylinder height
+        c_top (float): concentration top
+        c_bottom (float): concentration bottom
+    """
+    # creating a mesh with FEniCS
+    r1 = r0 + radius
+    z0 = 0
+    z1 = z0 + height
+
+    mesh_fenics = f.RectangleMesh(f.Point(r0, z0), f.Point(r1, z1), 50, 50)
+
+    volume_markers = f.MeshFunction("size_t", mesh_fenics, mesh_fenics.topology().dim())
+    volume_markers.set_all(1)
+    dx = f.Measure("dx", domain=mesh_fenics, subdomain_data=volume_markers)
+
+    my_export = AverageVolumeCylindrical("solute", 1)
+    V = f.FunctionSpace(mesh_fenics, "P", 1)
+    c_fun = lambda z: c_bottom + (c_top - c_bottom) / (height) * z
+    expr = f.Expression(
+        ccode(c_fun(y)),
+        degree=1,
+    )
+    my_export.function = f.interpolate(expr, V)
+    my_export.dx = dx
+
+    expected_value = (c_bottom + c_top) / 2
+
+    computed_value = my_export.compute()
+
+    assert np.isclose(computed_value, expected_value)
+
+
+@pytest.mark.parametrize("radius", [2, 4])
+@pytest.mark.parametrize("r0", [3, 5])
+@pytest.mark.parametrize("c_left", [8, 9])
+@pytest.mark.parametrize("c_right", [10, 11])
+def test_compute_spherical(r0, radius, c_left, c_right):
+    """
+    Test that AverageVolumeSpherical computes the average value correctly
+    on a hollow sphere
+
+    Args:
+        r0 (float): internal radius
+        radius (float): sphere  radius
+        c_left (float): concentration left
+        c_right (float): concentration right
+    """
+    # creating a mesh with FEniCS
+    r1 = r0 + radius
+    mesh_fenics = f.IntervalMesh(10, r0, r1)
+
+    # marking physical groups (volumes and surfaces)
+    volume_markers = f.MeshFunction("size_t", mesh_fenics, mesh_fenics.topology().dim())
+    volume_markers.set_all(1)
+    dx = f.Measure("dx", domain=mesh_fenics, subdomain_data=volume_markers)
+
+    my_export = AverageVolumeSpherical("solute", 1)
+    V = f.FunctionSpace(mesh_fenics, "P", 1)
+    c_fun = lambda r: c_left + (c_right - c_left) / (r1 - r0) * r
+    expr = f.Expression(
+        ccode(c_fun(x)),
+        degree=1,
+    )
+    my_export.function = f.interpolate(expr, V)
+
+    my_export.dx = dx
+
+    expected_value = c_left + (3 * (c_right - c_left)) / (4 * (r1**3 - r0**3)) * (
+        r1 + r0
+    ) * (r1**2 + r0**2)
+
+    computed_value = my_export.compute()
+
+    assert np.isclose(computed_value, expected_value)
+
+
+def test_average_volume_cylindrical_title_no_units_solute():
+    """A simple test to check that the title is set correctly in
+    festim.AverageVolumeCylindrical with a solute field without units"""
+
+    my_export = AverageVolumeCylindrical("solute", 4)
+    assert my_export.title == "Average solute volume 4"
+
+
+def test_average_volume_cylindrical_title_no_units_temperature():
+    """A simple test to check that the title is set correctly in
+    festim.AverageVolumeCylindrical with a T field without units"""
+
+    my_export = AverageVolumeCylindrical("T", 5)
+    assert my_export.title == "Average T volume 5"
+
+
+def test_average_volume_spherical_title_no_units_solute():
+    """A simple test to check that the title is set correctly in
+    festim.AverageVolumeSpherical with a solute field without units"""
+
+    my_export = AverageVolumeSpherical("solute", 6)
+    assert my_export.title == "Average solute volume 6"
+
+
+def test_average_volume_spherical_title_no_units_temperature():
+    """A simple test to check that the title is set correctly in
+    festim.AverageVolumeSpherical with a T field without units"""
+
+    my_export = AverageVolumeSpherical("T", 9)
+    assert my_export.title == "Average T volume 9"
