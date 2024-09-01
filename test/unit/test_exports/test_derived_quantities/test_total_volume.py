e48463f6375c4c543ea45424b2ef9e6a3cc4d4e6
rekomodo <jairrs@mit.edu>
2024-09-01 13:36:33 -0400

    tests for cylindrical total volume

--- test/unit/test_exports/test_derived_quantities/test_total_volume.py
+++ test/unit/test_exports/test_derived_quantities/test_total_volume.py
@@ -1,8 +1,9 @@
-from festim import TotalVolume
+from festim import TotalVolume, TotalVolumeCylindrical
 import fenics as f
 import pytest
 from .tools import c_1D, c_2D, c_3D
 import pytest
+import numpy as np
 
 
 @pytest.mark.parametrize("field,volume", [("solute", 1), ("T", 2)])
@@ -44,6 +45,63 @@ class TestCompute:
 
         produced = self.my_total.compute()
         assert produced == expected
+
+
+@pytest.mark.parametrize("radius", [2, 3])
+@pytest.mark.parametrize("r0", [0, 2])
+@pytest.mark.parametrize("height", [2, 3])
+@pytest.mark.parametrize("azimuth_range", [(0, np.pi), (0, np.pi/2)])
+def test_compute_cylindrical(r0, radius, height, azimuth_range):
+    """
+    Test that TotalVolumeCylindrical computes the volume correctly on a cylinder
+
+    Args:
+        r0 (float): internal radius
+        radius (float): cylinder radius
+        height (float): cylinder height
+        azimuth_range (tuple): range of the azimuthal angle
+        soret (bool): if True, the Soret effect will be set
+    """
+    # creating a mesh with FEniCS
+    r1 = r0 + radius
+    z0 = 0
+    z1 = z0 + height
+    mesh_fenics = f.RectangleMesh(f.Point(r0, z0), f.Point(r1, z1), 10, 10)
+
+    # marking physical groups (volumes and surfaces)
+    volume_markers = f.MeshFunction("size_t", mesh_fenics, mesh_fenics.topology().dim())
+    volume_markers.set_all(1)
+
+    volume = 1
+    my_total = TotalVolumeCylindrical("solute", volume, azimuth_range)
+
+    dx = f.Measure("dx", domain=mesh_fenics, subdomain_data=volume_markers)
+
+    V = f.FunctionSpace(mesh_fenics, "P", 1)
+    r = f.interpolate(f.Expression("x[0]", degree=1), V)
+    c = 2*r
+
+    my_total.dx = dx
+    my_total.function = c
+    my_total.r = f.Expression("x[0]", degree=1)
+
+    az0, az1 = azimuth_range
+
+    expected_value = f.assemble((az1 - az0) * c * r * dx(volume))
+    computed_value = my_total.compute()
+
+    assert np.isclose(expected_value, computed_value)
+
+
+@pytest.mark.parametrize(
+    "azimuth_range", [(-1, np.pi), (0, 3 * np.pi), (-1, 3 * np.pi)]
+)
+def test_azimuthal_range_cylindrical(azimuth_range):
+    """
+    Tests that an error is raised when the azimuthal range is out of bounds
+    """
+    with pytest.raises(ValueError):
+        TotalVolumeCylindrical("solute", 1, azimuth_range=azimuth_range)
 
 
 @pytest.mark.parametrize(
