eab5fa536f54a822e871c7ed6a50c12007ca51ec
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-10-23 12:44:41 -0400

    type hinting for as_fenics_constant

--- festim/helpers.py
+++ festim/helpers.py
@@ -1,21 +1,24 @@
 from dolfinx import fem
+import dolfinx
 
 
-def as_fenics_constant(value, mesh):
+def as_fenics_constant(
+    value: float | int | fem.Constant, mesh: dolfinx.mesh.Mesh
+) -> fem.Constant:
     """Converts a value to a dolfinx.Constant
 
     Args:
-        value (float, int or dolfinx.Constant): the value to convert
-        mesh (dolfinx.mesh.Mesh): the mesh of the domiain
+        value: the value to convert
+        mesh: the mesh of the domiain
 
     Returns:
-        dolfinx.Constant: the converted value
+        The converted value
 
     Raises:
         TypeError: if the value is not a float, an int or a dolfinx.Constant
     """
     if isinstance(value, (float, int)):
-        return fem.Constant(mesh, float(value))
+        return fem.Constant(mesh, dolfinx.default_scalar_type(value))
     elif isinstance(value, fem.Constant):
         return value
     else:
