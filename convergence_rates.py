3101c40f2a98d5060328627297e7080d383886ec
Jørgen S. Dokken <dokken@simula.no>
2024-10-23 21:03:11 -0400

    Change linting

--- convergence_rates.py
+++ convergence_rates.py
@@ -127,7 +127,8 @@ def run(N):
     my_problem.run()
 
     computed_solution = my_problem.u
-    final_time_sim = my_problem.t.value  # we use the exact final time of the simulation which may differ from the one specified in the settings
+    # we use the exact final time of the simulation which may differ from the one specified in the settings
+    final_time_sim = my_problem.t.value
 
     def exact_solution_end(x):
         return exact_solution(x, final_time_sim)
