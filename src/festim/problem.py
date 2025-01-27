be656dc5041ca156cf50d17a63be26bb8387c9e6
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-01-26 18:36:35 -0800

    adjust default rtol

--- src/festim/problem.py
+++ src/festim/problem.py
@@ -121,7 +121,7 @@ class ProblemBase:
         )
         self.solver = NewtonSolver(MPI.COMM_WORLD, problem)
         self.solver.atol = self.settings.atol
-        self.solver.rtol = self.settings.rtol if not callable(self.settings.rtol) else 1e-10
+        self.solver.rtol = self.settings.rtol if not callable(self.settings.rtol) else self.settings.rtol
         self.solver.max_it = self.settings.max_iterations
 
         ksp = self.solver.krylov_solver
