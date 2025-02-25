7b0ea6dc0826f61ca0e9a6c9f17b5016908c4970
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-02-25 17:58:08 -0500

    add temp field setter

--- src/festim/exports/surface_temperature.py
+++ src/festim/exports/surface_temperature.py
@@ -55,6 +55,18 @@ class SurfaceTemperature:
     @property
     def temperature_field(self):
         return self._temperature_field
+    
+    @temperature_field.setter
+    def temperature_field(self, value):
+        # check that temperature field is fem.Constant, fem.Function, or fem.Expression
+        if not isinstance(value, (fem.Constant, str)):
+            raise TypeError("field must be of type fem.Constant, fem.Function, or fem.Expression")
+        elif not isinstance(value, (fem.Function, str)):
+            raise TypeError("field must be of type fem.Constant, fem.Function, or fem.Expression")
+        elif not isinstance(value, (fem.Expression, str)):
+            raise TypeError("field must be of type fem.Constant, fem.Function, or fem.Expression")
+
+        self._field = value
 
     @property
     def title(self):
