4e5e68b05ac83b705c6977d4ce5144b92241dcdf
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-02-25 16:15:04 -0500

    add surface temp to derived quantities test

--- test/test_derived_quantities.py
+++ test/test_derived_quantities.py
@@ -10,6 +10,7 @@ surf_1 = F.SurfaceSubdomain(id=1)
 surf_2 = F.SurfaceSubdomain(id=2)
 vol_1 = F.VolumeSubdomain(id=1, material="test")
 vol_2 = F.VolumeSubdomain(id=2, material="test")
+temp = 400
 results = "test.csv"
 
 surface_flux = F.SurfaceFlux(field=mobile_H, surface=surf_1, filename=results)
@@ -24,6 +25,7 @@ avg_surface = F.AverageSurface(mobile_D, surface=surf_1, filename=results)
 avg_vol = F.AverageVolume(mobile_H, volume=vol_2, filename=results)
 surf_quant = F.SurfaceQuantity(mobile_H, surface=surf_1, filename=results)
 vol_quant = F.VolumeQuantity(mobile_H, volume=vol_1, filename=results)
+surf_temp = F.SurfaceTemperature(temperature_field=temp, surface=surf_1, filename=results)
 
 
 @pytest.mark.parametrize(
@@ -39,6 +41,7 @@ vol_quant = F.VolumeQuantity(mobile_H, volume=vol_1, filename=results)
         (max_surface, "Maximum H surface 2"),
         (avg_surface, "Average D surface 1"),
         (avg_vol, "Average H volume 2"),
+        (surf_temp, "Temperature surface 1")
     ],
 )
 def test_title(quantity, expected_title, tmp_path):
