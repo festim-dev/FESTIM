66ad7883356294796dfd856a024f2037c21016b5
kaelyndunnell <kaelyndunnell03@gmail.com>
2025-02-25 23:14:32 -0500

    uodate test

--- test/test_surface_temperature.py
+++ test/test_surface_temperature.py
@@ -44,7 +44,7 @@ def test_surface_temperature_compute_1D(T_function, expected_values):
         my_model.t.value += dt.value
         my_model.update_time_dependent_values()
 
-    my_export = F.SurfaceTemperature(temperature_field=T_function, surface=dummy_surface)
+    my_export = F.SurfaceTemperature(temperature_field=my_model.temperature, surface=dummy_surface)
     my_export.compute(ds)
 
     # TEST
