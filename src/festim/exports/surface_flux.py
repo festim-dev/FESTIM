58577e87cf65080e48a4590fd0b124a022619889
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-04-18 10:09:26 -0400

    integrated SurfaceFlux

--- src/festim/exports/surface_flux.py
+++ src/festim/exports/surface_flux.py
@@ -20,7 +20,7 @@ class SurfaceFlux(SurfaceQuantity):
     def title(self):
         return f"{self.field.name} flux surface {self.surface.id}"
 
-    def compute(self, ds):
+    def compute(self, u, ds, entity_maps=None):
         """Computes the value of the flux at the surface
 
         Args:
@@ -29,17 +29,16 @@ class SurfaceFlux(SurfaceQuantity):
 
         # obtain mesh normal from field
         # if case multispecies, solution is an index, use sub_function_space
-        if isinstance(self.field.solution, ufl.indexed.Indexed):
+        if isinstance(u, ufl.indexed.Indexed):
             mesh = self.field.sub_function_space.mesh
         else:
-            mesh = self.field.solution.function_space.mesh
+            mesh = u.function_space.mesh
         n = ufl.FacetNormal(mesh)
 
         self.value = fem.assemble_scalar(
             fem.form(
-                -self.D
-                * ufl.dot(ufl.grad(self.field.solution), n)
-                * ds(self.surface.id)
+                -self.D * ufl.dot(ufl.grad(u), n) * ds(self.surface.id),
+                entity_maps=entity_maps,
             )
         )
         self.data.append(self.value)
