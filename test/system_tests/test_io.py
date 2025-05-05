38543a84dd3d774b759b1956822e04773c3bbeaa
jhdark <darkj385@mit.edu>
2025-05-05 10:37:04 -0400

    additional tests for coverage

--- test/system_tests/test_io.py
+++ test/system_tests/test_io.py
@@ -1,7 +1,11 @@
-import festim as F
+from unittest.mock import patch
+
+import mpi4py.MPI as MPI
 
 import dolfinx
-import mpi4py.MPI as MPI
+import numpy as np
+
+import festim as F
 
 
 def test_writing_and_reading_of_species_function_using_checkpoints(tmpdir):
@@ -105,3 +109,89 @@ def test_writing_and_reading_of_species_function_using_checkpoints(tmpdir):
         1.5,
         atol=1e-10,
     )
+
+
+def test_VTXExport_times_added_to_milestones(tmpdir):
+    """Creates a HydrogenTransportProblem object and checks that, if no
+    stepsize.milestones are given and VTXExport.times are given, VTXExport.times are
+    are added to stepsize.milestones by .initialise()
+
+    Args:
+        tmpdir (os.PathLike): path to the pytest temporary folder
+    """
+    H = F.Species("H", mobile=True)
+    filename = str(tmpdir.join("my_export.bp"))
+    my_export = F.VTXSpeciesExport(
+        field=H,
+        filename=filename,
+        times=[1, 2, 3],
+    )
+
+    # build
+    my_model = F.HydrogenTransportProblem()
+    my_model.mesh = F.Mesh1D([1, 2, 3])
+    my_model.species = [H]
+    my_model.subdomains = [
+        F.VolumeSubdomain1D(id=1, borders=[1, 3], material=F.Material(D_0=1, E_D=0))
+    ]
+    my_model.temperature = 100
+    my_model.settings = F.Settings(
+        atol=1e0,
+        rtol=1e0,
+        final_time=4,
+        transient=True,
+        stepsize=F.Stepsize(initial_value=3),
+    )
+    my_model.exports = [my_export]
+
+    # run
+    my_model.initialise()
+
+    # test
+    assert my_model.settings.stepsize.milestones == my_export.times
+
+
+def test_vtx_writer_called_only_at_specified_times(tmpdir):
+    """test that the VTXWriter.write function is called the number of times specified in
+    the export.times"""
+
+    filename = str(tmpdir.join("my_export.bp"))
+
+    with patch("dolfinx.io.VTXWriter") as MockWriter:
+        # This is the mock instance of VTXWriter
+        mock_writer_instance = MockWriter.return_value
+
+        my_model = F.HydrogenTransportProblem()
+
+        my_mesh = F.Mesh1D(vertices=np.linspace(0, 10, 100))
+        my_model.mesh = my_mesh
+
+        H = F.Species("H", mobile=True)
+        my_model.species = [H]
+
+        my_mat = F.Material(D_0=1.0, E_D=0)
+        vol = F.VolumeSubdomain1D(id=1, borders=[0, 10], material=my_mat)
+        left = F.SurfaceSubdomain1D(id=2, x=0)
+        right = F.SurfaceSubdomain1D(id=3, x=10)
+        my_model.subdomains = [vol, left, right]
+
+        my_model.temperature = 500
+
+        my_model.boundary_conditions = [
+            F.FixedConcentrationBC(subdomain=left, value=5.0, species=H),
+            F.FixedConcentrationBC(subdomain=right, value=0, species=H),
+        ]
+
+        my_export = F.VTXSpeciesExport(filename=filename, field=H, times=[5, 10, 15])
+        my_model.exports = [my_export]
+
+        dt = F.Stepsize(initial_value=1)
+        my_model.settings = F.Settings(
+            atol=1e-10, rtol=1e-10, transient=True, final_time=15, stepsize=dt
+        )
+
+        my_model.initialise()
+        my_model.run()
+
+        # Check number of write calls
+        assert mock_writer_instance.write.call_count == 3
