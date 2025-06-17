d6cc1d97ad30a8d3796798ba35c2fc994987becf
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-06-17 15:37:01 -0400

    added locator argument

--- src/festim/subdomain/surface_subdomain.py
+++ src/festim/subdomain/surface_subdomain.py
@@ -1,15 +1,80 @@
+import dolfinx
+import numpy as np
+from typing import Callable
+
+
 class SurfaceSubdomain:
     """
     Surface subdomain class
 
     Args:
-        id (int): the id of the surface subdomain
+        id: the id of the surface subdomain
+        locator: a callable function that locates the boundary facets of the subdomain
+
+    Examples:
+
+        .. testsetup:: SurfaceSubdomain
+
+            from festim import SurfaceSubdomain
+
+        .. testcode:: SurfaceSubdomain
+
+            SurfaceSubdomain(id=1, locator=lambda x: np.isclose(x[0], 1.0))
+            SurfaceSubdomain(id=1, locator=lambda x: np.logical_or(np.isclose(x[1], 0.0), np.isclose(x[1], 1.0)))
+            SurfaceSubdomain(id=1, locator=lambda x: np.logical_and(np.isclose(x[0], 0.0), np.isclose(x[1], 1.0)))
+            SurfaceSubdomain(id=1, locator=lambda x: np.logical_and(np.isclose(x[0], 0.0), x[1] <= 0.5))
     """
 
     id: int
+    locator: Callable
 
-    def __init__(self, id):
+    def __init__(self, id: int, locator: Callable = None):
         self.id = id
+        self.locator = locator
+
+    def locate_boundary_facet_indices(self, mesh):
+        if self.locator is None:
+            raise ValueError(
+                "No locator function provided for locating boundary facets."
+            )
+
+        return dolfinx.mesh.locate_entities_boundary(
+            mesh,
+            mesh.topology.dim - 1,
+            self.locator,
+        )
+
+
+class SurfaceSubdomain1D(SurfaceSubdomain):
+    """
+    Surface subdomain class for 1D cases
+
+    Args:
+        id: the id of the surface subdomain
+        x: the x coordinate of the surface subdomain
+
+    Attributes:
+        id: the id of the surface subdomain
+        x: the x coordinate of the surface subdomain
+
+    Examples:
+
+        .. testsetup:: SurfaceSubdomain1D
+
+            from festim import SurfaceSubdomain1D
+
+        .. testcode:: SurfaceSubdomain1D
+
+            SurfaceSubdomain1D(id=1, x=1)
+    """
+
+    # FIXME: Rename this to _id and use getter/setter
+    id: int
+    x: float
+
+    def __init__(self, id: int, x: float) -> None:
+        super().__init__(id, locator=lambda x_: np.isclose(x_[0], x))
+        self.x = x
 
 
 def find_surface_from_id(id: int, surfaces: list):
