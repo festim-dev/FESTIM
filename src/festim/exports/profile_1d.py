9f21386d0e95456f2b237176d28e609702748c42
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2025-07-16 10:23:16 -0400

    removed unused code

--- src/festim/exports/profile_1d.py
+++ src/festim/exports/profile_1d.py
@@ -21,13 +21,3 @@ class Profile1DExport:
 
         self._dofs = None
         self._sort_coords = None
-
-
-def compute_profile(u, index):
-    V0, dofs = u.function_space.sub(index).collapse()
-    coords = V0.tabulate_dof_coordinates()[:, 0]
-    sort_coords = np.argsort(coords)
-    c = u.x.array[dofs][sort_coords]
-    x = coords[sort_coords]
-
-    return x, c
