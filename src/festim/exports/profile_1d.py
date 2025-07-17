2f91376b970b1c9c2455affd1f47181ed5c7d56c
Rémi Delaporte-Mathurin <40028739+RemDelaporteMathurin@users.noreply.github.com>
2025-07-17 11:04:40 -0400

    docstrings
    Co-authored-by: James Dark <65899899+jhdark@users.noreply.github.com>

--- src/festim/exports/profile_1d.py
+++ src/festim/exports/profile_1d.py
@@ -4,7 +4,20 @@ from festim.subdomain import VolumeSubdomain
 
 
 class Profile1DExport:
-    """Class to export 1D profiles of a field in a simulation."""
+    """Class to export 1D profiles of a field in a simulation.
+
+    Args:
+        field: the species for which the profile is computed.
+        subdomain: the volume subdomain to compute the profile on. If None, the profile
+            is computed over the entire domain.
+
+    Attributes:
+        field: the species for which the profile is computed.
+        subdomain: the volume subdomain to compute the profile on. If None, the profile
+            is computed over the entire domain.
+        x: the coordinates along which the profile is computed.
+        data: the computed profile data.
+    """
 
     x: np.ndarray
     data: list
