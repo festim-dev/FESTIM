1dfd6019d226d75b2a3f5632c41b47efb24f724b
jhdark <james.dark.95@gmail.com>
2026-01-16 13:51:36 -0500

    Use function instead of self.field

--- src/festim/exports/average_surface.py
+++ src/festim/exports/average_surface.py
@@ -1,3 +1,5 @@
+import ufl
+from dolfinx import fem
 from scifem import assemble_scalar
 
 from festim.exports.surface_quantity import SurfaceQuantity
@@ -19,13 +21,20 @@ class AverageSurface(SurfaceQuantity):
     def title(self):
         return f"Average {self.field.name} surface {self.surface.id}"
 
-    def compute(self, ds):
+    def compute(
+        self, u: fem.Function | ufl.indexed.Indexed, ds: ufl.Measure, entity_maps=None
+    ):
         """
         Computes the average value of the field on the defined surface
         subdomain, and appends it to the data list
+
+        Args:
+            u: field for which the average value is computed
+            ds: surface measure of the model
+            entity_maps: entity maps relating parent mesh and submesh
         """
 
         self.value = assemble_scalar(
-            self.field.solution * ds(self.surface.id)
+            u.solution * ds(self.surface.id)
         ) / assemble_scalar(1 * ds(self.surface.id))
         self.data.append(self.value)
