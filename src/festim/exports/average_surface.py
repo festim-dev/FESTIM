bf1232e56148baedcbc567fd55d5d28d9b4e4661
jhdark <james.dark.95@gmail.com>
2026-01-16 13:57:47 -0500

    use entity maps

--- src/festim/exports/average_surface.py
+++ src/festim/exports/average_surface.py
@@ -34,7 +34,7 @@ class AverageSurface(SurfaceQuantity):
             entity_maps: entity maps relating parent mesh and submesh
         """
 
-        self.value = assemble_scalar(u * ds(self.surface.id)) / assemble_scalar(
-            1 * ds(self.surface.id)
-        )
+        self.value = assemble_scalar(
+            fem.form(u * ds(self.surface.id), entity_maps=entity_maps)
+        ) / assemble_scalar(fem.form(1 * ds(self.surface.id), entity_maps=entity_maps))
         self.data.append(self.value)
