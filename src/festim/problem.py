dec3499a44a674c8e4c4198d09c07cc49f0ca7f3
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2026-05-18 09:30:56 -0400

    calculate the form everytime

--- src/festim/problem.py
+++ src/festim/problem.py
@@ -123,8 +123,8 @@ class ProblemBase:
         """Defines the dirichlet boundary conditions of the model."""
         for bc in self.boundary_conditions:
             if isinstance(bc, F.DirichletBCBase):
+                form = self.create_dirichletbc_form(bc)
                 if not bc.enforce_weakly:
-                    form = self.create_dirichletbc_form(bc)
                     self.bc_forms.append(form)
 
     def get_petsc_options(self) -> dict[str, Any]:
