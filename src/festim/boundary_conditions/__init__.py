ba5802bb9d357ff9c126a0a2307bdad248f79f87
jhdark <darkj385@mit.edu>
2026-06-03 14:21:50 -0600

    additioanl bc for fixed conc at inlet

--- src/festim/boundary_conditions/__init__.py
+++ src/festim/boundary_conditions/__init__.py
@@ -1,16 +1,24 @@
-from .dirichlet_bc import DirichletBCBase, FixedConcentrationBC, FixedTemperatureBC
+from .dirichlet_bc import (
+    DirichletBCBase,
+    FixedConcentrationBC,
+    FixedConcentrationInflowBC,
+    FixedTemperatureBC,
+)
 from .flux_bc import FluxBCBase, HeatFluxBC, ParticleFluxBC
 from .henrys_bc import HenrysBC
+from .outflow_bc import OutflowBC
 from .sieverts_bc import SievertsBC
 from .surface_reaction import SurfaceReactionBC, SurfaceReactionBCpartial
 
 __all__ = [
     "DirichletBCBase",
     "FixedConcentrationBC",
+    "FixedConcentrationInflowBC",
     "FixedTemperatureBC",
     "FluxBCBase",
     "HeatFluxBC",
     "HenrysBC",
+    "OutflowBC",
     "ParticleFluxBC",
     "SievertsBC",
     "SurfaceReactionBC",
