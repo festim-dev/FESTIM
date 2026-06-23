e79006320ef73634c2d4b54606a417f9386dcb0d
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2026-06-23 13:31:31 -0400

    rewrote tests

--- test/test_plot.py
+++ test/test_plot.py
@@ -1,145 +1,80 @@
-import importlib
-import sys
-import types
+from mpi4py import MPI
 
+import numpy as np
 import pytest
+from dolfinx import fem, mesh
 
 import festim as F
 
-_MOCK_GRID = object()
+pyvista = pytest.importorskip("pyvista")
+pyvista.OFF_SCREEN = True
 
 
-class FakePlotter:
-    def __init__(self, shape=None):
-        self.shape = shape
-        self.mesh_calls = []
-        self.subplot_calls = []
-        self.text_calls = []
-        self.show_called = False
-        self.screenshot_filename = None
-
-    def subplot(self, row, col):
-        self.subplot_calls.append((row, col))
-
-    def add_mesh(self, grid, **kwargs):
-        self.mesh_calls.append((grid, kwargs))
-
-    def view_xy(self):
-        pass
-
-    def add_text(self, text, font_size):
-        self.text_calls.append((text, font_size))
-
-    def show(self):
-        self.show_called = True
-
-    def screenshot(self, filename):
-        self.screenshot_filename = filename
+def create_mock_solution():
+    test_mesh = mesh.create_unit_interval(MPI.COMM_WORLD, 10)
+    V = fem.functionspace(test_mesh, ("Lagrange", 1))
+    u = fem.Function(V)
+    u.x.array[:] = np.ones_like(u.x.array)
+    return u
 
 
-def _setup_fake_pyvista(monkeypatch, off_screen=False):
-    fake_pyvista = types.SimpleNamespace(
-        OFF_SCREEN=off_screen,
-        Plotter=FakePlotter,
-        UnstructuredGrid=object,
-    )
-    monkeypatch.setitem(sys.modules, "pyvista", fake_pyvista)
-    return fake_pyvista
-
-
-def test_plot_single_species(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=False)
-    plot_module = importlib.import_module("festim.plot")
-    monkeypatch.setattr(
-        plot_module, "_make_ugrid", lambda solution, pyvista_module: _MOCK_GRID
-    )
-
+def test_plot_single_species():
     species = F.Species("H")
-    species.post_processing_solution = object()
+    species.post_processing_solution = create_mock_solution()
+
     plotter = F.plot(species, show_edges=True, opacity=0.5)
 
-    assert plotter.shape is None
-    assert len(plotter.mesh_calls) == 1
-    assert plotter.mesh_calls[0][1]["show_edges"] is True
-    assert plotter.mesh_calls[0][1]["opacity"] == 0.5
-    assert plotter.show_called is True
+    assert isinstance(plotter, pyvista.Plotter)
+    assert plotter.shape == (1, 1)
 
 
-def test_plot_multiple_species_creates_subplots(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=False)
-    plot_module = importlib.import_module("festim.plot")
-    monkeypatch.setattr(
-        plot_module, "_make_ugrid", lambda solution, pyvista_module: _MOCK_GRID
-    )
-
+def test_plot_multiple_species_creates_subplots():
     h = F.Species("H")
     d = F.Species("D")
-    h.post_processing_solution = object()
-    d.post_processing_solution = object()
+    h.post_processing_solution = create_mock_solution()
+    d.post_processing_solution = create_mock_solution()
+
     plotter = F.plot([h, d])
 
     assert plotter.shape == (1, 2)
-    assert plotter.subplot_calls == [(0, 0), (0, 1)]
-    assert len(plotter.mesh_calls) == 2
 
 
-def test_plot_subdomain_uses_subdomain_solution(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=False)
-    plot_module = importlib.import_module("festim.plot")
-
-    used_solutions = []
-
-    def fake_make_ugrid(solution, pyvista_module):
-        used_solutions.append(solution)
-        return _MOCK_GRID
-
-    monkeypatch.setattr(plot_module, "_make_ugrid", fake_make_ugrid)
-
+def test_plot_subdomain_uses_subdomain_solution():
     material = F.Material(D_0=1, E_D=0)
     vol_1 = F.VolumeSubdomain(id=1, material=material)
     vol_2 = F.VolumeSubdomain(id=2, material=material)
-    sol_1 = object()
-    sol_2 = object()
+    sol_1 = create_mock_solution()
+    sol_2 = create_mock_solution()
+
     h = F.Species("H")
     h.subdomain_to_post_processing_solution = {vol_1: sol_1, vol_2: sol_2}
 
-    F.plot(h, subdomain=vol_2)
-    assert used_solutions == [sol_2]
+    plotter = F.plot(h, subdomain=vol_2)
+    assert isinstance(plotter, pyvista.Plotter)
 
 
-def test_plot_with_filename_saves_screenshot(monkeypatch, tmp_path):
-    _setup_fake_pyvista(monkeypatch, off_screen=True)
-    plot_module = importlib.import_module("festim.plot")
-    monkeypatch.setattr(
-        plot_module, "_make_ugrid", lambda solution, pyvista_module: _MOCK_GRID
-    )
-
+def test_plot_with_filename_saves_screenshot(tmp_path):
     species = F.Species("H")
-    species.post_processing_solution = object()
+    species.post_processing_solution = create_mock_solution()
     filename = tmp_path / "out.png"
-    plotter = F.plot(species, filename=filename)
 
-    assert plotter.screenshot_filename == str(filename)
-    assert plotter.show_called is False
+    F.plot(species, filename=filename)
+
+    assert filename.exists()
 
 
-def test_plot_with_string_filename_saves_screenshot(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=True)
-    plot_module = importlib.import_module("festim.plot")
-    monkeypatch.setattr(
-        plot_module, "_make_ugrid", lambda solution, pyvista_module: _MOCK_GRID
-    )
+def test_plot_with_string_filename_saves_screenshot(tmp_path, monkeypatch):
+    monkeypatch.chdir(tmp_path)
 
     species = F.Species("H")
-    species.post_processing_solution = object()
-    plotter = F.plot(species, filename="out.png")
+    species.post_processing_solution = create_mock_solution()
 
-    assert plotter.screenshot_filename == "out.png"
-    assert plotter.show_called is False
+    F.plot(species, filename="out.png")
+
+    assert (tmp_path / "out.png").exists()
 
 
-def test_plot_raises_for_invalid_field_type(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=False)
+def test_plot_raises_for_invalid_field_type():
     with pytest.raises(
         TypeError,
         match=r"field must be of type festim\.Species or a list of festim\.Species",
@@ -147,38 +82,15 @@ def test_plot_raises_for_invalid_field_type(monkeypatch):
         F.plot("H")
 
 
-def test_plot_raises_if_no_solution(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=False)
+def test_plot_raises_if_no_solution():
     with pytest.raises(ValueError, match="has no post_processing_solution to plot"):
         F.plot(F.Species("H"))
 
 
-def test_plot_default_show_edges_and_empty_name(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=False)
-    plot_module = importlib.import_module("festim.plot")
-    monkeypatch.setattr(
-        plot_module, "_make_ugrid", lambda solution, pyvista_module: _MOCK_GRID
-    )
-
+def test_plot_default_show_edges_and_empty_name():
     species = F.Species()
-    species.post_processing_solution = object()
+    species.post_processing_solution = create_mock_solution()
+
     plotter = F.plot(species)
 
-    assert plotter.mesh_calls[0][1]["show_edges"] is False
-    assert plotter.text_calls == []
-    assert plotter.show_called is True
-
-
-def test_plot_off_screen_without_filename_does_not_show(monkeypatch):
-    _setup_fake_pyvista(monkeypatch, off_screen=True)
-    plot_module = importlib.import_module("festim.plot")
-    monkeypatch.setattr(
-        plot_module, "_make_ugrid", lambda solution, pyvista_module: _MOCK_GRID
-    )
-
-    species = F.Species("H")
-    species.post_processing_solution = object()
-    plotter = F.plot(species)
-
-    assert plotter.show_called is False
-    assert plotter.screenshot_filename is None
+    assert isinstance(plotter, pyvista.Plotter)
